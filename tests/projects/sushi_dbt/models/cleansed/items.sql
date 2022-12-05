28b58d94d87226a7937fa4866fc151c1d89cc524
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2022-12-05 12:39:17 -0800

    Add sushi dbt project

---
+++ tests/projects/sushi_dbt/models/cleansed/items.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized='incremental',
+        incremental_strategy='delete+insert',
+        cluster_by=['ds'],
+        unique_key=['ds'],
+    )
+}}
+
+SELECT
+  id::DOUBLE AS id, /* Primary key */
+  name::TEXT AS name, /* Name of the sushi */
+  price::DOUBLE AS price, /* Price of the sushi */
+  ds::TEXT AS ds /* Date */
+FROM {{ source('raw', 'items') }}
+{% if is_incremental() %}
+WHERE
+  ds > (select max(ds) from {{ this }})
+{% endif %}
