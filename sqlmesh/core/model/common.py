455cac737aae8e49ab1782fe4a19a8f8cf938fe0
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2022-12-22 14:01:24 -0800

    simplify name splitting

--- sqlmesh/core/model/common.py
+++ sqlmesh/core/model/common.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import typing as t
 
+from sqlglot.expressions import split_num_words
+
 
 def parse_model_name(name: str) -> t.Tuple[t.Optional[str], t.Optional[str], str]:
     """Convert a model name into table parts.
@@ -12,9 +14,4 @@ def parse_model_name(name: str) -> t.Tuple[t.Optional[str], t.Optional[str], str
     Returns:
         A tuple consisting of catalog, schema, table name.
     """
-    splits = name.split(".")
-    if len(splits) == 3:
-        return (splits[0], splits[1], splits[2])
-    if len(splits) == 2:
-        return (None, splits[0], splits[1])
-    return (None, None, name)
+    return split_num_words(name, ".", 3)  # type: ignore
