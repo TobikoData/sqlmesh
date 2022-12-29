04187d4ca5a002121f1ed316328a42863d9d78e9
Chris Rericha <67359577+crericha@users.noreply.github.com>
2022-12-29 18:44:41 -0500

    Additional DBT configuration support (#103)
    * Allow extra config fields
    
    * support dbt_project.yml model-paths
    
    * Collapse general DBT config properties into inherited class
    
    * Use sqlglot ensure_list

--- sqlmesh/dbt/common.py
+++ sqlmesh/dbt/common.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import typing as t
 from enum import Enum, auto
 
-from sqlmesh.utils.conversions import try_str_to_bool
+from pydantic import validator
+from sqlglot.helper import ensure_list
+
+from sqlmesh.utils.conversions import ensure_bool, try_str_to_bool
 from sqlmesh.utils.errors import ConfigError
 from sqlmesh.utils.pydantic import PydanticModel
 
@@ -23,7 +26,7 @@ class UpdateStrategy(Enum):
 def update_field(
     old: t.Optional[t.Any],
     new: t.Any,
-    update_strategy: t.Optional[UpdateStrategy] = UpdateStrategy.REPLACE,
+    update_strategy: t.Optional[UpdateStrategy] = None,
 ) -> t.Any:
     """
     Update config field with new config value
@@ -38,6 +41,8 @@ def update_field(
     """
     if not old:
         return new
+
+    update_strategy = update_strategy or UpdateStrategy.REPLACE
 
     if update_strategy == UpdateStrategy.IMMUTABLE:
         raise ConfigError("Cannot modify property: {old}")
@@ -80,11 +85,19 @@ def update_field(
 
         return combined
 
-    raise ConfigError("Unknown update strategy {update_strategy}")
+    raise ConfigError(f"Unknown update strategy {update_strategy}")
 
 
 class BaseConfig(PydanticModel):
+    """
+    Base configuration functionality for DBT configuration classes
+    """
+
     _FIELD_UPDATE_STRATEGY: t.ClassVar[t.Dict[str, UpdateStrategy]] = {}
+
+    class Config:
+        extra = "allow"
+        allow_mutation = True
 
     def update_with(self: T, config: t.Dict[str, t.Any]) -> T:
         """
@@ -99,8 +112,8 @@ class BaseConfig(PydanticModel):
         copy = self.copy()
         other = self.__class__(**config)
 
-        for field in other.__fields__:
-            if field in config:
+        for field in other.__fields_set__:
+            if field in copy.__fields__:
                 setattr(
                     copy,
                     field,
@@ -110,6 +123,8 @@ class BaseConfig(PydanticModel):
                         self._FIELD_UPDATE_STRATEGY.get(field),
                     ),
                 )
+            else:
+                setattr(copy, field, getattr(other, field))
 
         return copy
 
@@ -122,6 +137,65 @@ class BaseConfig(PydanticModel):
         """
         for field in other.__fields_set__:
             setattr(self, field, getattr(other, field))
+
+
+class GeneralConfig(BaseConfig):
+    """
+    General DBT configuration properties for models, sources, seeds, columns, etc.
+
+    Args:
+        description: Description of element
+        tests: Tests for the element
+        enabled: When false, the element is ignored
+        docs: Documentation specific configuration
+        perist_docs: Persist resource descriptions as column and/or relation comments in the database
+        tags: List of tags that can be used for element grouping
+        meta: Dictionary of metadata for the element
+    """
+
+    description: t.Optional[str] = None
+    # TODO add test support
+    tests: t.Dict[str, t.Any] = {}
+    enabled: bool = True
+    docs: t.Dict[str, t.Any] = {"show": True}
+    persist_docs: t.Dict[str, t.Any] = {}
+    tags: t.List[str] = []
+    meta: t.Dict[str, t.Any] = {}
+
+    @validator("enabled", pre=True)
+    def _validate_bool(cls, v: str) -> bool:
+        return ensure_bool(v)
+
+    @validator("docs", pre=True)
+    def _validate_dict(cls, v: t.Dict[str, t.Any]) -> t.Dict[str, t.Any]:
+        for key, value in v.items():
+            if isinstance(value, str):
+                v[key] = try_str_to_bool(value)
+
+        return v
+
+    @validator("persist_docs", pre=True)
+    def _validate_persist_docs(cls, v: t.Dict[str, str]) -> t.Dict[str, bool]:
+        return {key: bool(value) for key, value in v.items()}
+
+    @validator("tags", pre=True)
+    def _validate_list(cls, v: t.Union[str, t.List[str]]) -> t.List[str]:
+        return ensure_list(v)
+
+    @validator("meta", pre=True)
+    def _validate_meta(cls, v: t.Dict[str, t.Union[str, t.Any]]) -> t.Dict[str, t.Any]:
+        return parse_meta(v)
+
+    _FIELD_UPDATE_STRATEGY: t.ClassVar[t.Dict[str, UpdateStrategy]] = {
+        **BaseConfig._FIELD_UPDATE_STRATEGY,
+        **{
+            "tests": UpdateStrategy.KEY_UPDATE,
+            "docs": UpdateStrategy.KEY_UPDATE,
+            "persist_docs": UpdateStrategy.KEY_UPDATE,
+            "tags": UpdateStrategy.APPEND,
+            "meta": UpdateStrategy.KEY_UPDATE,
+        },
+    }
 
 
 def parse_meta(v: t.Dict[str, t.Any]) -> t.Dict[str, t.Any]:
