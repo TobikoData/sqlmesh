9cdfbf3fc50f996453cb4a80ac9b59fa6861e1e9
dgtlspc <mykalmax@yahoo.com>
2023-01-16 11:49:56 -0800

    forgot to fix one more import

--- web/client/src/routes.tsx
+++ web/client/src/routes.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "./library/components/Root/Root";
+import Root from "./library/components/root/Root";
 
 export const router = createBrowserRouter([
   {
