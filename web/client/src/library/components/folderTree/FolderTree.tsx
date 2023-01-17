c6589b6676021d8dd521b8c5203c1c50f3d95ec9
dgtlspc <mykalmax@yahoo.com>
2023-01-16 17:26:30 -0800

    layout bluprint

--- web/client/src/library/components/folderTree/FolderTree.tsx
+++ web/client/src/library/components/folderTree/FolderTree.tsx
@@ -1,6 +1,11 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { useProjectStructure, type Folder, type File } from '../../../api';
+import { FolderOpenIcon, DocumentIcon } from '@heroicons/react/24/solid'
+import { FolderIcon } from '@heroicons/react/24/outline'
+import { ChevronRightIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react';
 
+const CSS_ICON_SIZE = 'w-5 h-5';
 
 export default function FolderTree() {
   useQueryClient();
@@ -17,32 +22,57 @@ export default function FolderTree() {
 
 
   return (
-    <div>
-      {data?.folders && <Folders folders={data.folders} isOpen={true} />}
-      {data?.files && <Files files={data.files}/>}
+    <div className='py-4 px-2 overflow-hidden'>
+      {data?.folders?.length && <Folders folders={data.folders} withIndent={false} />}
+      {data?.files?.length && <Files files={data.files}/>}
     </div>
   );
 }
 
-function Folders(props: { folders: Folder[], isOpen: boolean } = { folders: [], isOpen: true }) {
+function Folders(props: { folders: Folder[], withIndent: boolean } = { folders: [], withIndent: false }) {
   return (
-    <ul className='mx-4'>
+    <ul className={`${props.withIndent ? 'ml-4': '' } mr-1 overflow-hidden`}>
       {props.folders.map((folder) => (
-        <li key={folder.id} className='p-1'>
-          {folder.name}
-          {props.isOpen && Array.isArray(folder.folders) && <Folders folders={folder.folders} isOpen={false} />}
-          {props.isOpen && Array.isArray(folder.files) && <Files files={folder.files}/>}
+        <li key={folder.id} title={folder.name}>
+          <Folder folder={folder} />
         </li>
       ))}
     </ul>
   )
 }
 
+function Folder(props: { folder: Folder }) {
+  const [isOpen, setOpen] = useState(false);
+  const IconChevron = isOpen ? ChevronDownIcon : ChevronRightIcon;
+  const IconFolder = isOpen ? FolderOpenIcon : FolderIcon;
+
+  return (
+    <>
+      <span className='text-base block whitespace-nowrap pb-1 cursor-pointer hover:bg-gray-800' onClick={() => setOpen(!isOpen)}>
+        <IconChevron className={`inline-block ${CSS_ICON_SIZE} mr-2 text-gray-100`} />
+        <IconFolder  className={`inline-block ${CSS_ICON_SIZE} mr-3 text-gray-100`} />
+        <span className='inline-block'>
+          {props.folder.name}
+        </span>
+      </span>
+      {isOpen && Array.isArray(props.folder.folders) && Boolean(props.folder.folders.length) && <Folders folders={props.folder.folders} withIndent={true} />}
+      {isOpen && Array.isArray(props.folder.files) && Boolean(props.folder.files.length) && <Files files={props.folder.files}/>}
+    </>
+  )
+}
+
 function Files(props: { files: File[] } = { files: [] }) {
   return (
-    <ul className='mx-4'>
+    <ul className='ml-4 mr-1 overflow-hidden'>
       {props.files.map((file) => (
-        <li key={file.id} className='p-1'>{file.name}</li>
+        <li key={file.id} title={file.name}>
+          <span className='text-base block whitespace-nowrap pb-1 cursor-pointer hover:bg-gray-800'>
+            <DocumentIcon className={`inline-block ${CSS_ICON_SIZE} mr-3 text-gray-100`} />
+            <span className='inline-block'>
+              {file.name}
+            </span>
+          </span>
+        </li>
       ))}
     </ul>
   )
--- web/client/src/library/components/root/Root.tsx
+++ web/client/src/library/components/root/Root.tsx
@@ -1,25 +1,130 @@
 import FolderTree from '../folderTree/FolderTree';
 import LogoTobiko from '../logo/Tobiko'
+import { PlayIcon, PauseIcon } from '@heroicons/react/24/solid'
 import "./Root.css";
 
 export default function Root() {
   return (
     <>
-      <header className='bg-gray-800 px-2 py-1'>
-        <LogoTobiko style={{ height: '32px' }} />
-      </header>
-      <main className='w-full h-full flex overflow-hidden'>
-        <div className='bg-gray-800 min-w-[4rem] max-w-[4rem] py-2'></div>
-        <div className='bg-gray-700 min-w-[12rem] w-full max-w-[16rem] py-2 overflow-auto'>
-          <FolderTree />
-        </div>
-        <div className='bg-gray-100 min-w-[20rem] w-full flex flex-col'>
-          <div className='bg-gray-100 h-full py-2'></div>
-          <div className='bg-gray-200 h-full max-h-[25%] py-2'></div>
-        </div>
-        <div className='bg-gray-800 min-w-[12rem] max-w-[12rem] py-2'></div>
-      </main>
-      <footer className='px-2 py-1 bg-gray-800 text-xs'>{(new Date).getFullYear()}</footer>
+      <Header></Header>
+      <Main>
+        <IDE></IDE>
+      </Main>
+      <Footer></Footer>
     </>
   );
+}
+
+
+function Header() {
+  return (
+    <header className='min-h-[3rem] px-2 py-1 flex justify-between items-center border-b-2 border-gray-600'>
+      <LogoTobiko style={{ height: '32px' }} />
+      <div>
+        platform header
+      </div>
+    </header>
+  )
+}
+
+function Main({ children }: { children: React.ReactNode }) {
+  return (
+    <main className='font-sans w-full h-full flex flex-col overflow-hidden'>
+      {children}
+    </main>
+  )
+}
+
+function IDE() {
+  return (
+    <>
+      <div className='w-full border-b-2 border-gray-600 flex justify-between min-h-[2rem]'>
+        <div className='px-4'>Wursthall</div>
+        <div className='px-4 flex'>
+          <div className='px-4'>TobikoData/wursthall</div>
+          <div className='px-4'>main</div>
+        </div>
+        <div className='px-4 w-full'>search</div>
+        <div className='px-4 whitespace-nowrap'>
+          <small>Envoirment: development</small>
+        </div>
+        <div className='px-4 flex items-center'>
+          <div className='bg-gray-800 w-7 h-6 rounded flex justify-center items-center mr-2'>
+            <PlayIcon className='w-4 h-4 text-primary-300' />
+          </div>
+          <div className='bg-gray-800 w-7 h-6 rounded flex justify-center items-center mr-2'>
+            <PauseIcon className='w-4 h-4 text-primary-300' />
+          </div>          
+        </div>
+        <div className='px-4'>users</div>
+      </div>
+      <div className='flex w-full h-full overflow-hidden'>
+        <div className='min-w-[12rem] w-full max-w-[16rem] overflow-hidden overflow-y-auto border-r-2 border-gray-600'>
+          <FolderTree />
+        </div>
+        <div className='h-full w-full flex flex-col overflow-hidden'>
+
+          <div className='w-full overflow-hidden px-2 border-b-2 border-gray-600 min-h-[1.5rem] flex items-center'>
+            <small className='inline-block cursor-pointer hover:text-gray-300'>audit</small>
+            <small className='inline-block px-2'>/</small>
+            <small className='inline-block cursor-pointer hover:text-gray-300'>items.sql</small>
+          </div>
+
+          <div className='min-w-[20rem] w-full h-full flex overflow-hidden border-b-2 border-gray-600'>
+            <div className='w-full h-full flex flex-col border-r-2 border-gray-600'>
+              <div className=' bg-gray-800 text-center flex'>
+                <div className='p-1 min-w-[10rem] bg-gray-900 '>
+                  Tab 1
+                </div>
+                <div className='p-1 min-w-[10rem] bg-gray-700 border-b-2 border-gray-600'>
+                  Inactive
+                </div>                
+                <div className='p-1 border-b-2 border-gray-600 w-full'>
+                  ide editor tabs
+                </div>
+              </div>
+              <div className='w-full h-full flex'>
+                <div className='h-full w-full p-2'>ide editor</div>
+              </div>
+              <div className='px-2 py-1 border-t-2 border-gray-600 flex justify-between'>
+                <small>
+                  ide editor footer
+                </small>
+                <small>
+                  Total char count: 0
+                </small>
+              </div>
+            </div>
+            <div className='h-full min-w-[15%] w-full max-w-[25%] p-2'>Inspector</div>
+          </div>
+          <div className='w-full min-h-[10rem]'>
+            <div className=' bg-gray-800 text-center flex'>
+              <div className='p-1 min-w-[10rem] bg-gray-900 '>
+                Table
+              </div>
+              <div className='p-1 min-w-[10rem] bg-gray-700 border-b-2 border-gray-600'>
+                DAG
+              </div>                
+              <div className='p-1 border-b-2 border-gray-600 w-full'>
+                ide preview tabs
+              </div>
+            </div>
+            <div>ide preview</div>
+          </div>
+        </div>
+      </div>
+      <div className='p-1 border-t-2 border-gray-600'>ide footer</div>
+    </>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className='px-2 py-1 text-xs flex justify-between border-t-2 border-gray-600'>
+      <small>        
+        {(new Date).getFullYear()}
+      </small>
+      platform footer
+    </footer>
+  )
 }
