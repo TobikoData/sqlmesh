637d1b7d257f710df07300e4bab6da80b9e69e7c
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-01-17 12:56:30 -0800

    fix test

--- tests/core/engine_adapter/test_base_spark.py
+++ tests/core/engine_adapter/test_base_spark.py
@@ -11,7 +11,7 @@ def test_replace_query(mocker: MockerFixture):
     cursor_mock = mocker.Mock()
     connection_mock.cursor.return_value = cursor_mock
 
-    adapter = BaseSparkEngineAdapter(lambda: connection_mock)
+    adapter = BaseSparkEngineAdapter(lambda: connection_mock, "spark")
     adapter.replace_query("test_table", parse_one("SELECT a FROM tbl"), {"a": "int"})
 
     cursor_mock.execute.assert_called_once_with(
@@ -24,7 +24,7 @@ def test_replace_query_pandas(mocker: MockerFixture):
     cursor_mock = mocker.Mock()
     connection_mock.cursor.return_value = cursor_mock
 
-    adapter = BaseSparkEngineAdapter(lambda: connection_mock)
+    adapter = BaseSparkEngineAdapter(lambda: connection_mock, "spark")
     df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     adapter.replace_query("test_table", df, {"a": "int", "b": "int"})
 
