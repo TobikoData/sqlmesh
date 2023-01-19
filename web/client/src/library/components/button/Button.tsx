dd559240293bed647436ad50c8a7104cc6004600
dgtlspc <mykalmax@yahoo.com>
2023-01-19 10:37:42 -0800

    provide some types to Button

--- web/client/src/library/components/button/Button.tsx
+++ web/client/src/library/components/button/Button.tsx
@@ -1,7 +1,8 @@
 import clsx from "clsx"
+import { EnumSize } from "../../../types/enum";
 
 export type ButtonVariant = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'alternative';
-export type ButtonSize = 'small' | 'medium' | 'large';
+export type ButtonSize = Subset<Size, typeof EnumSize.sm | typeof EnumSize.md | typeof EnumSize.lg>;
 export type ButtonShape = "square" | "rounded" | "circle" | "pill";
 
 export interface PropsButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -12,7 +13,7 @@ export interface PropsButton extends React.ButtonHTMLAttributes<HTMLButtonElemen
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const VARIANT = new Map([
+const VARIANT = new Map<ButtonVariant, string>([
   ['primary', 'bg-primary-500 hover:bg-primary-600 active:bg-primary-400 text-gray-100'],
   ['secondary', 'bg-secondary-500 hover:bg-secondary-600 active:bg-secondary-400 text-gray-100'],
   ['success', 'bg-success-500 hover:bg-success-600 active:bg-success-400 text-gray-100'],
@@ -21,17 +22,17 @@ const VARIANT = new Map([
   ['alternative', 'bg-gray-500 hover:bg-gray-600 active:bg-gray-400 text-gray-100'],
 ]);
 
-const SHAPE = new Map([
+const SHAPE = new Map<ButtonShape, string>([
   ['rounded', `rounded-md`],
 ]);
 
-const SIZE = new Map([
-  ['small', `px-2 py-1 text-sm`],
-  ['medium', `px-3 py-2 text-base`],
-  ['large', `px-4 py-3 text-lg`],
+const SIZE = new Map<ButtonSize, string>([
+  [EnumSize.sm, `px-2 py-1 text-xs`],
+  [EnumSize.md, `px-3 py-2 text-base`],
+  [EnumSize.lg, `px-4 py-3 text-lg`],
 ]);
 
-export function Button({ variant = 'secondary', shape = 'rounded', size = 'medium', children = [], onClick }: PropsButton) {
+export function Button({ variant = 'secondary', shape = 'rounded', size = EnumSize.md, children = [], onClick }: PropsButton) {
   return (
     <button 
       className={clsx(
