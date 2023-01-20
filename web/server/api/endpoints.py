4fda0d1ba178a026349770ee1e0b68a0287e3e5f
Vincent Chan <vchan@users.noreply.github.com>
2023-01-19 17:18:13 -0800

    Add file API CRUD endpoints (#210)
    * Add file API CRUD endpoints
    
    * PR feedback
    
    * Respect ignore_patterns
    
    * PR feedback

--- web/server/api/endpoints.py
+++ web/server/api/endpoints.py
@@ -1,12 +1,28 @@
+from __future__ import annotations
+
 import os
 import typing as t
+from pathlib import Path
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends, HTTPException, Response, status
 
+from sqlmesh.core.context import Context
 from web.server.models import Directory, File
 from web.server.settings import Settings, get_settings
 
 router = APIRouter()
+
+
+def _validate_path(path: str, context: Context) -> None:
+    resolved_context_path = context.path.resolve()
+    full_path = (resolved_context_path / path).resolve()
+    try:
+        full_path.relative_to(resolved_context_path)
+    except ValueError:
+        raise HTTPException(status_code=404)
+
+    if any(full_path.match(pattern) for pattern in context._ignore_patterns):
+        raise HTTPException(status_code=404)
 
 
 @router.get("/files")
@@ -15,12 +31,20 @@ def get_files(
 ) -> Directory:
     """Get all project files."""
 
-    def walk_path(path: str) -> t.Tuple[t.List[Directory], t.List[File]]:
+    def walk_path(path: str | Path) -> t.Tuple[t.List[Directory], t.List[File]]:
         directories = []
         files = []
         with os.scandir(path) as entries:
             for entry in entries:
-                if entry.name == "__pycache__" or entry.name.startswith("."):
+                entry_path = Path(entry.path)
+                if (
+                    entry.name == "__pycache__"
+                    or entry.name.startswith(".")
+                    or any(
+                        entry_path.match(pattern)
+                        for pattern in settings.context._ignore_patterns
+                    )
+                ):
                     continue
 
                 relative_path = os.path.relpath(entry.path, settings.project_path)
@@ -47,3 +71,46 @@ def get_files(
         directories=directories,
         files=files,
     )
+
+
+@router.get("/files/{path:path}")
+def get_file(
+    path: str,
+    settings: Settings = Depends(get_settings),
+) -> File:
+    """Get a file, including its contents."""
+    _validate_path(path, settings.context)
+    try:
+        with open(settings.project_path / path) as f:
+            content = f.read()
+    except FileNotFoundError:
+        raise HTTPException(status_code=404)
+    return File(name=os.path.basename(path), path=path, content=content)
+
+
+@router.post("/files/{path:path}")
+async def write_file(
+    path: str,
+    content: str = Body(),
+    settings: Settings = Depends(get_settings),
+) -> File:
+    """Create or update a file."""
+    _validate_path(path, settings.context)
+    with open(settings.project_path / path, "w", encoding="utf-8") as f:
+        f.write(content)
+    return File(name=os.path.basename(path), path=path, content=content)
+
+
+@router.delete("/files/{path:path}")
+async def delete_file(
+    path: str,
+    response: Response,
+    settings: Settings = Depends(get_settings),
+) -> None:
+    """Delete a file."""
+    _validate_path(path, settings.context)
+    try:
+        (settings.project_path / path).unlink()
+        response.status_code = status.HTTP_204_NO_CONTENT
+    except FileNotFoundError:
+        raise HTTPException(status_code=404)
--- web/server/tests/test_main.py
+++ web/server/tests/test_main.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import pytest
 from fastapi.testclient import TestClient
 
 from web.server.main import app
@@ -6,13 +9,133 @@ from web.server.settings import Settings, get_settings
 client = TestClient(app)
 
 
-def get_settings_override() -> Settings:
-    return Settings(project_path="examples/sushi")
+@pytest.fixture
+def project_tmp_path(tmp_path: Path) -> Path:
+    def get_settings_override() -> Settings:
+        return Settings(project_path=tmp_path)
+
+    config = tmp_path / "config.py"
+    config.write_text(
+        """from sqlmesh.core.config import Config
+config = Config()
+    """
+    )
+
+    app.dependency_overrides[get_settings] = get_settings_override
+    return tmp_path
 
 
-app.dependency_overrides[get_settings] = get_settings_override
+def test_get_files(project_tmp_path: Path) -> None:
+    models_dir = project_tmp_path / "models"
+    models_dir.mkdir()
+    sql_file = models_dir / "foo.sql"
+    sql_file.write_text("MODEL (name foo); SELECT ds;")
 
-
-def test_get_files() -> None:
     response = client.get("/api/files")
     assert response.status_code == 200
+    assert response.json() == {
+        "name": project_tmp_path.name,
+        "path": "",
+        "directories": [
+            {
+                "name": "models",
+                "path": "models",
+                "directories": [],
+                "files": [
+                    {
+                        "name": "foo.sql",
+                        "path": "models/foo.sql",
+                        "extension": ".sql",
+                        "is_supported": True,
+                        "content": None,
+                    }
+                ],
+            }
+        ],
+        "files": [
+            {
+                "name": "config.py",
+                "path": "config.py",
+                "extension": ".py",
+                "is_supported": True,
+                "content": None,
+            }
+        ],
+    }
+
+
+def test_get_file(project_tmp_path: Path) -> None:
+    txt_file = project_tmp_path / "foo.txt"
+    txt_file.write_text("bar")
+
+    response = client.get("/api/files/foo.txt")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "foo.txt",
+        "path": "foo.txt",
+        "extension": ".txt",
+        "is_supported": False,
+        "content": "bar",
+    }
+
+
+def test_get_file_not_found() -> None:
+    response = client.get("/api/files/not_found.txt")
+    assert response.status_code == 404
+
+
+def test_get_file_invalid_path(project_tmp_path: Path) -> None:
+    config = project_tmp_path / "config.py"
+    config.write_text(
+        """from sqlmesh.core.config import Config
+config = Config(ignore_patterns=["*.txt"])
+    """
+    )
+    foo_txt = project_tmp_path / "foo.txt"
+    foo_txt.touch()
+
+    response = client.get("/api/files/foo.txt")
+    assert response.status_code == 404
+
+
+def test_write_file(project_tmp_path: Path) -> None:
+    response = client.post("/api/files/foo.txt", data='"bar"')
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "foo.txt",
+        "path": "foo.txt",
+        "extension": ".txt",
+        "is_supported": False,
+        "content": "bar",
+    }
+    assert (project_tmp_path / "foo.txt").read_text() == "bar"
+
+
+def test_update_file(project_tmp_path: Path) -> None:
+    txt_file = project_tmp_path / "foo.txt"
+    txt_file.write_text("bar")
+
+    response = client.post("/api/files/foo.txt", data='"baz"')
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "foo.txt",
+        "path": "foo.txt",
+        "extension": ".txt",
+        "is_supported": False,
+        "content": "baz",
+    }
+    assert (project_tmp_path / "foo.txt").read_text() == "baz"
+
+
+def test_delete_file(project_tmp_path: Path) -> None:
+    txt_file = project_tmp_path / "foo.txt"
+    txt_file.write_text("bar")
+
+    response = client.delete("/api/files/foo.txt")
+    assert response.status_code == 204
+    assert not txt_file.exists()
+
+
+def test_delete_file_not_found() -> None:
+    response = client.delete("/api/files/not_found.txt")
+    assert response.status_code == 404
