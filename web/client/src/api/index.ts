a6856ad84c9fd330f5a3e238584207746076abaf
Max Mykal <mykalmax@yahoo.com>
2023-01-23 18:35:47 -0800

    add plan component

--- web/client/src/api/index.ts
+++ web/client/src/api/index.ts
@@ -1,20 +1,26 @@
-import { useQuery, useMutation, QueryClient } from "@tanstack/react-query";
-import { getFileByPath, getFiles, saveFileByPath } from "./endpoints";
-
+import { useQuery, useMutation, QueryClient } from '@tanstack/react-query'
+import { getFileByPath, getFiles, getPlan, saveFileByPath } from './endpoints'
 
 export function useApiFileByPath(path?: string) {
   return useQuery({
     queryKey: [`/api/files`, path],
     queryFn: path ? () => getFileByPath(path) : undefined,
     enabled: !!path,
-  });
+  })
 }
 
 export function useApiFiles() {
   return useQuery({
-    queryKey: ["/api/files"],
+    queryKey: ['/api/files'],
     queryFn: getFiles,
-  });
+  })
+}
+
+export function useApiPlan() {
+  return useQuery({
+    queryKey: ['/api/plan'],
+    queryFn: getPlan,
+  })
 }
 
 export function useMutationApiSaveFile(client: QueryClient) {
@@ -22,6 +28,6 @@ export function useMutationApiSaveFile(client: QueryClient) {
     mutationFn: saveFileByPath,
     onMutate: async ({ path }: any) => {
       await client.cancelQueries({ queryKey: [`/api/files`, path] })
-    }
+    },
   })
 }
