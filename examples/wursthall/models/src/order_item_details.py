3535fe62fe84630af57b4ca46070e47c2c44cb12
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-01-24 14:52:11 -0800

    fix imports and add faker dependency

--- examples/wursthall/models/src/order_item_details.py
+++ examples/wursthall/models/src/order_item_details.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from faker import Faker
 
-from models.src.shared import DATA_START_DATE_STR, iter_dates, set_seed
+from examples.wursthall.models.src.shared import DATA_START_DATE_STR, iter_dates, set_seed
 from sqlmesh import ExecutionContext, model
 from sqlmesh.core.model import IncrementalByTimeRangeKind, TimeColumn
 from sqlmesh.utils.date import to_ds
