8d0233b81764d9df27a0827fb989e17551c77a00
Max Mykal <mykalmax@yahoo.com>
2023-01-24 17:06:53 -0800

    handle context API endpoint on UI

--- web/client/src/api/endpoints.ts
+++ web/client/src/api/endpoints.ts
@@ -4,17 +4,23 @@ export type File = {
   is_supported: boolean
   content: string
   extension: string
-};
+}
 
 export type Directory = {
   name: string
   path: string
   directories: Directory[]
   files: File[]
-};
+}
+
+export type Plan = {}
 
 export async function getFiles() {
-  return await fetchApi<{ name: string, directories: Directory[], files: File[] }>("files")
+  return await fetchApi<{
+    name: string
+    directories: Directory[]
+    files: File[]
+  }>('files')
 }
 
 export async function getFileByPath(path: string) {
@@ -22,20 +28,47 @@ export async function getFileByPath(path: string) {
 }
 
 export async function saveFileByPath({ path, body = '' }: any) {
-   return await fetchApi<File>(`files/${path}`, { method: 'post', body })
+  return await fetchApi<File>(`files/${path}`, { method: 'post', body })
 }
 
-type FetchResponse<T> =  Promise<T | null> 
+export async function getContext() {
+  return await fetchApi<{
+    models: Array<string>
+    engine_adapter: string
+    path: string
+    dialect: string
+    scheduler: string
+    time_column_format: string
+    concurrent_tasks: number
+  }>('context')
+}
 
-async function fetchApi<T = unknown>(path: Path, options: RequestInit = { method: 'GET' }): FetchResponse<T> {
+export async function getContextByEnvironment(environment: string = '') {
+  return await fetchApi<{
+    environment: string
+    backfills: Array<[string, string]>,
+    changes: {
+      added: Array<string>,
+      removed: Array<string>,
+      modified: Array<string>,
+    }
+  }>(`context/${environment}`)
+}
+
+type FetchResponse<T> = Promise<T | null>
+
+async function fetchApi<T = unknown>(
+  path: Path,
+  options: RequestInit = { method: 'GET' }
+): FetchResponse<T> {
   const host = window.location.origin
   // Ensure that the final string does not contain any unnecessary or duplicate slashes
-  const distanation = `/api/${path}`.replace(/([^:]\/)\/+/g, "$1") 
+  const distanation = `/api/${path}`.replace(/([^:]\/)\/+/g, '$1')
   const url = new URL(distanation, host)
 
   try {
     const response = await fetch(url, options)
-    
+
     if (response.ok) {
       return await response.json()
     }
