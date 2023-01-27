d42f96893f5aeb2f0f7ac62945f3a1b1237ed3fe
Chris Rericha <tristipher@gmail.com>
2023-01-26 19:03:38 -0500

    Make sushi_dbt not have source dependencies and store profile target sensitive data in environment vars

---
+++ examples/sushi_dbt_with_sources/config.py
@@ -0,0 +1,3 @@
+from sqlmesh.dbt.loader import sqlmesh_config
+
+config = sqlmesh_config()
--- examples/sushi_dbt_with_sources/models/cleansed/waiters.sql
+++ examples/sushi_dbt_with_sources/models/cleansed/waiters.sql
@@ -2,18 +2,15 @@
     config(
         materialized='incremental',
         incremental_strategy='delete+insert',
-        cluster_by=['ds'],
         time_column='ds',
+        cluster_by=['ds'],
     )
 }}
 
-SELECT
-  id::INT AS id, /* Primary key */
-  order_id::INT AS order_id, /* Order id */
-  item_id::INT AS item_id, /* Item id */
-  quantity::INT AS quantity, /* Quantity of items ordered */
-  ds::TEXT AS ds /* Date of order */
-FROM {{ ref('raw_order_items') }}
+SELECT DISTINCT
+  waiter_id::INT AS waiter_id,
+  ds::TEXT AS ds
+FROM {{ source('raw', 'orders') }}
 {% if is_incremental() %}
 WHERE
   ds > (select max(ds) from {{ this }})
