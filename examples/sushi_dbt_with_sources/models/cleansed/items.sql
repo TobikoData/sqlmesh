ce462c99f12437358bba7b8a160133859f88499a
Chris Rericha <67359577+crericha@users.noreply.github.com>
2023-01-27 12:21:57 -0500

    Remove sushi_dbt source dependencies and use env vars for sensitive profile data (#257)
    * Make sushi_dbt not have source dependencies and store profile target sensitive data in environment vars
    
    * Add __init__.py to all example projects for mypy

--- examples/sushi_dbt_with_sources/models/cleansed/items.sql
+++ examples/sushi_dbt_with_sources/models/cleansed/items.sql
@@ -8,13 +8,11 @@
 }}
 
 SELECT
-  id::INT AS id, /* Primary key */
-  customer_id::INT AS customer_id, /* Id of customer who made the order */
-  waiter_id::INT AS waiter_id, /* Id of waiter who took the order */
-  start_ts::TEXT AS start_ts, /* Start timestamp */
-  end_ts::TEXT AS end_ts, /* End timestamp */
-  ds::TEXT AS ds /* Date of order */
-FROM {{ ref('raw_orders') }}
+  id::DOUBLE AS id, /* Primary key */
+  name::TEXT AS name, /* Name of the sushi */
+  price::DOUBLE AS price, /* Price of the sushi */
+  ds::TEXT AS ds /* Date */
+FROM {{ ref('raw_items') }}
 {% if is_incremental() %}
 WHERE
   ds > (select max(ds) from {{ this }})
