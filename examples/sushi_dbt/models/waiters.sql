bb0a716beca6428f4ba71ef7a4f21a0f426da538
Chris Rericha <tristipher@gmail.com>
2023-01-27 20:15:51 -0500

    Update sushi_dbt example to match sushi and add duckdb targets

--- examples/sushi_dbt/models/waiters.sql
+++ examples/sushi_dbt/models/waiters.sql
@@ -1,16 +1,13 @@
 {{
     config(
-        materialized='incremental',
-        incremental_strategy='delete+insert',
-        time_column='ds',
-        cluster_by=['ds'],
+        materialized='ephemeral',
     )
 }}
 
 SELECT DISTINCT
   waiter_id::INT AS waiter_id,
   ds::TEXT AS ds
-FROM {{ ref('raw_orders') }}
+FROM {{ ref('orders') }}
 {% if is_incremental() %}
 WHERE
   ds > (select max(ds) from {{ this }})
