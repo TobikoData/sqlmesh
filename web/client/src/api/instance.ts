c40cd8b2b56094613f49305cd4210df24e93503b
Max Mykal <mykalmax@yahoo.com>
2023-01-30 16:16:42 -0800

    create search params using web api

--- web/client/src/api/instance.ts
+++ web/client/src/api/instance.ts
@@ -1,41 +1,50 @@
-const baseURL = window.location.origin
+const baseURL = window.location.origin;
 
 export const fetchAPI = async <T>({
-  url,
-  method,
-  params,
-  data,
-  headers,
-  credentials,
-  mode,
-  cache,
+	url,
+	method,
+	params,
+	data,
+	headers,
+	credentials,
+	mode,
+	cache,
 }: {
-  url: string;
-  method: 'get' | 'post' | 'put' | 'delete' | 'patch';
-  data?: BodyInit;
-  responseType?: string;
-  headers?: { [key: string]: string };
-  credentials?: 'omit' | 'same-origin' | 'include';
-  mode?: 'cors' | 'no-cors' | 'same-origin';
-  cache?: 'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached';
-  params?: any;
+	url: string;
+	method: "get" | "post" | "put" | "delete" | "patch";
+	data?: BodyInit;
+	responseType?: string;
+	headers?: { [key: string]: string };
+	credentials?: "omit" | "same-origin" | "include";
+	mode?: "cors" | "no-cors" | "same-origin";
+	cache?:
+		| "default"
+		| "no-store"
+		| "reload"
+		| "no-cache"
+		| "force-cache"
+		| "only-if-cached";
+	params?: any;
 }): Promise<T> => {
-  const withSearchParams = Object.keys(params || {}).length > 0
-  const fullUrl = `${url}${withSearchParams ? '?' + new URLSearchParams(params) : ''}`.replace(/([^:]\/)\/+/g, '$1')
-  const input = new URL(fullUrl, baseURL)
+	const withSearchParams =
+		Object.keys(params || {}).length > 0;
+	const fullUrl = url.replace(/([^:]\/)\/+/g, "$1");
+	const input = new URL(fullUrl, baseURL);
 
-  const response = await fetch(input,
-    {
-      method,
-      headers,
-      credentials,
-      mode,
-      cache,
-      ...(data ? { body: JSON.stringify(data) } : {}),
-    },
-  );
+	if (withSearchParams) {
+		input.search = new URLSearchParams(params).toString();
+	}
 
-  return response.json();
+	const response = await fetch(input, {
+		method,
+		headers,
+		credentials,
+		mode,
+		cache,
+		...(data ? { body: JSON.stringify(data) } : {}),
+	});
+
+	return response.json();
 };
 
 export default fetchAPI;
