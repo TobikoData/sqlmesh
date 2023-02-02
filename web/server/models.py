83a78accb2ce32577bd9adf59890b79997cf6774
Max Mykal <mykalmax@yahoo.com>
2023-02-02 12:22:01 -0800

    move get_modified_snapshots to ModelsDiff class

--- web/server/models.py
+++ web/server/models.py
@@ -5,6 +5,7 @@ import typing as t
 
 from pydantic import validator
 
+from sqlmesh.core.context_diff import ContextDiff
 from sqlmesh.utils.pydantic import PydanticModel
 
 SUPPORTED_EXTENSIONS = {".py", ".sql", ".yaml"}
@@ -47,9 +48,39 @@ class Context(PydanticModel):
 
 
 class ModelsDiff(PydanticModel):
-    direct: t.Dict[str, str]
+    direct: t.List[t.Dict[str, str]]
     indirect: t.Set[str]
     metadata: t.Set[str]
+
+    @classmethod
+    def get_modified_snapshots(
+        cls,
+        context_diff: ContextDiff,
+    ) -> ModelsDiff:
+        """Get the modified snapshots for a environment."""
+
+        direct = []
+        indirect = set()
+        metadata = set()
+
+        for snapshot_name in context_diff.snapshots.keys():
+            if context_diff.directly_modified(snapshot_name):
+                direct.append(
+                    {
+                        "model_name": snapshot_name,
+                        "diff": context_diff.text_diff(snapshot_name),
+                    }
+                )
+            elif context_diff.indirectly_modified(snapshot_name):
+                indirect.add(snapshot_name)
+            elif context_diff.metadata_updated(snapshot_name):
+                metadata.add(snapshot_name)
+
+        return ModelsDiff(
+            direct=direct,
+            indirect=indirect,
+            metadata=metadata,
+        )
 
 
 class ContextEnvironmentChanges(PydanticModel):
