d9d192afefe8e2f7e7fc4d90f1c6f94dc2727191
Max Mykal <mykalmax@yahoo.com>
2023-02-02 12:23:18 -0800

    set progress bar to accept classes

--- web/client/src/library/components/progress/Progress.tsx
+++ web/client/src/library/components/progress/Progress.tsx
@@ -1,6 +1,8 @@
-export function Progress({ progress = 0, delay = 150, duration = 500 }: any) {
+import clsx from "clsx";
+
+export function Progress({ progress = 0, delay = 150, duration = 500, className }: any) {
   return (
-    <div className="w-full h-1 bg-gray-100 overflow-hidden flex items-center rounded-lg my-1">
+    <div className={clsx('w-full h-1 bg-gray-100 overflow-hidden flex items-center rounded-lg my-1', className)}>
       <div
         className={`transition-[width] h-full bg-success-500 rounded-lg`}
         style={{
