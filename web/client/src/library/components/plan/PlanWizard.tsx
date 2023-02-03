a749836dd04689f43e1e02bcbd8c89cb2c13e540
Max Mykal <mykalmax@yahoo.com>
2023-02-02 17:23:31 -0800

    set activePlan to control tasks progress

--- web/client/src/library/components/plan/PlanWizard.tsx
+++ web/client/src/library/components/plan/PlanWizard.tsx
@@ -23,7 +23,7 @@ export function PlanWizard({
   const category = useStorePlan((s: any) => s.category)
   const categories = useStorePlan((s: any) => s.categories)
   const setWithBackfill = useStorePlan((s: any) => s.setWithBackfill)
-  const tasks = useStorePlan((s: any) => s.tasks)
+  const activePlan = useStorePlan((s: any) => s.activePlan)
 
   const { data: context } = useApiContextByEnvironment(environment)
 
@@ -245,10 +245,10 @@ export function PlanWizard({
                           )}>{model_name}</p>
                           <small className="inline-block pl-3 text-xs text-gray-900">{interval[0]} - {interval[1]}</small>
                         </div>
-                        <small className="inline-block text-xs">{tasks?.[model_name]?.completed ?? 0} / {batches} batch{batches > 1 ? 'es' : ''}</small>
+                        <small className="inline-block text-xs">{activePlan?.tasks?.[model_name]?.completed ?? 0} / {batches} batch{batches > 1 ? 'es' : ''}</small>
                       </div>
                       <Progress
-                        progress={Math.ceil(tasks?.[model_name]?.completed / tasks?.[model_name]?.total * 100)}
+                        progress={Math.ceil(activePlan?.tasks?.[model_name]?.completed / activePlan?.tasks?.[model_name]?.total * 100)}
                       />
                     </li>
                   ))}
