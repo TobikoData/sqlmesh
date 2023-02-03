cb999cba6c444b095b7f539b8dd389c1dda272a3
Max Mykal <mykalmax@yahoo.com>
2023-02-02 17:23:03 -0800

    remove temp data

--- web/client/src/context/plan.ts
+++ web/client/src/context/plan.ts
@@ -22,7 +22,8 @@ export type PlanState = typeof EnumPlanState[keyof typeof EnumPlanState]
 export const useStorePlan = create((set) => ({
   state: EnumPlanState.Init,
   action: EnumPlanState.None,
-  activePlan: getPlan(),
+  activePlan: null,
+  setActivePlan: (activePlan: any) => set(() => ({ activePlan })),
   setNewPlan: (newPlan: any) => set(() => ({ newPlan })),
   setPlan: (plan: any) => set(() => ({ plan })),
   setState: (state: string) => set(() => ({ state })),
@@ -56,50 +57,4 @@ export const useStorePlan = create((set) => ({
   setWithBackfill: (withBackfill: boolean) => set(() => ({ withBackfill })),
   backfills: [],
   setBackfills: (backfills: any) => set(() => ({ backfills })),
-  tasks: {},
-  setTasks: (tasks: any) => set(() => ({ tasks })),
 }))
-
-function getPlan() {
-  return {
-    environment: "prod",
-    tasks: {
-      "sushi.customer_revenue_by_day": {
-        "completed": 7,
-        "total": 40
-      },
-      "sushi.customers": {
-        "completed": 0,
-        "total": 1
-      },
-      "sushi.top_waiters": {
-        "completed": 1,
-        "total": 1
-      },
-      "sushi.waiter_as_customer_by_day": {
-        "completed": 1,
-        "total": 1
-      },
-      "sushi.waiter_names": {
-        "completed": 1,
-        "total": 1
-      },
-      "sushi.waiter_revenue_by_day": {
-        "completed": 7,
-        "total": 40
-      },
-      "sushi.items": {
-        "completed": 7,
-        "total": 14
-      },
-      "sushi.order_items": {
-        "completed": 7,
-        "total": 14
-      },
-      "sushi.orders": {
-        "completed": 7,
-        "total": 14
-      }
-    }
-  }
-}
