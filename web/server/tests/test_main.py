30ca025e89e1c8fd02efd7f337cb7279f70f0091
Vincent Chan <vchan@users.noreply.github.com>
2023-02-03 16:14:08 -0800

    Add cancel API endpoint (#295)
    * Add cancel API endpoint
    
    * PR feedback

--- web/server/tests/test_main.py
+++ web/server/tests/test_main.py
@@ -1,3 +1,4 @@
+import asyncio
 from pathlib import Path
 
 import pytest
@@ -197,3 +198,18 @@ def test_apply() -> None:
 def test_tasks() -> None:
     response = client.get("/api/tasks")
     assert response.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_cancel() -> None:
+    app.state.task = asyncio.create_task(asyncio.sleep(1))
+    response = client.post("/api/plan/cancel")
+    await asyncio.sleep(0.1)
+    assert response.status_code == 204
+    assert app.state.task.cancelled()
+
+
+def test_cancel_no_task() -> None:
+    response = client.post("/api/plan/cancel")
+    assert response.status_code == 422
+    assert response.json() == {"detail": "No active task found."}
