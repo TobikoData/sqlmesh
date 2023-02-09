5c0c3751057193f833b13596e06f9cd81aae541e
Max Mykal <mykalmax@yahoo.com>
2023-02-09 14:57:58 -0800

    Add Split Pane component (#342)

--- web/client/src/library/components/splitPane/SplitPane.tsx
+++ web/client/src/library/components/splitPane/SplitPane.tsx
@@ -1,60 +1,18 @@
-import React, {
-  MouseEventHandler,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import SplitPaneContext from "../../../context/SplitPaneContext";
+import Split, { SplitProps } from 'react-split'
+import './SplitPane.css'
 
-export function SplitPane({ children, ...props }: { children: React.ReactNode }) {
-  const [clientHeight, setClientHeight] = useState(0)
-  const [clientWidth, setClientWidth] = useState(0)
-  const yDividerPos = useRef(0)
-  const xDividerPos = useRef(0)
-
-  const onMouseDown: MouseEventHandler = (e: React.MouseEvent) => {
-    yDividerPos.current = e.clientY
-    xDividerPos.current = e.clientX
-  };
-
-  const onMouseUp = () => {
-    yDividerPos.current = 0
-    xDividerPos.current = 0
-  };
-
-  const onMouseMove = (e: MouseEvent) => {
-    if (!yDividerPos.current && !xDividerPos.current) return
-
-    setClientHeight(clientHeight + e.clientY - yDividerPos.current)
-    setClientWidth(clientWidth + e.clientX - xDividerPos.current)
-
-    yDividerPos.current = e.clientY;
-    xDividerPos.current = e.clientX;
-  }
-
-  useEffect(() => {
-    document.addEventListener("mouseup", onMouseUp);
-    document.addEventListener("mousemove", onMouseMove);
-
-    return () => {
-      document.removeEventListener("mouseup", onMouseUp);
-      document.removeEventListener("mousemove", onMouseMove);
-    };
-  });
+export function SplitPane({ className, children, sizes, direction }: SplitProps) {
 
   return (
-    <div {...props}>
-      <SplitPaneContext.Provider
-        value={{
-          clientHeight,
-          setClientHeight,
-          clientWidth,
-          setClientWidth,
-          onMouseDown,
-        }}
-      >
-        {children}
-      </SplitPaneContext.Provider>
-    </div>
+    <Split
+      className={className}
+      sizes={sizes}
+      expandToMin={false}
+      gutterAlign="center"
+      gutterSize={5}
+      direction={direction}
+    >
+      {children}
+    </Split>
   );
 };
