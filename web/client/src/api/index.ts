a99b3dc31f0be37d3aec859bb035e4d2d0b67537
Max Mykal <mykalmax@yahoo.com>
2023-02-10 18:09:33 -0800

    Fix file not getting save after a change (not sending api request) (#351)
    * add onSuccess and onMutate to better spit state change
    
    * add default value for `activeFile`

--- web/client/src/api/index.ts
+++ web/client/src/api/index.ts
@@ -1,52 +1,84 @@
-import { useQuery, useMutation, QueryClient } from '@tanstack/react-query'
-import { saveFileByPath } from './endpoints'
-import { getFileApiFilesPathGet, getFilesApiFilesGet, getPlanApiPlanGet, getApiContextApiContextGet } from "./client";
+import {
+	useQuery,
+	useMutation,
+	QueryClient,
+} from "@tanstack/react-query";
+import { saveFileByPath } from "./endpoints";
+import {
+	getFileApiFilesPathGet,
+	getFilesApiFilesGet,
+	getPlanApiPlanGet,
+	getApiContextApiContextGet,
+} from "./client";
 
 export function useApiFileByPath(path?: string) {
-  return useQuery({
-    queryKey: [`/api/files`, path],
-    queryFn: path ? () => getFileApiFilesPathGet(path) : undefined,
-    enabled: !!path,
-    cacheTime: 0
-  })
+	return useQuery({
+		queryKey: [`/api/files`, path],
+		queryFn: path
+			? () => getFileApiFilesPathGet(path)
+			: undefined,
+		enabled: !!path,
+		cacheTime: 0,
+	});
 }
 
 export function useApiFiles() {
-  return useQuery({
-    queryKey: ['/api/files'],
-    queryFn: getFilesApiFilesGet,
-    cacheTime: 0
-  })
+	return useQuery({
+		queryKey: ["/api/files"],
+		queryFn: getFilesApiFilesGet,
+		cacheTime: 0,
+	});
 }
 
 export function useApiContext() {
-  return useQuery({
-    queryKey: ['/api/context'],
-    queryFn: getApiContextApiContextGet,
-    cacheTime: 0
-  })
+	return useQuery({
+		queryKey: ["/api/context"],
+		queryFn: getApiContextApiContextGet,
+		cacheTime: 0,
+	});
 }
 
-export async function useApiContextCancel(client: QueryClient) {
-  await client.cancelQueries({ queryKey: [`/api/context`] })
+export async function useApiContextCancel(
+	client: QueryClient
+) {
+	await client.cancelQueries({
+		queryKey: [`/api/context`],
+	});
 }
 
 export function useApiContextByEnvironment(value?: string) {
-  const environment = value || ''
+	const environment = value || "";
 
-  return useQuery({
-    queryKey: [`/api/plan`, environment],
-    queryFn: () => getPlanApiPlanGet({ environment }),
-    enabled: false,
-    cacheTime: 0
-  })
+	return useQuery({
+		queryKey: [`/api/plan`, environment],
+		queryFn: () => getPlanApiPlanGet({ environment }),
+		enabled: false,
+		cacheTime: 0,
+	});
 }
 
-export function useMutationApiSaveFile(client: QueryClient) {
-  return useMutation({
-    mutationFn: saveFileByPath,
-    onMutate: async ({ path }: any) => {
-      await client.cancelQueries({ queryKey: [`/api/files`, path] })
-    },
-  })
+export function useMutationApiSaveFile(
+	client: QueryClient,
+	callbacks: {
+		onSuccess?: () => void;
+		onMutate?: () => void;
+	}
+) {
+	return useMutation({
+		mutationFn: saveFileByPath,
+		async onMutate({ path }) {
+			await client.cancelQueries({
+				queryKey: [`/api/files`, path],
+			});
+
+			callbacks.onMutate && callbacks.onMutate();
+		},
+		async onSuccess({ path }) {
+			await client.invalidateQueries({
+				queryKey: [`/api/files`, path],
+			});
+
+			callbacks.onSuccess && callbacks.onSuccess();
+		},
+	});
 }
