e305c203639c015d3af2f8f365a0d1f2d925776e
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-02-14 13:09:07 -0800

    Move transaction management into connection pool (#364)

--- tests/utils/test_connection_pool.py
+++ tests/utils/test_connection_pool.py
@@ -57,6 +57,39 @@ def test_singleton_connection_pool_close(mocker: MockerFixture):
     assert connection_factory_mock.call_count == 3
 
 
+def test_singleton_connection_pool_transaction(mocker: MockerFixture):
+    cursor_mock = mocker.Mock()
+    connection_mock = mocker.Mock()
+    connection_mock.cursor.return_value = cursor_mock
+    connection_factory_mock = mocker.Mock(return_value=connection_mock)
+
+    pool = SingletonConnectionPool(connection_factory_mock)
+
+    assert not pool.is_transaction_active
+
+    pool.begin()
+    assert pool.is_transaction_active
+
+    pool.commit()
+    assert not pool.is_transaction_active
+
+    pool.begin()
+    pool.rollback()
+    assert not pool.is_transaction_active
+
+    pool.begin()
+    pool.close()
+    assert not pool.is_transaction_active
+
+    pool.begin()
+    pool.close_all()
+    assert not pool.is_transaction_active
+
+    assert cursor_mock.begin.call_count == 4
+    assert cursor_mock.commit.call_count == 1
+    assert cursor_mock.rollback.call_count == 1
+
+
 def test_thread_local_connection_pool(mocker: MockerFixture):
     cursor_mock_thread_one = mocker.Mock()
     connection_mock_thread_one = mocker.Mock()
@@ -117,3 +150,60 @@ def test_thread_local_connection_pool(mocker: MockerFixture):
 
     assert connection_mock_thread_two.cursor.call_count == 1
     assert connection_mock_thread_two.close.call_count == 1
+
+
+def test_thread_local_connection_pool_transaction(mocker: MockerFixture):
+    cursor_mock_thread_one = mocker.Mock()
+    connection_mock_thread_one = mocker.Mock()
+    connection_mock_thread_one.cursor.return_value = cursor_mock_thread_one
+
+    cursor_mock_thread_two = mocker.Mock()
+    connection_mock_thread_two = mocker.Mock()
+    connection_mock_thread_two.cursor.return_value = cursor_mock_thread_two
+
+    test_thread_id = get_ident()
+
+    def connection_factory():
+        return (
+            connection_mock_thread_one
+            if get_ident() == test_thread_id
+            else connection_mock_thread_two
+        )
+
+    connection_factory_mock = mocker.Mock(side_effect=connection_factory)
+    pool = ThreadLocalConnectionPool(connection_factory_mock)
+
+    def thread():
+        pool.begin()
+        assert pool.is_transaction_active
+
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        executor.submit(thread).result()
+
+    pool.begin()
+    assert pool.is_transaction_active
+
+    assert len(pool._thread_transactions) == 2
+
+    pool.commit()
+    assert not pool.is_transaction_active
+    assert len(pool._thread_transactions) == 1
+
+    pool.begin()
+    pool.rollback()
+    assert not pool.is_transaction_active
+    assert len(pool._thread_transactions) == 1
+
+    pool.begin()
+    pool.close()
+    assert not pool.is_transaction_active
+    assert len(pool._thread_transactions) == 1
+
+    pool.close_all()
+    assert not pool._thread_transactions
+
+    assert cursor_mock_thread_one.begin.call_count == 3
+    assert cursor_mock_thread_one.commit.call_count == 1
+    assert cursor_mock_thread_one.rollback.call_count == 1
+
+    assert cursor_mock_thread_two.begin.call_count == 1
