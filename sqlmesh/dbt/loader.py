c9d75ce75fc77e76828defc7aa5d9a507a2f83a9
Chris Rericha <67359577+crericha@users.noreply.github.com>
2023-02-17 14:24:39 -0500

    Introduce DBT packages (#370)
    * Move model and seed loading to package loader
    
    * Minor dbt clean-up
    
    * Move dbt project/package macros loading to PackageLoader
    
    * Split package into its own module
    
    * Introduce DBT packages
    
    * Fix test
    
    * Integrate variables with package restructuring
    
    * Remove dbt READMEs
    
    * Fix package macros field construction
    
    * Remove unneccessary .gitkeep files

--- sqlmesh/dbt/loader.py
+++ sqlmesh/dbt/loader.py
@@ -11,9 +11,13 @@ from sqlmesh.core.loader import LoadedProject, Loader
 from sqlmesh.core.macros import MacroRegistry, macro
 from sqlmesh.core.model import Model
 from sqlmesh.dbt.common import Dependencies
-from sqlmesh.dbt.macros import builtin_methods
+from sqlmesh.dbt.macros import MacroConfig, builtin_methods
+from sqlmesh.dbt.model import ModelConfig
+from sqlmesh.dbt.package import Package
 from sqlmesh.dbt.profile import Profile
 from sqlmesh.dbt.project import Project
+from sqlmesh.dbt.seed import SeedConfig
+from sqlmesh.dbt.source import SourceConfig
 from sqlmesh.utils import UniqueKeyDict
 from sqlmesh.utils.jinja import MacroInfo
 
@@ -43,10 +47,7 @@ class DbtLoader(Loader):
         registry.update(builtin_methods())
 
         return (
-            self._add_jinja_macros(
-                registry,
-                Path(self._context.path, "macros").glob("**/*.sql"),
-            ),
+            registry,
             UniqueKeyDict("hooks"),
         )
 
@@ -61,23 +62,49 @@ class DbtLoader(Loader):
         for path in config.project_files:
             self._track_file(path)
 
-        models.update({seed.seed_name: seed.to_sqlmesh() for seed in config.seeds.values()})
+        all_sources: UniqueKeyDict[str, SourceConfig] = UniqueKeyDict("sources")
+        all_seeds: UniqueKeyDict[str, SeedConfig] = UniqueKeyDict("seeds")
+        all_models: UniqueKeyDict[str, ModelConfig] = UniqueKeyDict("models")
+        all_variables: UniqueKeyDict[str, t.Any] = UniqueKeyDict("variables")
 
-        models.update(
-            {
-                model.model_name: model.to_sqlmesh(
-                    config.sources,
-                    config.models,
-                    config.seeds,
-                    config.variables,
-                    macros,
-                    self._macro_dependencies,
+        for package in config.packages.values():
+            all_sources.update(package.sources)
+            all_seeds.update(package.seeds)
+            all_models.update(package.models)
+            all_variables.update(package.variables)
+
+        for name, package in config.packages.items():
+            all_macros = self._macros_for_package(name, config.packages)
+            for name, macro in macros.items():
+                all_macros[name] = MacroConfig(macro=macro, dependencies=Dependencies())
+
+            for model in package.models.values():
+                models[model.model_name] = model.to_sqlmesh(
+                    all_sources, all_models, all_seeds, all_variables, all_macros
                 )
-                for model in config.models.values()
-            }
-        )
+
+            for seed in package.seeds.values():
+                models[seed.seed_name] = seed.to_sqlmesh()
 
         return models
+
+    def _macros_for_package(
+        self, name: str, packages: t.Dict[str, Package]
+    ) -> UniqueKeyDict[str, MacroConfig]:
+        """
+        Macros are namespaced by the package they reside in. Do not include
+        the namespace for the macros in the package being processed
+        """
+        macros: UniqueKeyDict[str, MacroConfig] = UniqueKeyDict("macros")
+        for package_name, package in packages.items():
+            if package_name == name:
+                macros.update(package.macros)
+                continue
+
+            for macro_name, macro in package.macros.items():
+                macros[f"{package_name}.{macro_name}"] = macro
+
+        return macros
 
     def _load_audits(self) -> UniqueKeyDict[str, Audit]:
         return UniqueKeyDict("audits")
