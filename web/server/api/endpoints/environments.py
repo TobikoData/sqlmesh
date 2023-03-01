864b9e8e18301cfff60a6c4ff1ab4ad3d61394b9
Vincent Chan <vchan@users.noreply.github.com>
2023-02-28 16:15:41 -0800

    [API] Include a default prod environment if it does not exist yet (#458)

--- web/server/api/endpoints/environments.py
+++ web/server/api/endpoints/environments.py
@@ -2,6 +2,7 @@ import typing as t
 
 from fastapi import APIRouter, Depends
 
+from sqlmesh.core import constants as c
 from sqlmesh.core.context import Context
 from sqlmesh.core.environment import Environment
 from web.server.settings import get_loaded_context
@@ -12,5 +13,12 @@ router = APIRouter()
 @router.get("")
 def get_environments(context: Context = Depends(get_loaded_context)) -> t.Dict[str, Environment]:
     """Get the environments"""
-
-    return {env.name: env for env in context.state_reader.get_environments()}
+    environments = {env.name: env for env in context.state_reader.get_environments()}
+    if c.PROD not in environments:
+        environments[c.PROD] = Environment(
+            name=c.PROD,
+            snapshots=[],
+            start_at=0,
+            plan_id="",
+        )
+    return environments
