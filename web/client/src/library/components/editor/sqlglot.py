a05c28d55d2eb810f0e178fe48eaf4998b9c4425
Max Mykal <mykalmax@yahoo.com>
2023-03-07 14:51:47 -0800

    fix case

--- web/client/src/library/components/editor/sqlglot.py
+++ web/client/src/library/components/editor/sqlglot.py
@@ -1,10 +1,11 @@
 import json
 
 import sqlglot
+from sqlglot.dialects.dialect import DialectType as DialectType
 
 
-def parseToJSON(sql: str) -> str:
-    return json.dumps([exp.dump() if exp else {} for exp in sqlglot.parse(sql)])
+def parse_to_json(sql: str, read: DialectType = None) -> str:
+    return json.dumps([exp.dump() if exp else {} for exp in sqlglot.parse(sql, read=read)])
 
 
-[sqlglot.transpile, parseToJSON]
+[sqlglot.transpile, parse_to_json]
