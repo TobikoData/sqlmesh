7601766dd45d0487b66f4fe2f5107b359ec39de2
tobymao <toby.mao@gmail.com>
2023-03-23 16:21:48 -0700

    fix documentation and change categorized

--- sqlmesh/core/context.py
+++ sqlmesh/core/context.py
@@ -642,7 +642,7 @@ class Context(BaseContext):
         if not no_auto_categorization and not forward_only:
             # Attempt to automatically determine and assign change categories.
             for new, old in plan.context_diff.modified_snapshots.values():
-                if new in plan.directly_modified:
+                if new in plan.directly_modified and plan.is_new_snapshot(new):
                     change_category = categorize_change(
                         new, old, config=self.config.auto_categorize_changes
                     )
