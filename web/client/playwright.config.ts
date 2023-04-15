51c7d5286aa66013a24bd00c6abc5ed2541c1b95
Max Mykal <mykalmax@yahoo.com>
2023-04-14 17:15:12 -0700

    disable vite proxy for testing

--- web/client/playwright.config.ts
+++ web/client/playwright.config.ts
@@ -38,12 +38,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
