eaed3fcd3121499895178dfe7294d81b12b38fd4
Chris Rericha <tristipher@gmail.com>
2023-04-24 17:06:55 -0400

    dry

--- tests/dbt/conftest.py
+++ tests/dbt/conftest.py
@@ -1,23 +1,20 @@
 from __future__ import annotations
 
 from pathlib import Path
-from shutil import rmtree
 
 import pytest
 from pytest_mock.plugin import MockerFixture
 
 from sqlmesh.dbt.context import DbtContext
 from sqlmesh.dbt.project import Project
+from tests.conftest import delete_cache
 
 
 @pytest.fixture()
 def sushi_test_project(mocker: MockerFixture) -> Project:
-    project_root = Path("tests/fixtures/dbt/sushi_test")
-    try:
-        rmtree(str(project_root) + "/.cache")
-    except FileNotFoundError:
-        pass
-    project = Project.load(DbtContext(project_root=project_root))
+    project_root = "tests/fixtures/dbt/sushi_test"
+    delete_cache(project_root)
+    project = Project.load(DbtContext(project_root=Path(project_root)))
     for package_name, package in project.packages.items():
         project.context.jinja_macros.add_macros(
             package.macro_infos,
