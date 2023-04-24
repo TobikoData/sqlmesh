07b8826ffd5d71a7180a7c8d1ba6c9c477a50720
Max Mykal <mykalmax@yahoo.com>
2023-04-24 08:02:00 -0700

    move _get_path into watch_project's scope

--- web/server/watcher.py
+++ web/server/watcher.py
@@ -1,11 +1,9 @@
 import asyncio
 import json
-import typing as t
 from pathlib import Path
 
 from watchfiles import awatch
 
-from sqlmesh.core.context import Context
 from web.server.api.endpoints.models import get_models
 from web.server.settings import get_loaded_context, get_settings
 from web.server.sse import Event
@@ -15,23 +13,20 @@ async def watch_project(queue: asyncio.Queue) -> None:
     settings = get_settings()
     context = await get_loaded_context(settings)
 
-    _get_path = _to_relative_path(context)
+    def _get_path(path: str) -> str:
+        return str(Path(path).relative_to(context.path))
 
     async for changes in awatch(settings.project_path):
         context.load()
 
+        changes_paths = {_get_path(path) for type, path in changes if type == 2}
         changed_models = []
         output_models = []
-        changes_path = {_get_path(path) for type, path in changes if type == 2}
 
         for model in get_models(context):
-            if model.path in changes_path:
+            if model.path in changes_paths:
                 changed_models.append(model)
             output_models.append(model.dict())
 
         if changed_models:
             queue.put_nowait(Event(event="models", data=json.dumps(output_models)))
-
-
-def _to_relative_path(context: Context) -> t.Callable[[str], str]:
-    return lambda path: str(Path(path).relative_to(context.path))
