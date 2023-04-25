c6dcb3cfb15d83f87e029827faa6238aeeba8feb
Chris Rericha <67359577+crericha@users.noreply.github.com>
2023-04-25 12:36:58 -0400

    Chore: Delete project cache before using in a test (#762)

--- tests/dbt/conftest.py
+++ tests/dbt/conftest.py
@@ -7,11 +7,14 @@ from pytest_mock.plugin import MockerFixture
 
 from sqlmesh.dbt.context import DbtContext
 from sqlmesh.dbt.project import Project
+from tests.conftest import delete_cache
 
 
 @pytest.fixture()
 def sushi_test_project(mocker: MockerFixture) -> Project:
-    project = Project.load(DbtContext(project_root=Path("tests/fixtures/dbt/sushi_test")))
+    project_root = "tests/fixtures/dbt/sushi_test"
+    delete_cache(project_root)
+    project = Project.load(DbtContext(project_root=Path(project_root)))
     for package_name, package in project.packages.items():
         project.context.jinja_macros.add_macros(
             package.macro_infos,
