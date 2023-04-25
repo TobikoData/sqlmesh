c161afd77d92dba2e52de019956310a4f989b4e4
Max Mykal <mykalmax@yahoo.com>
2023-04-25 14:20:22 -0700

    use DefaultFilter from wathcfiles

--- web/server/watcher.py
+++ web/server/watcher.py
@@ -1,8 +1,7 @@
 import asyncio
 import json
-from pathlib import Path
 
-from watchfiles import Change, DefaultFilter, awatch
+from watchfiles import DefaultFilter, awatch
 
 from sqlmesh.core import constants as c
 from web.server.api.endpoints.models import get_models
@@ -14,15 +13,12 @@ async def watch_project(queue: asyncio.Queue) -> None:
     settings = get_settings()
     context = await get_loaded_context(settings)
 
-    class IgnorePatternsFilter(DefaultFilter):
-        ignore_patterns = context.config.ignore_patterns if context else c.IGNORE_PATTERNS
-
-        def __call__(self, change: Change, path: str) -> bool:
-            return super().__call__(change, path) and not any(
-                Path(path).match(pattern) for pattern in self.ignore_patterns
-            )
-
-    async for _ in awatch((context.path / c.MODELS).resolve(), watch_filter=IgnorePatternsFilter()):
+    async for _ in awatch(
+        (context.path / c.MODELS).resolve(),
+        watch_filter=DefaultFilter(
+            ignore_entity_patterns=context.config.ignore_patterns if context else c.IGNORE_PATTERNS
+        ),
+    ):
         context.load()
 
         queue.put_nowait(
