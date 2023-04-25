cc49a3dcb7559679a8aade5e59bd84ace1959efe
Max Mykal <mykalmax@yahoo.com>
2023-04-25 14:24:02 -0700

    clean up

--- web/client/src/library/components/graph/help.ts
+++ web/client/src/library/components/graph/help.ts
@@ -133,8 +133,6 @@ export function getNodesAndEdges({
                 },
               ),
           )
-
-          // console.log([modelSource, modelTarget], { currentEdge })
         }
       }
     }
