768e805a173496e8a71c7f613ab0580669ed222c
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-05-03 16:52:04 -0700

    remove extra union

--- sqlmesh/core/_typing.py
+++ sqlmesh/core/_typing.py
@@ -1,22 +1,12 @@
 from __future__ import annotations
 
-import sys
 import typing as t
 
-if sys.version_info >= (3, 9):
-    from typing import Annotated
-else:
-    from typing_extensions import Annotated
-
-from pydantic import Field
 from sqlglot import exp
 
 from sqlmesh.core.notification_target import ConsoleNotificationTarget
 
-NotificationTarget = Annotated[
-    t.Union[ConsoleNotificationTarget],
-    Field(discriminator="type_"),
-]
+NotificationTarget = ConsoleNotificationTarget
 
 if t.TYPE_CHECKING:
     TableName = t.Union[str, exp.Table]
