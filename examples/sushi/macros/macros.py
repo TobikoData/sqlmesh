fa5a90c5f9b665083bdcdcf31108e891a5fdecf0
Toby Mao <toby.mao@gmail.com>
2023-05-04 20:47:13 -0700

    Feat: BREAKING always parse macro returns as sql (#815)

--- examples/sushi/macros/macros.py
+++ examples/sushi/macros/macros.py
@@ -1,21 +1,11 @@
-from sqlglot import exp
-
-from sqlmesh.core.dialect import MacroVar
-from sqlmesh.core.macros import MacroEvaluator, macro
-from sqlmesh.utils.errors import MacroEvalError
+from sqlmesh import macro
 
 
 @macro()
-def add_one(evaluator: MacroEvaluator, column: str) -> exp.Expression:
-    return evaluator.parse_one(f"{column} + 1")
+def add_one(evaluator, column):
+    return f"{column} + 1"
 
 
 @macro()
-def incremental_by_ds(evaluator: MacroEvaluator, column: exp.Column) -> exp.Expression:
-    expression = evaluator.transform(
-        exp.Between(this=column, low=MacroVar(this="start_ds"), high=MacroVar(this="end_ds"))
-    )
-    if not isinstance(expression, exp.Expression):
-        raise MacroEvalError(f"Return type is {type(expression)}, expected exp.Expression")
-
-    return expression
+def incremental_by_ds(evaluator, column):
+    return column.between(evaluator.locals["start_ds"], evaluator.locals["end_ds"])
