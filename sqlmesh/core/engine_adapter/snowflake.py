144c2fc2bef9f85e07dc3af160f1de80089f7bed
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-05-16 11:40:10 -0700

    Fix: In Snowflake adapter return the DataFrame as is without modifying its column names.  (#846)

--- sqlmesh/core/engine_adapter/snowflake.py
+++ sqlmesh/core/engine_adapter/snowflake.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import typing as t
 
 import pandas as pd
-from sqlglot import exp, parse_one
+from sqlglot import exp
 
 from sqlmesh.core.engine_adapter.base import EngineAdapter
 from sqlmesh.core.engine_adapter.shared import DataObject, DataObjectType
@@ -25,27 +25,14 @@ class SnowflakeEngineAdapter(EngineAdapter):
         self.execute(query)
 
         try:
-            df = self.cursor.fetch_pandas_all()
+            return self.cursor.fetch_pandas_all()
         except NotSupportedError:
             # Sometimes Snowflake will not return results as an Arrow result and the fetch from
             # pandas will fail (Ex: `SHOW TERSE OBJECTS IN SCHEMA`). Therefore we manually convert
             # the result into a DataFrame when this happens.
             rows = self.cursor.fetchall()
             columns = self.cursor._result_set.batches[0].column_names
-            df = pd.DataFrame([dict(zip(columns, row)) for row in rows])
-
-        # Snowflake returns uppercase column names if the columns are not quoted (so case-insensitive)
-        # so replace the column names returned by Snowflake with the column names in the expression
-        # if the expression was a select expression
-        if isinstance(query, str):
-            parsed_query = parse_one(query, read=self.dialect)
-            if parsed_query is None:
-                # If we didn't get a result from parsing we will just optimistically assume that the df is fine
-                return df
-            query = parsed_query
-        if isinstance(query, exp.Subqueryable):
-            df.columns = query.named_selects
-        return df
+            return pd.DataFrame([dict(zip(columns, row)) for row in rows])
 
     def _get_data_objects(
         self, schema_name: str, catalog_name: t.Optional[str] = None
