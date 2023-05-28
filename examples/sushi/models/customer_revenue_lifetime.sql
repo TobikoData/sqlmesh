0de5f80d870ec3caf478e0584e618b77bb70f293
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-05-27 18:16:31 -0700

    fmt model

--- examples/sushi/models/customer_revenue_lifetime.sql
+++ examples/sushi/models/customer_revenue_lifetime.sql
@@ -6,7 +6,7 @@
 MODEL (
   name sushi.customer_revenue_lifetime,
   kind full_with_history (
-    time_column (ds, 'YYYY-MM-dd'),
+    time_column (ds, 'YYYY-MM-dd')
   ),
   owner jen,
   cron '@daily',
@@ -14,46 +14,44 @@ MODEL (
   columns (
     customer_id INT,
     revenue DOUBLE,
-    ds TEXT,
-  ),
+    ds STRING
+  )
 );
 
 WITH order_total AS (
   SELECT
     oi.order_id AS order_id,
-    SUM(oi.quantity * i.price) AS total,
+    SUM(oi.quantity * i.price) AS total
   FROM sushi.order_items AS oi
   LEFT JOIN sushi.items AS i
     ON oi.item_id = i.id AND oi.ds = i.ds
   WHERE
     oi.ds = @end_ds
   GROUP BY
-    oi.order_id,
-),
-incremental_total AS (
-SELECT
-  o.customer_id::INT AS customer_id,
-  SUM(ot.total)::DOUBLE AS revenue,
-FROM sushi.orders AS o
-LEFT JOIN order_total AS ot
-  ON o.id = ot.order_id
-WHERE
-  o.ds = @end_ds
-GROUP BY
-  o.customer_id,
-),
-prev_total AS (
-SELECT
-  crl.customer_id,
-  crl.revenue,
-FROM sushi.customer_revenue_lifetime AS crl
-WHERE
-  crl.ds = DATE_FORMAT(@end_date - INTERVAL 1 DAY, 'YYYY-MM-dd')
+    oi.order_id
+), incremental_total AS (
+  SELECT
+    o.customer_id::INT AS customer_id,
+    SUM(ot.total)::DOUBLE AS revenue,
+  FROM sushi.orders AS o
+  LEFT JOIN order_total AS ot
+    ON o.id = ot.order_id
+  WHERE
+    o.ds = @end_ds
+  GROUP BY
+    o.customer_id
+), prev_total AS (
+  SELECT
+    crl.customer_id,
+    crl.revenue
+  FROM sushi.customer_revenue_lifetime AS crl
+  WHERE
+    crl.ds = DATE_FORMAT(@end_date - INTERVAL 1 DAY, 'YYYY-MM-dd')
 )
 SELECT
-  coalesce(it.customer_id, prev_total.customer_id) AS customer_id /* Customer id */,
-  coalesce(it.revenue, 0) + coalesce(prev_total.revenue, 0) AS revenue, /* Lifetime revenue from this customer */
-  @end_ds AS ds, /* End date of the lifetime calculation */
+  COALESCE(it.customer_id, prev_total.customer_id) AS customer_id, /* Customer id */
+  COALESCE(it.revenue, 0) + COALESCE(prev_total.revenue, 0) AS revenue, /* Lifetime revenue from this customer */
+  @end_ds AS ds /* End date of the lifetime calculation */
 FROM incremental_total AS it
 FULL OUTER JOIN prev_total AS prev_total
-ON it.customer_id = prev_total.customer_id
+  ON it.customer_id = prev_total.customer_id
