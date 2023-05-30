ceb19bf3d6d537af1b5ccaa3d0200461e88b8e9e
Toby Mao <toby.mao@gmail.com>
2023-05-30 16:19:19 -0700

    Chore: add external models to sushi project (#898)
    * Chore: add external models to sushi project
    
    * fix tests

--- examples/sushi/models/customers.sql
+++ examples/sushi/models/customers.sql
@@ -6,6 +6,26 @@ MODEL (
   tags (pii, fact)
 );
 
+CREATE SCHEMA IF NOT EXISTS raw;
+CREATE TABLE IF NOT EXISTS raw.marketing AS (
+  SELECT 1 AS customer_id, 'active' AS status
+);
+CREATE TABLE IF NOT EXISTS raw.demographics AS (
+  SELECT 1 AS customer_id, '00000' AS zip
+);
+
+WITH distinct_marketing AS (
+  SELECT DISTINCT
+    customer_id,
+    status
+  FROM raw.marketing
+)
 SELECT DISTINCT
-  customer_id::INT AS customer_id
+  o.customer_id::INT AS customer_id,
+  COALESCE(m.status, 'UNKNOWN') AS status,
+  d.zip
 FROM sushi.orders AS o
+LEFT JOIN distinct_marketing AS m
+    ON o.customer_id = m.customer_id
+LEFT JOIN raw.demographics AS d
+    ON o.customer_id = d.customer_id
