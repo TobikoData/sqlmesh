fae6952acedc1dca536c05b430e6f1c2338d3972
Chris Rericha <tristipher@gmail.com>
2023-06-01 12:55:22 -0400

    Chore: Fix typo and rephrase unresolved projections error.

--- sqlmesh/core/loader.py
+++ sqlmesh/core/loader.py
@@ -70,8 +70,9 @@ def update_model_schemas(dag: DAG[str], models: UniqueKeyDict[str, Model]) -> No
             if "*" in model.columns_to_types:
                 raise ConfigError(
                     f"Can't expand SELECT * expression for model '{name}'."
-                    " Projects from an external source must either be explicitly specified within models or added as"
-                    " external models by using the command 'sqlmesh create_external_models'."
+                    " Projections from an external source must be explicitly specified within"
+                    " models or add source tables as external models by using the command"
+                    " 'sqlmesh create_external_models'."
                 )
         elif model.mapping_schema:
             try:
