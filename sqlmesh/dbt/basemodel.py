b2936488c72876f2d11fad165792eefb0534bde7
Chris Rericha <67359577+crericha@users.noreply.github.com>
2023-06-12 15:41:41 -0400

    Fix: Determine model dependencies after rendering. (#972)

--- sqlmesh/dbt/basemodel.py
+++ sqlmesh/dbt/basemodel.py
@@ -235,9 +235,6 @@ class BaseModelConfig(GeneralConfig):
             "audits": [(test.name, {}) for test in self.tests],
             "columns": column_types_to_sqlmesh(self.columns) or None,
             "column_descriptions_": column_descriptions_to_sqlmesh(self.columns) or None,
-            "depends_on": {model.sql_name for model in model_context.refs.values()}.union(
-                {source.sql_name for source in model_context.sources.values()}
-            ),
             "jinja_macros": jinja_macros,
             "path": self.path,
             "hash_raw_query": True,
