30ca354c97d5dd2bce4edb6296e51eac8986827c
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-06-26 13:04:33 -0700

    Fix: Skip dbt tests that reference disabled models

--- sqlmesh/dbt/manifest.py
+++ sqlmesh/dbt/manifest.py
@@ -136,9 +136,24 @@ class ManifestHelper:
             if node.resource_type != "test":
                 continue
 
+            skip_test = False
+            package_name = node.package_name
+            refs = _refs(node)
+            for ref in refs:
+                ref_node_name = f"model.{ref}" if "." in ref else f"model.{package_name}.{ref}"
+                if ref_node_name in self._manifest.disabled:
+                    logger.info(
+                        "Skipping test '%s' which references a disabled model '%s'", node.name, ref
+                    )
+                    skip_test = True
+                    break
+
+            if skip_test:
+                continue
+
             dependencies = Dependencies(
                 macros=_macro_references(self._manifest, node),
-                refs=_refs(node),
+                refs=refs,
                 sources=_sources(node),
             )
             # Implicit dependencies for model test arg
