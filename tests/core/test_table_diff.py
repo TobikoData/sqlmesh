5b938658f140980566f9891bf189c15e4cd633c1
Trey Spiller <treyspiller@gmail.com>
2023-06-30 16:21:49 -0500

    Expand test coverage

--- tests/core/test_table_diff.py
+++ tests/core/test_table_diff.py
@@ -1,9 +1,15 @@
 from sqlglot import exp
 
 from sqlmesh.core.config import AutoCategorizationMode, CategorizerConfig
+from sqlmesh.core.model import SqlModel
 
 
 def test_data_diff(sushi_context_fixed_date):
+    model = sushi_context_fixed_date.models["sushi.customer_revenue_by_day"]
+
+    model.query.select(exp.cast("'1'", "VARCHAR").as_("modified_col"), "1 AS y", copy=False)
+    sushi_context_fixed_date.upsert_model(model)
+
     sushi_context_fixed_date.plan(
         "source_dev",
         no_prompts=True,
@@ -16,10 +22,31 @@ def test_data_diff(sushi_context_fixed_date):
 
     for column in model.query.find_all(exp.Column):
         if column.name == "total":
-            column.replace(exp.condition("ot.total * 0.1"))
+            column.replace(
+                exp.func(
+                    "IF",
+                    exp.column("total", "ot").__lt__(4),
+                    exp.condition("ot.total * 0.1"),
+                    exp.column("total", "ot"),
+                )
+            )
+
+    for alias in model.query.find_all(exp.Alias):
+        if alias.alias == "modified_col":
+            alias.this.set("to", exp.DataType.build("INT"))
+        if alias.alias == "y":
+            alias.pop()
 
     model.query.select("1 AS z", copy=False).limit(10000, copy=False)
-    sushi_context_fixed_date.upsert_model(model)
+    # add row to table with UNION
+    modified_model = model.dict()
+    modified_model["query"] = (
+        exp.select("*")
+        .from_(model.query.subquery())
+        .union("select -1, 9999.00, '2023-01-31', 1, 1")
+    )
+    modified_sqlmodel = SqlModel(**modified_model)
+    sushi_context_fixed_date.upsert_model(modified_sqlmodel)
 
     sushi_context_fixed_date.auto_categorize_changes = CategorizerConfig(
         sql=AutoCategorizationMode.FULL
@@ -43,9 +70,12 @@ def test_data_diff(sushi_context_fixed_date):
 
     schema_diff = diff.schema_diff()
     assert schema_diff.added == [("z", exp.DataType.build("int"))]
-    assert schema_diff.removed == []
+    assert schema_diff.modified == {
+        "modified_col": (exp.DataType.build("varchar"), exp.DataType.build("int"))
+    }
+    assert schema_diff.removed == [("y", exp.DataType.build("int"))]
 
     row_diff = diff.row_diff()
     assert row_diff.source_count == 546
-    assert row_diff.target_count == 546
-    assert row_diff.sample.shape == (20, 7)
+    assert row_diff.target_count == 547
+    assert row_diff.sample.shape == (6, 10)
