4707f314923fac47395a99ec0dde135af14cfb1f
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-07-11 10:23:15 -0700

    fix session_config being None

--- sqlmesh/core/config/connection.py
+++ sqlmesh/core/config/connection.py
@@ -218,6 +218,8 @@ class DatabricksConnectionConfig(_ConnectionConfig):
                 values["databricks_connect_access_token"] = access_token
             if not values.get("databricks_connect_cluster_id"):
                 values["databricks_connect_cluster_id"] = http_path.split("/")[-1]
+        if not values.get("session_configuration"):
+            values["session_configuration"] = {}
         values["session_configuration"]["spark.sql.sources.partitionOverwriteMode"] = "dynamic"
         return values
 
