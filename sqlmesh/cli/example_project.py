520d95b21133959205785284510bed48328f9f70
Trey Spiller <treyspiller@gmail.com>
2023-07-11 10:02:10 -0500

    Quote dialect in sqlmesh init dbt config

--- sqlmesh/cli/example_project.py
+++ sqlmesh/cli/example_project.py
@@ -39,7 +39,7 @@ model_defaults:
 from sqlmesh.dbt.loader import sqlmesh_config
 from sqlmesh.core.config.model import ModelDefaultsConfig
 
-config = sqlmesh_config(Path(__file__).parent, model_defaults=ModelDefaultsConfig(dialect={dialect}))
+config = sqlmesh_config(Path(__file__).parent, model_defaults=ModelDefaultsConfig(dialect='{dialect}'))
 """,
     }
 
