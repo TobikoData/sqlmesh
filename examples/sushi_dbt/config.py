c29b7d593d8df99c0480dc9293510bb4e64e0339
Chris Rericha <tristipher@gmail.com>
2023-07-13 02:09:58 -0400

    Remove sushi_dbt config changes

--- examples/sushi_dbt/config.py
+++ examples/sushi_dbt/config.py
@@ -1,17 +1,9 @@
 from pathlib import Path
 
-from sqlmesh.core.config import AirflowSchedulerConfig, GooglePostgresConnectionConfig
+from sqlmesh.core.config import AirflowSchedulerConfig
 from sqlmesh.dbt.loader import sqlmesh_config
 
-config = sqlmesh_config(
-    Path(__file__).parent,
-    state_connection=GooglePostgresConnectionConfig(
-        instance_connection_string="tobiko-1:us-central1:postgres-state-db",
-        user="chris@tobikodata.com",
-        enable_iam_auth=True,
-        db="state",
-    ),
-)
+config = sqlmesh_config(Path(__file__).parent)
 
 
 test_config = config
