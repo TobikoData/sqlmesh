aed743b496027e8a02aa7520f951d6adba1c243b
George Sittas <giwrgos.sittas@gmail.com>
2023-07-15 02:24:57 +0300

    Fix test

--- tests/core/test_snapshot_evaluator.py
+++ tests/core/test_snapshot_evaluator.py
@@ -388,7 +388,7 @@ def test_migrate_view(mocker: MockerFixture, make_snapshot):
     cursor_mock.execute.assert_has_calls(
         [
             call(
-                'CREATE OR REPLACE VIEW sqlmesh__test_schema.test_schema__test_model__1 AS SELECT "c" AS "c", "a" AS "a" FROM "tbl" AS "tbl"'
+                'CREATE OR REPLACE VIEW "sqlmesh__test_schema"."test_schema__test_model__1" AS SELECT "c" AS "c", "a" AS "a" FROM "tbl" AS "tbl"'
             )
         ]
     )
