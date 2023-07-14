62ff63782e4d21e912195e0cd839cbd7cc19aebb
Trey Spiller <treyspiller@gmail.com>
2023-07-14 15:44:17 -0500

    Add model default dialect to example sushi dbt config

--- examples/sushi_dbt/config.py
+++ examples/sushi_dbt/config.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 
-from sqlmesh.core.config import AirflowSchedulerConfig
+from sqlmesh.core.config import AirflowSchedulerConfig, ModelDefaultsConfig
 from sqlmesh.dbt.loader import sqlmesh_config
 
-config = sqlmesh_config(Path(__file__).parent)
+config = sqlmesh_config(Path(__file__).parent, model_defaults=ModelDefaultsConfig(dialect=""))
 
 
 test_config = config
 
 
-airflow_config = sqlmesh_config(Path(__file__).parent, default_scheduler=AirflowSchedulerConfig())
+airflow_config = sqlmesh_config(
+    Path(__file__).parent,
+    default_scheduler=AirflowSchedulerConfig(),
+    model_defaults=ModelDefaultsConfig(dialect=""),
+)
