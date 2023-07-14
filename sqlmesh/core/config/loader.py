dd3f95f58c391fcb6c0f6203f495cb9b79a99d29
Trey Spiller <treyspiller@gmail.com>
2023-07-14 15:17:52 -0500

    Remove project- and personal-config specific dialect validation

--- sqlmesh/core/config/loader.py
+++ sqlmesh/core/config/loader.py
@@ -18,73 +18,64 @@ def load_config_from_paths(
     config_name: str = "config",
     load_from_env: bool = True,
 ) -> Config:
+
     visited_folders: t.Set[Path] = set()
+    python_config: t.Optional[Config] = None
+    non_python_configs = []
 
     if not project_paths or not any([path.exists() for path in project_paths]):
         raise ConfigError(
-            "SQLMesh config could not be found. Point the cli to the right path with `sqlmesh -p`. If you haven't set up the SQLMesh project, run `sqlmesh init`."
+            "SQLMesh project config could not be found. Point the cli to the project path with `sqlmesh -p`. If you haven't set up the SQLMesh project, run `sqlmesh init`."
         )
 
-    def _load_configs(paths: t.List[Path]) -> t.Dict[str, t.Optional[t.Union[Config, t.List]]]:
-        python_config: Config | None = None
-        non_python_configs: t.List[t.Dict] = []
-        for path in paths:
-            if not path.exists():
-                continue
+    for path in [*project_paths, *personal_paths]:
+        if not path.exists():
+            continue
 
-            if not path.is_file():
-                raise ConfigError(f"Path '{path}' must be a file.")
+        if not path.is_file():
+            raise ConfigError(f"Path '{path}' must be a file.")
 
-            parent_path = path.parent
-            if parent_path in visited_folders:
-                raise ConfigError(f"Multiple configuration files found in folder '{parent_path}'.")
-            visited_folders.add(parent_path)
+        parent_path = path.parent
+        if parent_path in visited_folders:
+            raise ConfigError(f"Multiple configuration files found in folder '{parent_path}'.")
+        visited_folders.add(parent_path)
 
-            extension = path.name.split(".")[-1].lower()
-            if extension in ("yml", "yaml"):
-                if config_name != "config":
-                    raise ConfigError(
-                        "YAML configs do not support multiple configs. Use Python instead."
-                    )
-                non_python_configs.append(load_config_from_yaml(path))
-            elif extension == "py":
-                python_config = load_config_from_python_module(path, config_name=config_name)
-            else:
+        extension = path.name.split(".")[-1].lower()
+        if extension in ("yml", "yaml"):
+            if config_name != "config":
                 raise ConfigError(
-                    f"Unsupported config file extension '{extension}' in config file '{path}'."
+                    "YAML configs do not support multiple configs. Use Python instead."
                 )
-
-        return {"python_config": python_config, "non_python_configs": non_python_configs}
-
-    project_config_dicts = _load_configs(project_paths)
-    personal_config_dicts = _load_configs(personal_paths)
-
-    no_dialect_err_msg = "Default model SQL dialect is a required configuration parameter - set it in the `model_defaults` key."
-
-    if project_config_dicts.get("non_python_configs"):
-        non_python_config = Config.parse_obj(
-            merge_dicts(*project_config_dicts.get("non_python_configs"))
-        )
-        non_python_defaults = non_python_config.model_defaults
-        if non_python_defaults.dialect is None:
-            raise ConfigError(no_dialect_err_msg)
-
-        non_python_config.update_with(merge_dicts(*personal_config_dicts.get("non_python_configs")))
-    else:
-        non_python_config = Config()
+            non_python_configs.append(load_config_from_yaml(path))
+        elif extension == "py":
+            python_config = load_config_from_python_module(path, config_name=config_name)
+        else:
+            raise ConfigError(
+                f"Unsupported config file extension '{extension}' in config file '{path}'."
+            )
 
     if load_from_env:
         env_config = load_config_from_env()
         if env_config:
-            non_python_config.update_with(load_config_from_env())
+            non_python_configs.append(load_config_from_env())
 
-    python_config = project_config_dicts["python_config"]
+    if not non_python_configs and not python_config:
+        raise ConfigError(
+            "SQLMesh config could not be found. Point the cli to the right path with `sqlmesh -p`. If you haven't set up SQLMesh, run `sqlmesh init`."
+        )
+
+    no_dialect_err_msg = "Default model SQL dialect is a required configuration parameter - set it in the `model_defaults` key."
+
+    non_python_config = Config.parse_obj(merge_dicts(*non_python_configs))
     if python_config:
-        python_defaults = python_config.model_defaults
-        if python_defaults.dialect is None:
+        model_defaults = python_config.model_defaults
+        if model_defaults.dialect is None:
             raise ConfigError(no_dialect_err_msg)
         return python_config.update_with(non_python_config)
 
+    model_defaults = non_python_config.model_defaults
+    if model_defaults.dialect is None:
+        raise ConfigError(no_dialect_err_msg)
     return non_python_config
 
 
