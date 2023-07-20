f3d8a092d788e3a07fab791a4142bacb243c8171
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-07-20 09:27:48 -0700

    Fix: Support insert overwrite with dynamic partitions in the BigQuery adapter (#1188)

--- sqlmesh/core/engine_adapter/base.py
+++ sqlmesh/core/engine_adapter/base.py
@@ -592,10 +592,11 @@ class EngineAdapter:
                 ):
                     self.execute(exp.insert(expression, table_name, columns=column_names))
 
-    def insert_overwrite(
+    def insert_overwrite_by_partition(
         self,
         table_name: TableName,
         query_or_df: QueryOrDF,
+        partitioned_by: t.List[exp.Expression],
         columns_to_types: t.Optional[t.Dict[str, exp.DataType]] = None,
     ) -> None:
         self._insert_overwrite_by_condition(
@@ -905,7 +906,7 @@ class EngineAdapter:
             self.cursor.execute(sql, **kwargs)
 
     @contextlib.contextmanager
-    def temp_table(self, query_or_df: QueryOrDF, name: str = "diff") -> t.Iterator[exp.Table]:
+    def temp_table(self, query_or_df: QueryOrDF, name: TableName = "diff") -> t.Iterator[exp.Table]:
         """A context manager for working a temp table.
 
         The table will be created with a random guid and cleaned up after the block.
