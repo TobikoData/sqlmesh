bf81d6c075a23b7be981ffcccfbea50c97388e23
George Sittas <giwrgos.sittas@gmail.com>
2023-07-20 20:08:55 +0300

    Fix rebase error

--- sqlmesh/dbt/model.py
+++ sqlmesh/dbt/model.py
@@ -122,6 +122,7 @@ class ModelConfig(BaseModelConfig):
         },
     }
 
+    @property
     def model_materialization(self) -> Materialization:
         return Materialization(self.materialized.lower())
 
