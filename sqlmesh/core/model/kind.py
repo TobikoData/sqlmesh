7f38fd2976d363b31f1843eceb9899c226cf5b6f
George Sittas <giwrgos.sittas@gmail.com>
2023-08-03 20:53:53 +0300

    Get rid of Identifier instance check

--- sqlmesh/core/model/kind.py
+++ sqlmesh/core/model/kind.py
@@ -161,7 +161,7 @@ class TimeColumn(PydanticModel):
                 }
                 return TimeColumn(**kwargs)
 
-            if isinstance(v, (exp.Identifier, exp.Expression)):
+            if isinstance(v, exp.Expression):
                 return TimeColumn(column=v.name)
 
             if isinstance(v, str):
