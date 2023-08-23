2b0de1390af3ef268cfbd2746b27984a559b81a5
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-08-23 08:52:02 -0700

    Feat: Support model selection when constructing a plan (#1335)

---
+++ sqlmesh/core/selector.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+import typing as t
+from pathlib import Path
+
+from sqlmesh.core.environment import Environment
+from sqlmesh.core.loader import update_model_schemas
+from sqlmesh.core.model import Model
+from sqlmesh.core.state_sync import StateReader
+from sqlmesh.utils import UniqueKeyDict
+from sqlmesh.utils.dag import DAG
+from sqlmesh.utils.errors import SQLMeshError
+
+
+class Selector:
+    def __init__(
+        self,
+        state_reader: StateReader,
+        models: UniqueKeyDict[str, Model],
+        context_path: Path = Path("."),
+    ):
+        self._state_reader = state_reader
+        self._models = models
+        self._context_path = context_path
+
+    def select_models(
+        self,
+        model_selections: t.Iterable[str],
+        target_env_name: str,
+        fallback_env_name: t.Optional[str] = None,
+    ) -> UniqueKeyDict[str, Model]:
+        """Given a set of selections returns models from the current state with names matching the
+        selection while sourcing the remaining models from the target environment.
+
+        Args:
+            model_selections: A set of selections.
+            target_env_name: The name of the target environment.
+            fallback_env_name: The name of the fallback environment that will be used if the target
+                environment doesn't exist.
+
+        Returns:
+            A dictionary of models.
+        """
+        target_env = self._state_reader.get_environment(Environment.normalize_name(target_env_name))
+        if not target_env and fallback_env_name:
+            target_env = self._state_reader.get_environment(
+                Environment.normalize_name(fallback_env_name)
+            )
+        if not target_env:
+            raise SQLMeshError(
+                f"Either the '{target_env_name}' or the '{fallback_env_name}' environment must exist in order to apply model selection."
+            )
+
+        env_models = {
+            s.name: s.model
+            for s in self._state_reader.get_snapshots(
+                target_env.snapshots, hydrate_seeds=True
+            ).values()
+        }
+
+        # TODO: Support selection expressions.
+        all_selected_models = set(model_selections)
+
+        dag: DAG[str] = DAG()
+        models: UniqueKeyDict[str, Model] = UniqueKeyDict("models")
+        all_model_names = set(self._models) | set(env_models)
+        for name in all_model_names:
+            model: t.Optional[Model] = None
+            if name not in all_selected_models and name in env_models:
+                # Unselected modified or added model.
+                model = env_models[name]
+            elif name in all_selected_models and name in self._models:
+                # Selected modified or removed model.
+                model = self._models[name]
+
+            if model:
+                # model.copy() can't be used here due to a cached state that can be a part of a model instance.
+                model = type(model).parse_obj(model.dict(exclude={"mapping_schema"}))
+                models[name] = model
+                dag.add(model.name, model.depends_on)
+
+        update_model_schemas(dag, models, self._context_path)
+
+        return models
---
+++ tests/core/test_selector.py
@@ -0,0 +1,129 @@
+from __future__ import annotations
+
+import typing as t
+from unittest.mock import call
+
+import pytest
+from pytest_mock.plugin import MockerFixture
+
+from sqlmesh.core import dialect as d
+from sqlmesh.core.environment import Environment
+from sqlmesh.core.model import Model, SqlModel
+from sqlmesh.core.selector import Selector
+from sqlmesh.core.snapshot import SnapshotChangeCategory
+from sqlmesh.utils import UniqueKeyDict
+from sqlmesh.utils.errors import SQLMeshError
+
+
+def test_select_models(mocker: MockerFixture, make_snapshot):
+    added_model = SqlModel(name="added_model", query=d.parse_one("SELECT 1 AS a"))
+    modified_model_v1 = SqlModel(
+        name="modified_model", query=d.parse_one("SELECT a + 1 FROM added_model")
+    )
+    modified_model_v2 = SqlModel(
+        name="modified_model", query=d.parse_one("SELECT a + 2 FROM added_model")
+    )
+    removed_model = SqlModel(name="removed_model", query=d.parse_one("SELECT a FROM added_model"))
+
+    modified_model_v1_snapshot = make_snapshot(modified_model_v1)
+    modified_model_v1_snapshot.categorize_as(SnapshotChangeCategory.BREAKING)
+    removed_model_snapshot = make_snapshot(removed_model)
+    removed_model_snapshot.categorize_as(SnapshotChangeCategory.BREAKING)
+
+    env_name = "test_env"
+
+    state_reader_mock = mocker.Mock()
+    state_reader_mock.get_environment.return_value = Environment(
+        name=env_name,
+        snapshots=[s.table_info for s in (modified_model_v1_snapshot, removed_model_snapshot)],
+        start_at="2023-01-01",
+        end_at="2023-02-01",
+        plan_id="test_plan_id",
+    )
+    state_reader_mock.get_snapshots.return_value = {
+        modified_model_v1_snapshot.snapshot_id: modified_model_v1_snapshot,
+        removed_model_snapshot.snapshot_id: removed_model_snapshot,
+    }
+
+    added_model_schema = {"added_model": {"a": "INT"}}
+
+    local_models: UniqueKeyDict[str, Model] = UniqueKeyDict("models")
+    local_models[added_model.name] = added_model
+    local_models[modified_model_v2.name] = modified_model_v2.copy(
+        update={"mapping_schema": added_model_schema}
+    )
+
+    selector = Selector(state_reader_mock, local_models)
+
+    _assert_models_equal(
+        selector.select_models(["added_model"], env_name),
+        {
+            added_model.name: added_model,
+            modified_model_v1.name: modified_model_v1.copy(
+                update={"mapping_schema": added_model_schema}
+            ),
+            removed_model.name: removed_model.copy(update={"mapping_schema": added_model_schema}),
+        },
+    )
+    _assert_models_equal(
+        selector.select_models(["modified_model"], "missing_env", fallback_env_name=env_name),
+        {
+            modified_model_v2.name: modified_model_v2,
+            removed_model.name: removed_model,
+        },
+    )
+    _assert_models_equal(
+        selector.select_models(["removed_model"], env_name),
+        {
+            modified_model_v1.name: modified_model_v1,
+        },
+    )
+    _assert_models_equal(
+        selector.select_models(
+            ["added_model", "modified_model"], "missing_env", fallback_env_name=env_name
+        ),
+        {
+            added_model.name: added_model,
+            modified_model_v2.name: modified_model_v2.copy(
+                update={"mapping_schema": added_model_schema}
+            ),
+            removed_model.name: removed_model.copy(update={"mapping_schema": added_model_schema}),
+        },
+    )
+    _assert_models_equal(
+        selector.select_models(["added_model", "modified_model", "removed_model"], env_name),
+        local_models,
+    )
+
+
+def test_select_models_missing_env(mocker: MockerFixture, make_snapshot):
+    model = SqlModel(name="test_model", query=d.parse_one("SELECT 1 AS a"))
+
+    state_reader_mock = mocker.Mock()
+    state_reader_mock.get_environment.return_value = None
+
+    local_models: UniqueKeyDict[str, Model] = UniqueKeyDict("models")
+    local_models[model.name] = model
+
+    selector = Selector(state_reader_mock, local_models)
+
+    with pytest.raises(SQLMeshError):
+        selector.select_models([model.name], "missing_env")
+
+    with pytest.raises(SQLMeshError):
+        selector.select_models([model.name], "missing_env", fallback_env_name="another_missing_env")
+
+    state_reader_mock.get_environment.assert_has_calls(
+        [
+            call("missing_env"),
+            call("missing_env"),
+            call("another_missing_env"),
+        ]
+    )
+
+
+def _assert_models_equal(actual: t.Dict[str, Model], expected: t.Dict[str, Model]) -> None:
+    assert set(actual) == set(expected)
+    for name, model in actual.items():
+        # Use dict() to make Pydantic V2 happy.
+        assert model.dict() == expected[name].dict()
