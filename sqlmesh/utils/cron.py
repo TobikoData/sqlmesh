238eaf2ae0de3f0cd79e1ae39ce4297609974bb5
tobymao <toby.mao@gmail.com>
2023-09-22 15:16:13 -0700

    Feat: skip croniter when possible for performance

--- sqlmesh/utils/cron.py
+++ sqlmesh/utils/cron.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
 import typing as t
+from datetime import timedelta
 
 from croniter import croniter
+from sqlglot.helper import first
 
 from sqlmesh.utils import ttl_cache
 from sqlmesh.utils.date import TimeLike, now, to_datetime
@@ -19,14 +21,48 @@ def cron_prev(cron: str, time: TimeLike) -> float:
 
 
 class CroniterCache:
+    ESTIMATE_SAMPLES_NUM = 10
+
     def __init__(self, cron: str, time: t.Optional[TimeLike] = None):
         self.cron = cron
-        self.curr: TimeLike = time or now()
+        self.curr: TimeLike = now() if time is None else time
+        self._interval_seconds: t.Optional[int] = None
 
-    def get_next(self) -> float:
-        self.curr = cron_next(self.cron, self.curr)
+    @property
+    def interval_seconds(self) -> int:
+        """The estimated number of seconds between intervals of the given cron.
+
+        This method takes a sample of crons and if they are all evenly spaced,
+        than that number of seconds is returned. Otherwise, the sentinel value 0
+        is returned indicating that there is no deterministic number of seconds
+        to substitute for the cron call.
+        """
+        if self._interval_seconds is None:
+            seconds = set()
+            curr = to_datetime(self.curr)
+
+            for _ in range(self.ESTIMATE_SAMPLES_NUM):
+                prev = curr
+                curr = to_datetime(cron_next(self.cron, curr))
+                seconds.add(curr - prev)
+
+            if len(seconds) == 1:
+                self._interval_seconds = first(seconds).seconds
+            else:
+                self._interval_seconds = 0
+
+        return self._interval_seconds
+
+    def get_next(self, estimate: bool = False) -> float:
+        if estimate and self.interval_seconds:
+            self.curr = to_datetime(self.curr) + timedelta(seconds=self.interval_seconds)
+        else:
+            self.curr = cron_next(self.cron, self.curr)
         return t.cast(float, self.curr)
 
-    def get_prev(self) -> float:
-        self.curr = cron_prev(self.cron, self.curr)
+    def get_prev(self, estimate: bool = False) -> float:
+        if estimate and self.interval_seconds:
+            self.curr = to_datetime(self.curr) - timedelta(seconds=self.interval_seconds)
+        else:
+            self.curr = cron_prev(self.cron, self.curr)
         return t.cast(float, self.curr)
