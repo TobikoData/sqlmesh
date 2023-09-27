c7f754ebee871906a5e8ae125e241ca486055045
George Sittas <giwrgos.sittas@gmail.com>
2023-09-27 19:40:19 +0300

    Temporarily add debug statement

--- tests/core/test_model.py
+++ tests/core/test_model.py
@@ -1384,6 +1384,8 @@ from sqlmesh.core.macros import macro
 @macro()
 def apply_mask(evaluator, model_name, patterns):
     patterns = [p.this for p in patterns.expressions]
+    if not evaluator.models[model_name.sql()].columns_to_types:
+        print(evaluator.models)
     return [
         (
             f"HASH({column}) AS {column}_digest"
