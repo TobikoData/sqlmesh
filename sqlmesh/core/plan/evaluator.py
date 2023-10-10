8bc51650e5b92737e592210747f31b3141fbaa91
George Sittas <giwrgos.sittas@gmail.com>
2023-10-11 00:55:50 +0300

    Use new model count as a condition instead of plan.new_snapshots

--- sqlmesh/core/plan/evaluator.py
+++ sqlmesh/core/plan/evaluator.py
@@ -134,9 +134,10 @@ class BuiltInPlanEvaluator(PlanEvaluator):
             plan: The plan to source snapshots from.
         """
         snapshot_id_to_snapshot = {s.snapshot_id: s for s in plan.snapshots}
+        new_model_count = len([s for s in plan.new_snapshots if s.is_model])
 
-        if plan.new_snapshots:
-            self.console.start_creation_progress(len([s for s in plan.new_snapshots if s.is_model]))
+        if new_model_count > 0:
+            self.console.start_creation_progress(new_model_count)
 
         def on_complete(snapshot: SnapshotInfoLike) -> None:
             self.console.update_creation_progress(1)
