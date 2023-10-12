bc5270fada65a2c7ece8f71b1c34f29959c3fc06
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2023-10-12 14:47:19 -0700

    chore: fix debugging comment

--- tests/core/engine_adapter/test_integration.py
+++ tests/core/engine_adapter/test_integration.py
@@ -192,14 +192,14 @@ def config() -> Config:
 
 @pytest.fixture(
     params=[
-        # "duckdb",
-        # pytest.param("postgres", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
-        # pytest.param("mysql", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
-        # pytest.param("bigquery", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
-        # pytest.param("databricks", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        "duckdb",
+        pytest.param("postgres", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        pytest.param("mysql", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        pytest.param("bigquery", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        pytest.param("databricks", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
         pytest.param("redshift", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
-        # pytest.param("snowflake", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
-        # pytest.param("mssql", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        pytest.param("snowflake", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
+        pytest.param("mssql", marks=[pytest.mark.integration, pytest.mark.engine_integration]),
     ]
 )
 def engine_adapter(request, config) -> EngineAdapter:
