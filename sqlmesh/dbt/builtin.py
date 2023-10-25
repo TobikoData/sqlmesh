fbc1a3a8708a838234fd108a75c4e0042f980647
Chris Rericha <67359577+crericha@users.noreply.github.com>
2023-10-25 12:59:59 -0400

    Fix: Allow dbt project ref, source, and config macros to override the builtin version and use the logger for dbt logging macros (#1606)

--- sqlmesh/dbt/builtin.py
+++ sqlmesh/dbt/builtin.py
@@ -37,7 +37,7 @@ class Exceptions:
             raise CompilationException(msg)
 
     def warn(self, msg: str) -> str:
-        print(msg)
+        logger.warning(msg)
         return ""
 
 
@@ -146,11 +146,7 @@ def env_var(name: str, default: t.Optional[str] = None) -> t.Optional[str]:
 
 
 def log(msg: str, info: bool = False) -> str:
-    print(msg)
-    return ""
-
-
-def no_log(msg: str, info: bool = False) -> str:
+    logger.debug(msg)
     return ""
 
 
@@ -260,9 +256,9 @@ BUILTIN_GLOBALS = {
     "flags": Flags(),
     "fromjson": from_json,
     "fromyaml": from_yaml,
-    "log": no_log,
+    "log": log,
     "modules": Modules(),
-    "print": no_log,
+    "print": log,
     "return": return_val,
     "set": to_set,
     "set_strict": set,
@@ -337,14 +333,12 @@ def create_builtin_globals(
             table_mapping=jinja_globals.get("table_mapping", {}),
             is_dev=jinja_globals.get("is_dev", False),
         )
-        builtin_globals.update({"log": log, "print": log})
     else:
         adapter = ParsetimeAdapter(
             jinja_macros,
             jinja_globals={**builtin_globals, **jinja_globals},
             dialect=dialect,
         )
-        builtin_globals.update({"log": no_log, "print": no_log})
 
     sql_execution = SQLExecution(adapter)
     builtin_globals.update(
