a24e16118480ad0fcae8c7ba2ad353d4a359edc7
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-10-27 14:28:56 -0700

    Feat: Make sure 'sqlmesh run' evaluates against a finalized environment (#1625)

--- sqlmesh/core/config/__init__.py
+++ sqlmesh/core/config/__init__.py
@@ -18,4 +18,5 @@ from sqlmesh.core.config.gateway import GatewayConfig
 from sqlmesh.core.config.loader import load_config_from_paths, load_config_from_yaml
 from sqlmesh.core.config.model import ModelDefaultsConfig
 from sqlmesh.core.config.root import Config
+from sqlmesh.core.config.run import RunConfig
 from sqlmesh.core.config.scheduler import AirflowSchedulerConfig, BuiltInSchedulerConfig
