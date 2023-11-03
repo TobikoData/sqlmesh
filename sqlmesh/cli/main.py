af797d537c8edc8404dda615f9cbe2d51dfcf5a4
z3z1ma <butler.alex2010@gmail.com>
2023-11-03 13:39:55 -0700

    feat: opt disable pretty printing of model sql in render

--- sqlmesh/cli/main.py
+++ sqlmesh/cli/main.py
@@ -143,6 +143,9 @@ def init(
     type=str,
     help="The SQL dialect to render the query as.",
 )
+@click.option(
+    "--no-pretty-print", is_flag=True, help="Disable pretty printing of the query."
+)
 @click.pass_context
 @error_handler
 def render(
@@ -153,6 +156,7 @@ def render(
     execution_time: t.Optional[TimeLike] = None,
     expand: t.Optional[t.Union[bool, t.Iterable[str]]] = None,
     dialect: t.Optional[str] = None,
+    no_pretty_print: bool = False,
 ) -> None:
     """Renders a model's query, optionally expanding referenced models."""
     rendered = ctx.obj.render(
@@ -164,7 +168,10 @@ def render(
     )
 
     sql = rendered.sql(pretty=True, dialect=ctx.obj.config.dialect if dialect is None else dialect)
-    ctx.obj.console.show_sql(sql)
+    if no_pretty_print:
+        print(sql)
+    else:
+        ctx.obj.console.show_sql(sql)
 
 
 @cli.command("evaluate")
