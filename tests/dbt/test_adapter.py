2ee913b3aa3ae37867e492765168ad402c30face
Jo <46752250+GeorgeSittas@users.noreply.github.com>
2023-11-06 23:33:41 +0000

    Fix!: normalize identifiers before calling engine adapter methods in dbt (#1661)
    * Fix!: normalize dbt identifiers before hitting engine adapter calls
    
    * Minor fixups
    
    * Pass the engine adapter's dialect to normalize_identifiers
    
    * Add some tests
    
    * Add more tests
    
    * Add dbt-snowflake as a dev dependency
    
    * PR feedback
    
    * Fixups

--- tests/dbt/test_adapter.py
+++ tests/dbt/test_adapter.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 import typing as t
+from unittest.mock import call
 
 import pytest
 from dbt.adapters.base import BaseRelation
 from dbt.adapters.base.column import Column
+from dbt.contracts.relation import Policy
 from pytest_mock.plugin import MockerFixture
 from sqlglot import exp
 
+from sqlmesh.core.dialect import schema_
 from sqlmesh.dbt.project import Project
+from sqlmesh.dbt.target import SnowflakeConfig
 from sqlmesh.utils.errors import ConfigError
 
 
@@ -57,6 +61,83 @@ def test_adapter_relation(sushi_test_project: Project, runtime_renderer: t.Calla
         )
         == "[]"
     )
+
+
+def test_normalization(
+    sushi_test_project: Project, runtime_renderer: t.Callable, mocker: MockerFixture
+):
+    context = sushi_test_project.context
+    assert context.target
+
+    adapter_mock = mocker.MagicMock()
+    adapter_mock.dialect = "snowflake"
+
+    context.target = SnowflakeConfig(
+        account="test",
+        user="test",
+        authenticator="test",
+        name="test",
+        database="test",
+        schema="test",
+    )
+
+    renderer = runtime_renderer(context, engine_adapter=adapter_mock)
+
+    # bla and bob will be normalized to uppercase since we're dealing with Snowflake
+    schema_bla = schema_("BLA")
+    relation_bla_bob = exp.table_("BOB", db="BLA")
+
+    renderer("{{ adapter.get_relation(database=None, schema='bla', identifier='bob') }}")
+    adapter_mock._get_data_objects.assert_has_calls([call(schema_bla)])
+
+    renderer(
+        "{%- set relation = api.Relation.create(schema='bla') -%}"
+        "{{ adapter.create_schema(relation) }}"
+    )
+    adapter_mock.create_schema.assert_has_calls([call(schema_bla)])
+
+    renderer(
+        "{%- set relation = api.Relation.create(schema='bla') -%}"
+        "{{ adapter.drop_schema(relation) }}"
+    )
+    adapter_mock.drop_schema.assert_has_calls([call(schema_bla)])
+
+    renderer(
+        "{%- set relation = api.Relation.create(schema='bla', identifier='bob') -%}"
+        "{{ adapter.drop_relation(relation) }}"
+    )
+    adapter_mock.drop_table.assert_has_calls([call(relation_bla_bob)])
+
+    select_star_query: exp.Select = exp.maybe_parse("SELECT * FROM t", dialect="snowflake")
+
+    # The following call to run_query won't return dataframes and so we're expected to
+    # raise in adapter.execute right before returning from the method
+    with pytest.raises(AssertionError):
+        renderer("{{ run_query('SELECT * FROM t') }}")
+    adapter_mock.fetchdf.assert_has_calls([call(select_star_query, quote_identifiers=False)])
+
+    renderer("{% call statement('something') %} {{ 'SELECT * FROM t' }} {% endcall %}")
+    adapter_mock.execute.assert_has_calls([call(select_star_query, quote_identifiers=False)])
+
+    # Enforce case-sensitivity for database object names
+    setattr(
+        context.target.__class__,
+        "quote_policy",
+        Policy(database=True, schema=True, identifier=True),
+    )
+
+    adapter_mock.drop_table.reset_mock()
+    renderer = runtime_renderer(context, engine_adapter=adapter_mock)
+
+    bla_id = exp.to_identifier("bla", quoted=True)
+    bob_id = exp.to_identifier("bob", quoted=True)
+
+    # Ensures we'll pass lowercase names to the engine
+    renderer(
+        "{%- set relation = api.Relation.create(schema='bla', identifier='bob') -%}"
+        "{{ adapter.drop_relation(relation) }}"
+    )
+    adapter_mock.drop_table.assert_has_calls([call(exp.table_("bob", db="bla", quoted=True))])
 
 
 def test_adapter_dispatch(sushi_test_project: Project, runtime_renderer: t.Callable):
