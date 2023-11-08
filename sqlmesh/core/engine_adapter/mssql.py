d190a01558927565057316fc8ca029270a8f6148
Trey Spiller <treyspiller@gmail.com>
2023-11-08 11:53:02 -0600

    Bulk copy timestamps with TZ to text field

--- sqlmesh/core/engine_adapter/mssql.py
+++ sqlmesh/core/engine_adapter/mssql.py
@@ -182,6 +182,8 @@ class MSSQLEngineAdapter(
         temp_table = self._get_temp_table(target_table or "pandas")
 
         def query_factory() -> Query:
+            columns_to_types_tztext = None
+
             # pymssql doesn't convert Pandas Timestamp (datetime64) types
             # - this code is based on snowflake adapter implementation
             for column, kind in (columns_to_types or {}).items():
@@ -191,14 +193,25 @@ class MSSQLEngineAdapter(
                     elif getattr(df.dtypes[column], "tz", None) is not None:  # type: ignore
                         # MSSQL requires a colon in the offset (+00:00) so we use isoformat() instead of strftime()
                         df[column] = pd.to_datetime(df[column]).map(lambda x: x.isoformat(" "))  # type: ignore
+
+                        # bulk_copy() doesn't work with TZ timestamp, so load into string column and cast to
+                        # timestamp in SELECT statement
+                        columns_to_types_tztext = columns_to_types.copy()
+                        columns_to_types_tztext[column] = exp.DataType.build("TEXT")
                     else:  # type: ignore
                         df[column] = pd.to_datetime(df[column]).dt.strftime("%Y-%m-%d %H:%M:%S.%f")  # type: ignore
 
-            self.create_table(temp_table, columns_to_types)
+            self.create_table(
+                temp_table, columns_to_types_tztext if columns_to_types_tztext else columns_to_types
+            )
             rows: t.List[t.Tuple[t.Any, ...]] = list(df.itertuples(index=False, name=None))  # type: ignore
             conn = self._connection_pool.get()
             conn.bulk_copy(temp_table.sql(dialect=self.dialect), rows)
-            return exp.select(*columns_to_types).from_(temp_table)
+            return exp.select(
+                *self._casted_columns(columns_to_types)  # type: ignore
+                if columns_to_types_tztext
+                else columns_to_types
+            ).from_(temp_table)
 
         return [
             SourceQuery(
