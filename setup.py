13abc2038d30bdb570e8c4d4abe48017a955ec0e
Jo <46752250+georgesittas@users.noreply.github.com>
2023-11-10 22:38:29 +0000

    Chore!: bump sqlglot to v19.3.0 (#1697)
    * Chore!: bump sqlglot to v19.3.0
    
    * Fix MSSQL tests

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot~=19.2.0",
+        "sqlglot~=19.3.0",
     ],
     extras_require={
         "bigquery": [
