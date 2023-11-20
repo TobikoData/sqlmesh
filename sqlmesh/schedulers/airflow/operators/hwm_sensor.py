e2203ef6d8a4eaf4d2718a0595fc90ee96536c5e
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2023-11-20 13:28:08 -0800

    Fix: Include the external sensor operator for relevant snapshots as part of the plan DAG (#1739)

--- sqlmesh/schedulers/airflow/operators/hwm_sensor.py
+++ sqlmesh/schedulers/airflow/operators/hwm_sensor.py
@@ -10,7 +10,7 @@ from airflow.utils.context import Context
 
 from sqlmesh.core.snapshot import Snapshot, SnapshotTableInfo
 from sqlmesh.schedulers.airflow import util
-from sqlmesh.utils.date import now, to_datetime
+from sqlmesh.utils.date import TimeLike, now, to_datetime
 
 if t.TYPE_CHECKING:
     from airflow.sensors.base import PokeReturnValue
@@ -78,6 +78,8 @@ class HighWaterMarkExternalSensor(BaseSensorOperator):
         poke_interval: float = 60.0,
         timeout: float = 7.0 * 24.0 * 60.0 * 60.0,  # 7 days
         mode: str = "reschedule",
+        start: t.Optional[TimeLike] = None,
+        end: t.Optional[TimeLike] = None,
         **kwargs: t.Any,
     ):
         super().__init__(
@@ -88,12 +90,15 @@ class HighWaterMarkExternalSensor(BaseSensorOperator):
         )
         self.snapshot = snapshot
         self.external_table_sensor_factory = external_table_sensor_factory
+        self.start = start
+        self.end = end
 
     def poke(self, context: Context) -> t.Union[bool, PokeReturnValue]:
+        interval_unit = self.snapshot.node.interval_unit
         dag_run = context["dag_run"]
         signals = self.snapshot.model.render_signals(
-            start=to_datetime(dag_run.data_interval_start),
-            end=to_datetime(dag_run.data_interval_end),
+            start=interval_unit.cron_floor(self.start or dag_run.data_interval_start),
+            end=interval_unit.cron_floor(self.end or dag_run.data_interval_end),
             execution_time=now(minute_floor=False),
         )
         delegates = [self.external_table_sensor_factory(signal) for signal in signals]
