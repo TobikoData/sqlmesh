36652fad67309606cb89318fcd00ed6acbb9be60
Max Mykal <mykalmax@yahoo.com>
2023-11-28 14:05:21 -0800

    remove duplicate

--- web/client/src/library/components/plan/PlanActions.tsx
+++ web/client/src/library/components/plan/PlanActions.tsx
@@ -99,65 +99,65 @@ export default function PlanActions({
         isApplyVirtual ||
         isApplyBackfill ||
         isApplying) && (
-          <>
-            <div className="w-full flex px-4 py-2">
-              <p className="ml-2 text-xs">
-                <span>Plan for</span>
-                <b className="text-primary-500 font-bold mx-1">
-                  {environment.name}
-                </b>
-                <span className="inline-block mr-1">environment</span>
-                {
-                  <span className="inline-block mr-1">
-                    from{' '}
-                    <b>
-                      {isFalse(isStringEmptyOrNil(start))
-                        ? start
-                        : 'the begining of history'}
-                    </b>
-                  </span>
-                }
-                {
-                  <span className="inline-block mr-1">
-                    till{' '}
-                    <b>{isFalse(isStringEmptyOrNil(start)) ? end : 'today'}</b>
-                  </span>
-                }
-                {no_gaps && (
-                  <span className="inline-block mr-1">
-                    with <b>No Gaps</b>
-                  </span>
-                )}
-                {skip_backfill && (
-                  <span className="inline-block mr-1">
-                    without <b>Backfills</b>
-                  </span>
-                )}
-                {forward_only && (
-                  <span className="inline-block mr-1">
-                    consider as a <b>Breaking Change</b>
-                  </span>
-                )}
-                {no_auto_categorization && (
-                  <span className="inline-block mr-1">
-                    also set <b>Change Category</b> manually
-                  </span>
-                )}
-                {isFalse(isStringEmptyOrNil(restate_models)) && (
-                  <span className="inline-block mr-1">
-                    and restate folowing models <b>{restate_models}</b>
-                  </span>
-                )}
-                {include_unmodified && (
-                  <span className="inline-block mr-1">
-                    with views for all models
-                  </span>
-                )}
-              </p>
-            </div>
-            <Divider />
-          </>
-        )}
+        <>
+          <div className="w-full flex px-4 py-2">
+            <p className="ml-2 text-xs">
+              <span>Plan for</span>
+              <b className="text-primary-500 font-bold mx-1">
+                {environment.name}
+              </b>
+              <span className="inline-block mr-1">environment</span>
+              {
+                <span className="inline-block mr-1">
+                  from{' '}
+                  <b>
+                    {isFalse(isStringEmptyOrNil(start))
+                      ? start
+                      : 'the begining of history'}
+                  </b>
+                </span>
+              }
+              {
+                <span className="inline-block mr-1">
+                  till{' '}
+                  <b>{isFalse(isStringEmptyOrNil(start)) ? end : 'today'}</b>
+                </span>
+              }
+              {no_gaps && (
+                <span className="inline-block mr-1">
+                  with <b>No Gaps</b>
+                </span>
+              )}
+              {skip_backfill && (
+                <span className="inline-block mr-1">
+                  without <b>Backfills</b>
+                </span>
+              )}
+              {forward_only && (
+                <span className="inline-block mr-1">
+                  consider as a <b>Breaking Change</b>
+                </span>
+              )}
+              {no_auto_categorization && (
+                <span className="inline-block mr-1">
+                  also set <b>Change Category</b> manually
+                </span>
+              )}
+              {isFalse(isStringEmptyOrNil(restate_models)) && (
+                <span className="inline-block mr-1">
+                  and restate folowing models <b>{restate_models}</b>
+                </span>
+              )}
+              {include_unmodified && (
+                <span className="inline-block mr-1">
+                  with views for all models
+                </span>
+              )}
+            </p>
+          </div>
+          <Divider />
+        </>
+      )}
       <div className="flex justify-between px-4 py-2">
         <div className="flex w-full items-center">
           {(isRun || isRunning) && (
@@ -235,19 +235,6 @@ export default function PlanActions({
             {getActionName(planAction, [EnumPlanAction.Done], 'Close')}
           </Button>
         </div>
-        <Button
-          onClick={handleClose}
-          variant={isDone ? EnumVariant.Primary : EnumVariant.Neutral}
-          disabled={
-            includes(
-              [EnumPlanAction.Running, EnumPlanAction.Cancelling],
-              planAction,
-            ) || disabled
-          }
-          ref={isDone || isApplying ? setFocus : undefined}
-        >
-          {getActionName(planAction, [EnumPlanAction.Done], 'Close')}
-        </Button>
       </div>
     </div>
   )
