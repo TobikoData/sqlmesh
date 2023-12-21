128497bf84388469d1836390a9b48202a3a98b6d
Ryan Eakman <6326532+eakmanrq@users.noreply.github.com>
2023-12-21 14:45:15 -0800

    fix: properly quote set catalog (#1882)

---
+++ tests/core/engine_adapter/test_trino.py
@@ -0,0 +1,13 @@
+import typing as t
+
+from sqlmesh.core.engine_adapter import TrinoEngineAdapter
+from tests.core.engine_adapter import to_sql_calls
+
+
+def test_set_current_catalog(make_mocked_engine_adapter: t.Callable, duck_conn):
+    adapter = make_mocked_engine_adapter(TrinoEngineAdapter)
+    adapter.set_current_catalog("test_catalog")
+
+    assert to_sql_calls(adapter) == [
+        'USE "information_schema"."test_catalog"',
+    ]
