cb5c21014c1d3cc3ca75e74e6dd840a7db6b6015
Vincent Chan <vchan@users.noreply.github.com>
2024-01-19 17:14:28 -0800

    Feat: Add get model endpoint. (#1990)
    * Feat: Add get model endpoint. Make query rendering optional.
    
    * PR feedback

--- web/server/api/endpoints/models.py
+++ web/server/api/endpoints/models.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+import pathlib
 import typing as t
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlglot import exp
+from starlette.status import HTTP_404_NOT_FOUND
 
 from sqlmesh.core.context import Context
 from sqlmesh.core.model import Model
@@ -23,87 +25,105 @@ router = APIRouter()
 def get_models(
     context: Context = Depends(get_loaded_context),
 ) -> t.List[models.Model]:
-    """Get a mapping of model names to model metadata"""
-    return get_all_models(context)
+    """Get a list of models"""
+    return serialize_all_models(context)
 
 
-def get_all_models(context: Context) -> t.List[models.Model]:
-    output = []
+@router.get(
+    "/{name:str}",
+    response_model=models.Model,
+    response_model_exclude_unset=True,
+    response_model_exclude_none=True,
+)
+def get_model(
+    name: str,
+    context: Context = Depends(get_loaded_context),
+) -> models.Model:
+    """Get a single model"""
+    model = context.get_model(name)
+    if not model:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND)
+    return serialize_model(model, context.path, render_query=True)
 
-    def _get_model_type(model: Model) -> str:
-        if model.is_sql:
-            return models.ModelType.SQL
-        if model.is_python:
-            return models.ModelType.PYTHON
-        if model.is_seed:
-            return models.ModelType.SEED
-        return models.ModelType.EXTERNAL
 
-    for model in context.models.values():
-        type = _get_model_type(model)
-        default_catalog = model.default_catalog
-        dialect = model.dialect or "SQLGlot"
-        time_column = (
-            f"{model.time_column.column} | {model.time_column.format}"
-            if model.time_column
-            else None
+def serialize_all_models(context: Context) -> t.List[models.Model]:
+    return [serialize_model(model, context.path) for model in context.models.values()]
+
+
+def serialize_model(
+    model: Model, context_path: pathlib.Path, render_query: bool = False
+) -> models.Model:
+    type = _get_model_type(model)
+    default_catalog = model.default_catalog
+    dialect = model.dialect or "SQLGlot"
+    time_column = (
+        f"{model.time_column.column} | {model.time_column.format}" if model.time_column else None
+    )
+    tags = ", ".join(model.tags) if model.tags else None
+    partitioned_by = (
+        ", ".join(expr.sql(pretty=True, dialect=model.dialect) for expr in model.partitioned_by)
+        if model.partitioned_by
+        else None
+    )
+    clustered_by = ", ".join(model.clustered_by) if model.clustered_by else None
+    lookback = model.lookback if model.lookback > 0 else None
+    columns_to_types = model.columns_to_types or {}
+    columns = [
+        models.Column(
+            name=name, type=str(data_type), description=model.column_descriptions.get(name)
         )
-        tags = ", ".join(model.tags) if model.tags else None
-        partitioned_by = (
-            ", ".join(expr.sql(pretty=True, dialect=model.dialect) for expr in model.partitioned_by)
-            if model.partitioned_by
-            else None
-        )
-        clustered_by = ", ".join(model.clustered_by) if model.clustered_by else None
-        lookback = model.lookback if model.lookback > 0 else None
-        columns_to_types = model.columns_to_types or {}
-        columns = [
-            models.Column(
-                name=name, type=str(data_type), description=model.column_descriptions.get(name)
-            )
-            for name, data_type in columns_to_types.items()
-        ]
-        details = models.ModelDetails(
-            owner=model.owner,
-            kind=model.kind.name,
-            batch_size=model.batch_size,
-            cron=model.cron,
-            stamp=model.stamp,
-            start=model.start,
-            retention=model.retention,
-            storage_format=model.storage_format,
-            time_column=time_column,
-            tags=tags,
-            references=[
-                models.Reference(name=ref.name, expression=ref.expression.sql(), unique=ref.unique)
-                for ref in model.all_references
-            ],
-            partitioned_by=partitioned_by,
-            clustered_by=clustered_by,
-            lookback=lookback,
-            cron_prev=to_datetime(model.cron_prev(value=now())),
-            cron_next=to_datetime(model.cron_next(value=now())),
-            interval_unit=model.interval_unit,
-            annotated=model.annotated,
-        )
+        for name, data_type in columns_to_types.items()
+    ]
+    details = models.ModelDetails(
+        owner=model.owner,
+        kind=model.kind.name,
+        batch_size=model.batch_size,
+        cron=model.cron,
+        stamp=model.stamp,
+        start=model.start,
+        retention=model.retention,
+        storage_format=model.storage_format,
+        time_column=time_column,
+        tags=tags,
+        references=[
+            models.Reference(name=ref.name, expression=ref.expression.sql(), unique=ref.unique)
+            for ref in model.all_references
+        ],
+        partitioned_by=partitioned_by,
+        clustered_by=clustered_by,
+        lookback=lookback,
+        cron_prev=to_datetime(model.cron_prev(value=now())),
+        cron_next=to_datetime(model.cron_next(value=now())),
+        interval_unit=model.interval_unit,
+        annotated=model.annotated,
+    )
 
+    sql = None
+    if render_query:
         query = model.render_query() or (
             model.query if hasattr(model, "query") else exp.select('"FAILED TO RENDER QUERY"')
         )
+        sql = query.sql(pretty=True, dialect=model.dialect)
 
-        output.append(
-            models.Model(
-                name=model.name,
-                fqn=model.fqn,
-                path=str(model._path.relative_to(context.path)),
-                dialect=dialect,
-                columns=columns,
-                details=details,
-                description=model.description,
-                sql=query.sql(pretty=True, dialect=model.dialect),
-                type=type,
-                default_catalog=default_catalog,
-            )
-        )
+    return models.Model(
+        name=model.name,
+        fqn=model.fqn,
+        path=str(model._path.relative_to(context_path)),
+        dialect=dialect,
+        columns=columns,
+        details=details,
+        description=model.description,
+        sql=sql,
+        type=type,
+        default_catalog=default_catalog,
+    )
 
-    return output
+
+def _get_model_type(model: Model) -> str:
+    if model.is_sql:
+        return models.ModelType.SQL
+    if model.is_python:
+        return models.ModelType.PYTHON
+    if model.is_seed:
+        return models.ModelType.SEED
+    return models.ModelType.EXTERNAL
