8500529bad766f28083f095e97e63f5369705ec4
Max Mykal <mykalmax@yahoo.com>
2024-01-29 14:09:39 -0800

    Fix: Bg for modal component disappear (#2044)

--- web/client/src/library/components/modal/Modal.tsx
+++ web/client/src/library/components/modal/Modal.tsx
@@ -25,18 +25,7 @@ export default function Modal({
         className="relative z-50"
         onClose={onClose}
       >
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="bg-overlay opacity-0"
-          enterTo="bg-overlay opacity-90"
-          leave="ease-in duration-200"
-          leaveFrom="bg-overlay opacity-90"
-          leaveTo="bg-overlay opacity-0"
-        >
-          <div className="fixed inset-0 " />
-        </Transition.Child>
-
+        <div className="fixed inset-0 bg-overlay opacity-90" />
         <div className="fixed inset-0 overflow-y-auto">
           <div className="flex min-h-full items-center justify-center p-4 text-center">
             <Transition.Child
