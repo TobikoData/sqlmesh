144b7455a69bbc561a7e936b02f28047c9f1f461
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2024-02-08 17:18:02 -0800

    chore: fix pydantic 1 support with new type

--- sqlmesh/utils/pydantic.py
+++ sqlmesh/utils/pydantic.py
@@ -7,7 +7,6 @@ from functools import cached_property, wraps
 
 import pydantic
 from pydantic.fields import FieldInfo
-from pydantic_core.core_schema import ValidationInfo
 from sqlglot import exp, parse_one
 from sqlglot.helper import ensure_list
 from sqlglot.optimizer.normalize_identifiers import normalize_identifiers
@@ -34,6 +33,9 @@ PYDANTIC_MAJOR_VERSION, PYDANTIC_MINOR_VERSION = [int(p) for p in pydantic.__ver
 
 if PYDANTIC_MAJOR_VERSION >= 2:
 
+    if t.TYPE_CHECKING:
+        from pydantic_core.core_schema import ValidationInfo
+
     def field_validator(*args: t.Any, **kwargs: t.Any) -> t.Callable[[t.Any], t.Any]:
         # Pydantic v2 doesn't support "always" argument. The validator behaves as if "always" is True.
         kwargs.pop("always", None)
@@ -48,6 +50,8 @@ if PYDANTIC_MAJOR_VERSION >= 2:
         return pydantic.field_serializer(*args, **kwargs)  # type: ignore
 
 else:
+    if t.TYPE_CHECKING:
+        ValidationInfo = t.Dict[str, t.Any]
 
     def field_validator(*args: t.Any, **kwargs: t.Any) -> t.Callable[[t.Any], t.Any]:
         mode = kwargs.pop("mode", "after")
@@ -293,7 +297,7 @@ def positive_int_validator(v: t.Any) -> int:
 
 def _get_fields(
     v: t.Any,
-    values: t.Union[t.Dict, ValidationInfo],
+    values: ValidationInfo,
 ) -> t.List[exp.Expression]:
     values = values if isinstance(values, dict) else values.data
     dialect = values.get("dialect")
@@ -321,15 +325,11 @@ def _get_fields(
     return results
 
 
-def list_of_fields_validator(
-    v: t.Any, values: t.Union[t.Dict, ValidationInfo]
-) -> t.List[exp.Expression]:
+def list_of_fields_validator(v: t.Any, values: ValidationInfo) -> t.List[exp.Expression]:
     return _get_fields(v, values)
 
 
-def list_of_columns_validator(
-    v: t.Any, values: t.Union[t.Dict, ValidationInfo]
-) -> t.List[exp.Column]:
+def list_of_columns_validator(v: t.Any, values: ValidationInfo) -> t.List[exp.Column]:
     expressions = _get_fields(v, values)
     for expression in expressions:
         if not isinstance(expression, exp.Column):
@@ -338,7 +338,7 @@ def list_of_columns_validator(
 
 
 def list_of_columns_or_star_validator(
-    v: t.Any, values: t.Union[t.Dict, ValidationInfo]
+    v: t.Any, values: ValidationInfo
 ) -> t.Union[exp.Star, t.List[exp.Column]]:
     expressions = _get_fields(v, values)
     if len(expressions) == 1 and isinstance(expressions[0], exp.Star):
