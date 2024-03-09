3e8086205a9445347a8b2c34ea81612c8f0db0fa
George Sittas <giwrgos.sittas@gmail.com>
2024-03-09 11:34:12 +0200

    Bump to 22.3.1 to fix copy issue

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=22.3.0",
+        "sqlglot[rs]~=22.3.1",
     ],
     extras_require={
         "bigquery": [
