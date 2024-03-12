486b8f621af220311a1e5d19b3a348826be8a9f3
tobymao <toby.mao@gmail.com>
2024-03-12 11:33:32 -0700

    refactor so output is less redundant

--- sqlmesh/core/audit/builtin.py
+++ sqlmesh/core/audit/builtin.py
@@ -7,18 +7,19 @@ from sqlmesh.core.audit.definition import ModelAudit
 # not_null(columns=(column_1, column_2))
 not_null_audit = ModelAudit(
     name="not_null",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND (
+WHERE @AND(
   @REDUCE(
     @EACH(
       @columns,
       c -> c IS NULL
     ),
     (l, r) -> l OR r
-  )
+  ),
+  @condition,
 )
     """,
 )
@@ -26,7 +27,7 @@ WHERE @condition AND (
 # unique_values(columns=(column_1, column_2))
 unique_values_audit = ModelAudit(
     name="unique_values",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM (
@@ -36,7 +37,7 @@ FROM (
       c -> row_number() OVER (PARTITION BY c ORDER BY c) AS rank_@c
     )
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 )
 WHERE @REDUCE(
   @EACH(
@@ -51,24 +52,24 @@ WHERE @REDUCE(
 # accepted_values(column=column_name, is_in=(1, 2, 3))
 accepted_values_audit = ModelAudit(
     name="accepted_values",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND @column NOT IN @is_in
+WHERE @AND(@column NOT IN @is_in, @condition)
 """,
 )
 
 # number_of_rows(threshold=100)
 number_of_rows_audit = ModelAudit(
     name="number_of_rows",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT COUNT(*)
 FROM (
   SELECT 1
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
   LIMIT @threshold + 1
 )
 HAVING COUNT(*) <= @threshold
@@ -104,18 +105,20 @@ accepted_range_audit = ModelAudit(
         "min_v": exp.null(),
         "max_v": exp.null(),
         "inclusive": exp.true(),
-        "condition": exp.true(),
+        "condition": exp.null(),
     },
     query="""
 SELECT *
 FROM @this_model
 WHERE
-  @condition
-  AND (
-    @IF(@min_v IS NOT NULL AND @inclusive, @column < @min_v, false)
-    OR @IF(@min_v IS NOT NULL AND NOT @inclusive, @column <= @min_v, false)
-    OR @IF(@max_v IS NOT NULL AND @inclusive, @column > @max_v, false)
-    OR @IF(@max_v IS NOT NULL AND NOT @inclusive, @column >= @max_v, false)
+  @AND(
+    @OR(
+      @IF(@min_v IS NOT NULL AND @inclusive, @column < @min_v, NULL),
+      @IF(@min_v IS NOT NULL AND NOT @inclusive, @column <= @min_v, NULL),
+      @IF(@max_v IS NOT NULL AND @inclusive, @column > @max_v, NULL),
+      @IF(@max_v IS NOT NULL AND NOT @inclusive, @column >= @max_v, NULL),
+    ),
+    @condition,
   )
     """,
 )
@@ -123,11 +126,11 @@ WHERE
 # at_least_one(column=column_name)
 at_least_one_audit = ModelAudit(
     name="at_least_one",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT 1
 FROM @this_model
-WHERE @condition
+@WHERE(@condition) @condition
 GROUP BY 1
 HAVING COUNT(@column) = 0
     """,
@@ -136,13 +139,13 @@ HAVING COUNT(@column) = 0
 # not_constant(column=column_name)
 not_constant_audit = ModelAudit(
     name="not_constant",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT 1
 FROM (
   SELECT COUNT(DISTINCT @column) AS t_cardinality
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 ) AS r
 WHERE r.t_cardinality <= 1
     """,
@@ -151,18 +154,18 @@ WHERE r.t_cardinality <= 1
 # not_empty_string(column=column_name)
 not_empty_string_audit = ModelAudit(
     name="not_empty_string",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND @column = ''
+WHERE @AND(@column = '', @condition)
     """,
 )
 
 # not_null_proportion(column=column_name, threshold=0.9)
 not_null_proportion_audit = ModelAudit(
     name="not_null_proportion",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM (
@@ -171,7 +174,7 @@ FROM (
     count(@column) as cnt_not_null,
     count(*) - count(@column) as cnt_null
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 ) AS s
 WHERE s.cnt_not_null <= s.cnt_tot * @threshold
     """,
@@ -180,11 +183,11 @@ WHERE s.cnt_not_null <= s.cnt_tot * @threshold
 # not_accepted_values(column=column_name, is_in=(1, 2, 3))
 not_accepted_values_audit = ModelAudit(
     name="not_accepted_values",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND @column IN @is_in
+WHERE @AND(@column IN @is_in, @condition)
 """,
 )
 
@@ -192,7 +195,7 @@ WHERE @condition AND @column IN @is_in
 # TODO: support grouping
 sequential_values_audit = ModelAudit(
     name="sequential_values",
-    defaults={"interval": exp.Literal.number(1), "condition": exp.true()},
+    defaults={"interval": exp.Literal.number(1), "condition": exp.null()},
     query="""
 WITH windowed AS (
   SELECT
@@ -201,7 +204,7 @@ WITH windowed AS (
       ORDER BY @column
     ) AS prv
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 ), validation_errors AS (
     SELECT *
     FROM windowed
@@ -216,11 +219,11 @@ FROM validation_errors
 # unique_combination_of_columns(columns=(column_1, column_2))
 unique_combination_of_columns_audit = ModelAudit(
     name="unique_combination_of_columns",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT @EACH(@columns, c -> c)
 FROM @this_model
-WHERE @condition
+@WHERE(@condition) @condition
 GROUP BY @EACH(@columns, c -> c)
 HAVING COUNT(*) > 1
     """,
@@ -230,7 +233,7 @@ HAVING COUNT(*) > 1
 # TODO: make inclusivity configurable
 mutually_exclusive_ranges_audit = ModelAudit(
     name="mutually_exclusive_ranges",
-    defaults={"partition_clause": exp.false(), "condition": exp.true()},
+    defaults={"partition_clause": exp.false(), "condition": exp.null()},
     query="""
 WITH window_functions AS (
   SELECT
@@ -245,7 +248,7 @@ WITH window_functions AS (
       ORDER BY @lower_bound_column desc, @upper_bound_column desc
     ) = 1 AS is_last_record
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 ), calc AS (
   SELECT
     *,
@@ -276,25 +279,26 @@ FROM validation_errors
 # valid_uuid(column=column_name)
 valid_uuid_audit = ModelAudit(
     name="valid_uuid",
-    defaults={"condition": exp.true()},
-    dialect="postgres",
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition 
-  AND NOT LOWER(@column) ~ '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+WHERE @AND(
+  NOT REGEXP_LIKE(LOWER(@column), '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'),
+  @condition,
+)
+
     """,
 )
 
 # valid_url(column=column_name)
 valid_url_audit = ModelAudit(
     name="valid_url",
-    defaults={"condition": exp.true()},
-    dialect="postgres",
+    defaults={"condition": exp.null()},
     query=r"""
 SELECT *
 FROM @this_model
-WHERE @condition AND NOT @column ~ '^(https?|ftp)://[^\s/$.?#].[^\s]*$'
+WHERE @AND(NOT REGEXP_LIKE(@column, '^(https?|ftp)://[^\s/$.?#].[^\s]*$'), @condition)
     """,
 )
 
@@ -311,31 +315,30 @@ WHERE NOT @column IN ('GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'
 # valid_email(column=column_name)
 valid_email_audit = ModelAudit(
     name="valid_email",
-    dialect="postgres",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query=r"""
 SELECT *
 FROM @this_model
-WHERE @condition AND NOT @column ~ '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+WHERE @AND(NOT REGEXP_LIKE(@column, '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'), @condition)
     """,
 )
 
 # match_regex_pattern_list(column=column_name, patterns=('^pattern_1', 'pattern_2$'))
 match_regex_pattern_list_audit = ModelAudit(
     name="match_regex_pattern_list",
-    defaults={"condition": exp.true()},
-    dialect="postgres",
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND (
+WHERE @AND(
   @REDUCE(
     @EACH(
       @patterns,
-      c -> NOT @column ~ c
+      c -> NOT REGEXP_LIKE(@column, c)
     ),
     (l, r) -> l OR r
-  )
+  ),
+  @condition,
 )
     """,
 )
@@ -343,19 +346,19 @@ WHERE @condition AND (
 # not_match_regex_pattern_list(column=column_name, patterns=('^pattern_1', 'pattern_2$'))
 not_match_regex_pattern_list_audit = ModelAudit(
     name="not_match_regex_pattern_list",
-    defaults={"condition": exp.true()},
-    dialect="postgres",
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND (
+WHERE @AND(
   @REDUCE(
     @EACH(
       @patterns,
-      c -> @column ~ c
+      c -> REGEXP_LIKE(@column, c)
     ),
     (l, r) -> l OR r
-  )
+  ),
+  @condition,
 )
     """,
 )
@@ -382,18 +385,19 @@ WHERE @condition AND (
 # not_match_like_pattern_list(column=column_name, patterns=('%pattern_1%', 'pattern_2%'))
 not_match_like_pattern_list_audit = ModelAudit(
     name="not_match_like_pattern_list",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND (
+WHERE @AND(
   @REDUCE(
     @EACH(
       @patterns,
       c -> @column LIKE c
     ),
     (l, r) -> l OR r
-  )
+  ),
+  @condition,
 )
     """,
 )
@@ -401,14 +405,14 @@ WHERE @condition AND (
 # z_score_audit(column=column_name, threshold=3)
 z_score_audit = ModelAudit(
     name="z_score",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 WITH stats AS (
   SELECT
     AVG(@column) AS mean_@column,
     STDDEV(@column) AS stddev_@column
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 )
 SELECT
   @column,
@@ -425,17 +429,20 @@ string_length_between_audit = ModelAudit(
         "min_v": exp.null(),
         "max_v": exp.null(),
         "inclusive": exp.true(),
-        "condition": exp.true(),
+        "condition": exp.null(),
     },
     query="""
 SELECT *
 FROM @this_model
 WHERE
-  @condition AND (
-    @IF(@min_v IS NOT NULL AND @inclusive, LENGTH(@column) < @min_v, false)
-    OR @IF(@min_v IS NOT NULL AND NOT @inclusive, LENGTH(@column) <= @min_v, false)
-    OR @IF(@max_v IS NOT NULL AND @inclusive, LENGTH(@column) > @max_v, false)
-    OR @IF(@max_v IS NOT NULL AND NOT @inclusive, LENGTH(@column) >= @max_v, false)
+  @AND(
+    @OR(
+      @IF(@min_v IS NOT NULL AND @inclusive, LENGTH(@column) < @min_v, NULL),
+      @IF(@min_v IS NOT NULL AND NOT @inclusive, LENGTH(@column) <= @min_v, NULL),
+      @IF(@max_v IS NOT NULL AND @inclusive, LENGTH(@column) > @max_v, NULL),
+      @IF(@max_v IS NOT NULL AND NOT @inclusive, LENGTH(@column) >= @max_v, NULL),
+    ),
+    @condition,
   )
     """,
 )
@@ -443,11 +450,11 @@ WHERE
 # string_length_equal_audit(column=column_name, v=22)
 string_length_equal_audit = ModelAudit(
     name="string_length_equal",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 SELECT *
 FROM @this_model
-WHERE @condition AND LENGTH(@column) != @v
+WHERE @AND(LENGTH(@column) != @v, @condition)
     """,
 )
 
@@ -458,21 +465,22 @@ stddev_in_range_audit = ModelAudit(
         "min_v": exp.null(),
         "max_v": exp.null(),
         "inclusive": exp.true(),
-        "condition": exp.true(),
+        "condition": exp.null(),
     },
     query="""
 SELECT *
 FROM (
   SELECT STDDEV(@column) AS stddev_@column
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 )
 WHERE
-  false
-  OR @IF(@min_v IS NOT NULL AND @inclusive, stddev_@column < @min_v, False)
-  OR @IF(@min_v IS NOT NULL AND NOT @inclusive, stddev_@column <= @min_v, False)
-  OR @IF(@max_v IS NOT NULL AND @inclusive, stddev_@column > @max_v, False)
-  OR @IF(@max_v IS NOT NULL AND NOT @inclusive, stddev_@column >= @max_v, False)
+  @OR(
+    @IF(@min_v IS NOT NULL AND @inclusive, stddev_@column < @min_v, NULL),
+    @IF(@min_v IS NOT NULL AND NOT @inclusive, stddev_@column <= @min_v, NULL),
+    @IF(@max_v IS NOT NULL AND @inclusive, stddev_@column > @max_v, NULL),
+    @IF(@max_v IS NOT NULL AND NOT @inclusive, stddev_@column >= @max_v, NULL),
+  )
     """,
 )
 
@@ -483,28 +491,29 @@ mean_in_range_audit = ModelAudit(
         "min_v": exp.null(),
         "max_v": exp.null(),
         "inclusive": exp.true(),
-        "condition": exp.true(),
+        "condition": exp.null(),
     },
     query="""
 SELECT *
 FROM (
   SELECT AVG(@column) AS mean_@column
   FROM @this_model
-  WHERE @condition
+  @WHERE(@condition) @condition
 )
 WHERE
-  False
-  OR @IF(@min_v IS NOT NULL AND @inclusive, mean_@column < @min_v, false)
-  OR @IF(@min_v IS NOT NULL AND NOT @inclusive, mean_@column <= @min_v, false)
-  OR @IF(@max_v IS NOT NULL AND @inclusive, mean_@column > @max_v, false)
-  OR @IF(@max_v IS NOT NULL AND NOT @inclusive, mean_@column >= @max_v, false)
+  @OR(
+    @IF(@min_v IS NOT NULL AND @inclusive, mean_@column < @min_v, NULL),
+    @IF(@min_v IS NOT NULL AND NOT @inclusive, mean_@column <= @min_v, NULL),
+    @IF(@max_v IS NOT NULL AND @inclusive, mean_@column > @max_v, NULL),
+    @IF(@max_v IS NOT NULL AND NOT @inclusive, mean_@column >= @max_v, NULL),
+  )
     """,
 )
 
 # kl_divergence(column=age, target_column=normalized_age, threshold=0.1)
 kl_divergence_audit = ModelAudit(
     name="kl_divergence",
-    defaults={"condition": exp.true()},
+    defaults={"condition": exp.null()},
     query="""
 WITH
   table_a AS (
@@ -512,7 +521,7 @@ WITH
       @source_column,
       COUNT(*) AS num_rows
     FROM @this_model
-    WHERE @condition
+    @WHERE(@condition) @condition
     GROUP BY @source_column
   ),
   table_b AS (
@@ -520,7 +529,7 @@ WITH
       @target_column,
       COUNT(*) AS num_rows
     FROM @this_model
-    WHERE @condition
+    @WHERE(@condition) @condition
     GROUP BY @target_column
   ),
   table_a_with_p AS (
@@ -603,7 +612,7 @@ chi_square_audit = ModelAudit(
         "@def(r, (SELECT COUNT(DISTINCT x_b) FROM contingency_table))",
         "@def(E, (tot_a * tot_b / g_t))",
     ],
-    defaults={"dependent": exp.true(), "condition": exp.true()},
+    defaults={"dependent": exp.true(), "condition": exp.null()},
     query="""
 WITH
   samples AS (
@@ -611,7 +620,7 @@ WITH
       @column_a AS x_a,
       @column_b AS x_b,
     FROM @this_model
-    WHERE @condition AND @column_a IS NOT NULL AND @column_b IS NOT NULL
+    WHERE @AND(@column_a IS NOT NULL AND @column_b IS NOT NULL, @condition)
   ),
   contingency_table AS (
     SELECT
