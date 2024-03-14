bd2e7ca11805ab101aa5e0e458cde7dcb4af832d
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2024-03-14 10:54:43 -0700

    chore: publish sqlmesh tests

---
+++ tests/setup.py
@@ -0,0 +1,25 @@
+import os
+from distutils.core import run_setup, setup
+
+os.chdir(os.path.join(os.path.dirname(__file__), ".."))
+sqlmesh_dist = run_setup("setup.py", stop_after="init")
+requirements = sqlmesh_dist.install_requires + sqlmesh_dist.extras_require["dev"]  # type: ignore
+os.chdir(os.path.dirname(__file__))
+
+setup(
+    name="sqlmesh-tests",
+    description="Tests for SQLMesh",
+    url="https://github.com/TobikoData/sqlmesh",
+    author="TobikoData Inc.",
+    author_email="engineering@tobikodata.com",
+    license="Apache License 2.0",
+    package_dir={"sqlmesh_tests": ""},
+    package_data={"": ["fixtures/**"]},
+    use_scm_version={
+        "write_to": "_version.py",
+        "fallback_version": "0.0.0",
+        "local_scheme": "no-local-version",
+    },
+    setup_requires=["setuptools_scm"],
+    install_requires=requirements,
+)
