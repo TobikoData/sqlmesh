bb1bdeaa70e81da1ab1e5ee9ab57e1cfcfd24082
tobymao <toby.mao@gmail.com>
2024-04-22 16:58:07 -0700

    Revert "Toby/revert types (#2261)"
    This reverts commit e1c69d0a2c07bc09713cffb123563f883fc2860e.

--- examples/sushi/macros/macros.py
+++ examples/sushi/macros/macros.py
@@ -1,8 +1,9 @@
 from macros.utils import between  # type: ignore
+from sqlglot import exp
 
 from sqlmesh import macro
 
 
 @macro()
-def incremental_by_ds(evaluator, column):
+def incremental_by_ds(evaluator, column: exp.Column):
     return between(evaluator, column, evaluator.locals["start_date"], evaluator.locals["end_date"])
