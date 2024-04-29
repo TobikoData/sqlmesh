60917b911c8861b6598f39b793ada1fb411745b9
Toby Mao <toby.mao@gmail.com>
2024-04-29 16:36:54 -0700

    fix!: add implicit dialect to pydantic models in python (#2527)
    * fix!: add implicit dialect to pydantic models in python
    
    python models allow users to create various pydantic models that contain
    sql queries. these sql queries are by default parsed as sqlglot dialect
    which has unintended consequences.
    
    we now set the config default dialect while loading these python models
    which allows the pydantic validators to set the expected dialect
    
    * Update sqlmesh/utils/pydantic.py
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>
    
    * rebase
    
    * Set the default dialect in the validator
    
    * Set the default dialect in the validator
    
    * fix pydantic 1
    
    ---------
    
    Co-authored-by: Jo <46752250+georgesittas@users.noreply.github.com>
    Co-authored-by: Iaroslav Zeigerman <zeigerman.ia@gmail.com>

--- tests/core/test_snapshot.py
+++ tests/core/test_snapshot.py
@@ -109,7 +109,6 @@ def test_json(snapshot: Snapshot):
                 "batch_size": 30,
                 "forward_only": False,
                 "disable_restatement": False,
-                "dialect": "",
             },
             "mapping_schema": {},
             "start": "2020-01-01",
@@ -730,7 +729,7 @@ def test_fingerprint_jinja_macros(model: Model):
         }
     )
     original_fingerprint = SnapshotFingerprint(
-        data_hash="2664333317",
+        data_hash="2973224250",
         metadata_hash="892368116",
     )
 
