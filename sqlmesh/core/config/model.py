d850d8a536063761107d9a39ac888b01529b0b41
Trey Spiller <treyspiller@gmail.com>
2024-05-07 12:44:36 -0500

    Fix pydantic, dbt test issues

--- sqlmesh/core/config/model.py
+++ sqlmesh/core/config/model.py
@@ -30,7 +30,7 @@ class ModelDefaultsConfig(BaseConfig):
     owner: t.Optional[str] = None
     start: t.Optional[TimeLike] = None
     storage_format: t.Optional[str] = None
-    on_schema_change: t.Optional[OnSchemaChange | str] = None
+    on_schema_change: t.Optional[t.Union[OnSchemaChange, str]] = None
     session_properties: t.Optional[t.Dict[str, t.Any]] = None
 
     _model_kind_validator = model_kind_validator
