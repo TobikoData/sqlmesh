105fff11c97e9df2c56d7601dda0c2d714ad1c5f
George Sittas <giwrgos.sittas@gmail.com>
2024-05-14 01:50:47 +0300

    Fix mypy issue

--- sqlmesh/dbt/relation.py
+++ sqlmesh/dbt/relation.py
@@ -4,4 +4,4 @@ from sqlmesh.dbt.util import DBT_VERSION
 if DBT_VERSION < (1, 8):
     from dbt.contracts.relation import *  # type: ignore  # noqa: F403
 else:
-    from dbt.adapters.contracts.relation import *  # noqa: F403
+    from dbt.adapters.contracts.relation import *  # type: ignore # noqa: F403
