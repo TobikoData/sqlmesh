b47f6ef32bc976869debdb672293696252298ec6
Jo <46752250+georgesittas@users.noreply.github.com>
2024-05-20 03:11:40 +0300

    Chore!: bump sqlglot to v23.17.0 (#2636)

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=23.16.0",
+        "sqlglot[rs]~=23.17.0",
     ],
     extras_require={
         "bigquery": [
