9948dece167ab605bb1b33116486ecc99a2b952a
Jo <46752250+georgesittas@users.noreply.github.com>
2024-05-28 23:26:58 +0300

    Fix: ensure MacroEvaluator.columns_to_types returns types as DataTypes (#2694)

--- tests/core/test_macros.py
+++ tests/core/test_macros.py
@@ -96,9 +96,15 @@ def macro_evaluator() -> MacroEvaluator:
 
 def test_star(assert_exp_eq) -> None:
     sql = """SELECT @STAR(foo) FROM foo"""
-    expected_sql = """SELECT CAST([foo].[a] AS DATETIMEOFFSET) AS [a] FROM foo"""
+    expected_sql = """SELECT CAST([foo].[a] AS DATETIMEOFFSET) AS [a], CAST([foo].[b] AS INTEGER) AS [b] FROM foo"""
     schema = MappingSchema(
-        {"foo": {"a": exp.DataType.build("datetimeoffset", dialect="tsql")}}, dialect="tsql"
+        {
+            "foo": {
+                "a": exp.DataType.build("datetimeoffset", dialect="tsql"),
+                "b": "int",
+            },
+        },
+        dialect="tsql",
     )
     evaluator = MacroEvaluator(schema=schema, dialect="tsql")
     assert_exp_eq(evaluator.transform(parse_one(sql, read="tsql")), expected_sql, dialect="tsql")
