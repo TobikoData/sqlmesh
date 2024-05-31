8b8abed1c2e666cb76ba3be322343351efabcef1
Themis Valtinos <73662635+Themiscodes@users.noreply.github.com>
2024-05-31 21:31:02 +0300

    reformatting with the linter

--- sqlmesh/core/config/naming.py
+++ sqlmesh/core/config/naming.py
@@ -7,8 +7,8 @@ class NameInferenceConfig(BaseConfig):
     """Configuration for name inference of models from directory structure.
 
     Args:
-        name_inference: A flag indicating whether name inference is enabled. 
+        name_inference: A flag indicating whether name inference is enabled.
                         Default is False.
     """
-    
+
     name_inference: bool = False
