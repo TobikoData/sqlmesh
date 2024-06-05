c8b93fde2d5b8f1f4dea243c1c63e191b7204417
Toby Mao <toby.mao@gmail.com>
2024-06-04 19:28:01 -0700

    feat: add the ability to have audits on external models (#2715)

--- tests/integrations/jupyter/test_magics.py
+++ tests/integrations/jupyter/test_magics.py
@@ -291,7 +291,7 @@ def test_plan(
 
     # TODO: Should this be going to stdout? This is printing the status updates for when each batch finishes for
     # the models and how long it took
-    assert len(output.stdout.strip().split("\n")) == 20
+    assert len(output.stdout.strip().split("\n")) == 21
     assert not output.stderr
     assert len(output.outputs) == 5
     text_output = convert_all_html_output_to_text(output)
