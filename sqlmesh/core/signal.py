cf394644ed2e43bdaeaecbd98cf8083a96e43023
Philippe Laflamme <philippe@hopper.com>
2024-06-28 15:53:54 -0400

    feat: allow signals to return intervals

--- sqlmesh/core/signal.py
+++ sqlmesh/core/signal.py
@@ -1,11 +1,14 @@
 import abc
 import typing as t
+from datetime import datetime
+
+Interval = t.Tuple[datetime, datetime]
 
 
 class Signal(abc.ABC):
     @abc.abstractmethod
-    def wait(self) -> None:
-        """Wait for the signal to be ready. This method should block until the signal becomes ready."""
+    def poll_ready_intervals(self) -> bool | list[Interval]:
+        """Poll the signal and return True if it is ready, otherwise return False."""
 
 
 SignalFactory = t.Callable[[t.Dict[str, str | int | float | bool]], Signal]
