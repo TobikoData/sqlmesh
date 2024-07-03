5ce423d887dc4e5e55873b97f5cb71316eb53880
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2024-07-03 10:46:07 -0700

    Fix: Support for custom SQLMesh gateways in the dbt project config

--- sqlmesh/dbt/loader.py
+++ sqlmesh/dbt/loader.py
@@ -53,16 +53,16 @@ def sqlmesh_config(
         loader=DbtLoader,
         model_defaults=model_defaults,
         variables=variables or {},
-        **dict(
-            default_gateway=profile.target_name if "gateways" not in kwargs else "",
-            gateways={
+        **{
+            "default_gateway": profile.target_name if "gateways" not in kwargs else "",
+            "gateways": {
                 profile.target_name: GatewayConfig(
                     connection=profile.target.to_sqlmesh(**target_to_sqlmesh_args),
                     state_connection=state_connection,
                 )
             },  # type: ignore
             **kwargs,
-        ),
+        },
     )
 
 
