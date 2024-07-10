da6e0d25c5fb2c613de31530f390c5d11c55b6f8
Themis Valtinos <73662635+Themiscodes@users.noreply.github.com>
2024-07-10 14:36:41 +0300

    Add test for disabled python model

---
+++ examples/sushi/models/disabled.py
@@ -0,0 +1,15 @@
+import typing as t
+from sqlmesh import ExecutionContext, model
+
+@model(
+  "sushi.disabled_py",
+  columns={
+    "id": "int",
+  },
+  enabled=False
+)
+def execute(
+  context: ExecutionContext,
+  **kwargs: t.Any,
+):
+  pass
