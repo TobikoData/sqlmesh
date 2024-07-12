28e139b0f1d75f61792fe7371973d7cde827e335
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2024-07-11 20:18:20 -0600

    Fix: Avoid type coercion when converting pandas dataframes to agate tables in the dbt adapter (#2897)

--- sqlmesh/dbt/util.py
+++ sqlmesh/dbt/util.py
@@ -13,9 +13,9 @@ def _get_dbt_version() -> t.Tuple[int, int]:
 DBT_VERSION = _get_dbt_version()
 
 if DBT_VERSION < (1, 8):
-    from dbt.clients.agate_helper import table_from_data, empty_table, as_matrix  # type: ignore  # noqa: F401
+    from dbt.clients.agate_helper import table_from_data_flat, empty_table, as_matrix  # type: ignore  # noqa: F401
 else:
-    from dbt_common.clients.agate_helper import table_from_data, empty_table, as_matrix  # type: ignore  # noqa: F401
+    from dbt_common.clients.agate_helper import table_from_data_flat, empty_table, as_matrix  # type: ignore  # noqa: F401
 
 
 def pandas_to_agate(df: pd.DataFrame) -> agate.Table:
@@ -23,4 +23,4 @@ def pandas_to_agate(df: pd.DataFrame) -> agate.Table:
     Converts a Pandas DataFrame to an Agate Table
     """
 
-    return table_from_data(df.to_dict(orient="records"), df.columns.tolist())
+    return table_from_data_flat(df.to_dict(orient="records"), df.columns.tolist())
