91adbc8a3651904d2df5c349880cf82d42933a0b
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2024-07-17 12:03:34 -0700

    fix: bump sqlglot 25.6.0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=25.5.1",
+        "sqlglot[rs]~=25.6.0",
     ],
     extras_require={
         "bigquery": [
