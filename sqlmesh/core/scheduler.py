e9976afc260877ee141c7ed85a2ce27158d8fcc9
Philippe Laflamme <484152+plaflamme@users.noreply.github.com>
2024-08-14 17:32:45 -0400

    fix: adjust exception type and message
    Co-authored-by: Iaroslav Zeigerman <zeigerman.ia@gmail.com>

--- sqlmesh/core/scheduler.py
+++ sqlmesh/core/scheduler.py
@@ -102,7 +102,7 @@ def signal_factory(f: SignalFactory) -> None:
     global _registered_signal_factory
 
     if _registered_signal_factory is not None:
-        raise RuntimeError("only one function may be decorated with @signal_factory")
+        raise SQLMeshError("Only one function may be decorated with @signal_factory")
 
     _registered_signal_factory = f
 
