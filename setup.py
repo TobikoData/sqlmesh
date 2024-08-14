26b927d98a9a3619596fe689bf3e2dd5c37c23d9
George Sittas <giwrgos.sittas@gmail.com>
2024-08-14 05:53:33 +0300

    Bump to 25.11.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=25.11.1",
+        "sqlglot[rs]~=25.11.2",
     ],
     extras_require={
         "bigquery": [
