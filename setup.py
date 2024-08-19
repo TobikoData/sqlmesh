75aadd477c44c13c04aecb149f514c735bffa137
George Sittas <giwrgos.sittas@gmail.com>
2024-08-20 01:51:54 +0300

    Chore!: bump sqlglot to v25.15.0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=25.14.0",
+        "sqlglot[rs]~=25.15.0",
     ],
     extras_require={
         "bigquery": [
