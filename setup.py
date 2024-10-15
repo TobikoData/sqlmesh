3b665962597e867136919ff89eb80466873aec96
Ben King <9087625+benfdking@users.noreply.github.com>
2024-10-15 18:12:51 +0100

    Chore: bump sqlglot to v25.25.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "requests",
         "rich[jupyter]",
         "ruamel.yaml",
-        "sqlglot[rs]~=25.25.0",
+        "sqlglot[rs]~=25.25.1",
         "tenacity",
     ],
     extras_require={
