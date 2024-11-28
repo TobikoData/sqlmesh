32c763e7a8560995d67abf1098cbd94e712b7981
linsiyuan0303 <slin3@andrew.cmu.edu>
2024-11-27 21:31:02 -0500

    fix(rw): correct py style

--- tests/core/engine_adapter/test_risingwave.py
+++ tests/core/engine_adapter/test_risingwave.py
@@ -24,15 +24,12 @@ def test_create_view(make_mocked_engine_adapter: t.Callable):
             call('CREATE VIEW "db"."view" AS SELECT 1'),
         ]
     )
-    
-def test_drop_view(make_mocked_engine_adapter: t.Callable):
 
+
+def test_drop_view(make_mocked_engine_adapter: t.Callable):
     adapter = make_mocked_engine_adapter(RisingwaveEngineAdapter)
 
     adapter.SUPPORTS_MATERIALIZED_VIEWS = True
-
-
-
 
     adapter.drop_view("db.view")
 
@@ -40,25 +37,13 @@ def test_drop_view(make_mocked_engine_adapter: t.Callable):
 
     adapter.drop_view("db.view", cascade=False)
 
-
-
-
     adapter.cursor.execute.assert_has_calls(
-
         [
-
             # 1st call
-
             call('DROP VIEW IF EXISTS "db"."view" CASCADE'),
-
             # 2nd call
-
             call('DROP MATERIALIZED VIEW IF EXISTS "db"."view" CASCADE'),
-
             # 3rd call
-
             call('DROP VIEW IF EXISTS "db"."view"'),
-
         ]
-
     )
