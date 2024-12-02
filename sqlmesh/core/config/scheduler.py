99a749b2a6e091e37e1a88dbf35316349bdee7ad
Erin Drummond <erin.dru@gmail.com>
2024-12-02 20:04:58 +0000

    PR feedback

--- sqlmesh/core/config/scheduler.py
+++ sqlmesh/core/config/scheduler.py
@@ -86,19 +86,20 @@ class _EngineAdapterStateSyncSchedulerConfig(SchedulerConfig):
             context.config.get_state_connection(context.gateway) or context._connection_config
         )
 
+        warehouse_connection = context.config.get_connection(context.gateway)
+
         if (
             isinstance(state_connection, DuckDBConnectionConfig)
             and state_connection.concurrent_tasks <= 1
         ):
             # If we are using DuckDB, ensure that multithreaded mode gets enabled if necessary
-            warehouse_connection = context.config.get_connection(context.gateway)
             if warehouse_connection.concurrent_tasks > 1:
                 logger.warning(
                     "The duckdb state connection is configured for single threaded mode but the warehouse connection is configured for "
                     + f"multi threaded mode with {warehouse_connection.concurrent_tasks} concurrent tasks."
                     + " This can cause SQLMesh to hang. Overriding the duckdb state connection config to use multi threaded mode"
                 )
-                # this triggers multithreaded mode
+                # this triggers multithreaded mode and has to happen before the engine adapter is created below
                 state_connection.concurrent_tasks = warehouse_connection.concurrent_tasks
 
         engine_adapter = state_connection.create_engine_adapter()
@@ -107,11 +108,17 @@ class _EngineAdapterStateSyncSchedulerConfig(SchedulerConfig):
                 f"The {engine_adapter.DIALECT.upper()} engine cannot be used to store SQLMesh state - please specify a different `state_connection` engine."
                 + " See https://sqlmesh.readthedocs.io/en/stable/reference/configuration/#gateways for more information."
             )
-        if not state_connection.is_recommended_for_state_sync:
-            logger.warning(
-                f"The {state_connection.type_} engine is not recommended for storing SQLMesh state in production deployments. Please see"
-                + " https://sqlmesh.readthedocs.io/en/stable/guides/configuration/#state-connection for a list of recommended engines and more information."
-            )
+
+        # If the user is using DuckDB for both the state and the warehouse connection, they are most likely running an example project
+        # or POC. To reduce friction, we wont log a warning about DuckDB being used for state until they change to a proper warehouse
+        if not isinstance(state_connection, DuckDBConnectionConfig) or not isinstance(
+            warehouse_connection, DuckDBConnectionConfig
+        ):
+            if not state_connection.is_recommended_for_state_sync:
+                logger.warning(
+                    f"The {state_connection.type_} engine is not recommended for storing SQLMesh state in production deployments. Please see"
+                    + " https://sqlmesh.readthedocs.io/en/stable/guides/configuration/#state-connection for a list of recommended engines and more information."
+                )
 
         schema = context.config.get_state_schema(context.gateway)
         return EngineAdapterStateSync(
