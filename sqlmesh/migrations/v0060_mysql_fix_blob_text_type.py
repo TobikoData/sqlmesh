b1063e7d240b0a71fdd3423d2c7a59250e2c479f
Mike Wallis <82802348+MikeWallis42@users.noreply.github.com>
2024-12-03 16:37:00 +0000

    Fix(mysql): Use LONGTEXT data type when storing large blobs of text (#3442)

--- sqlmesh/migrations/v0060_mysql_fix_blob_text_type.py
+++ sqlmesh/migrations/v0060_mysql_fix_blob_text_type.py
@@ -1,4 +1,8 @@
-"""Use LONGTEXT type for blob fields in MySQL."""
+"""Duplicate of v0033, Use LONGTEXT type for blob fields in MySQL.
+
+Seeds table has since been dropped.
+Environments table now has a requirements column.
+"""
 
 from sqlglot import exp
 
@@ -13,19 +17,19 @@ def migrate(state_sync, **kwargs):  # type: ignore
     schema = state_sync.schema
     environments_table = "_environments"
     snapshots_table = "_snapshots"
-    seeds_table = "_seeds"
     plan_dags_table = "_plan_dags"
 
     if schema:
         environments_table = f"{schema}.{environments_table}"
         snapshots_table = f"{schema}.{snapshots_table}"
-        seeds_table = f"{state_sync.schema}.{seeds_table}"
         plan_dags_table = f"{schema}.{plan_dags_table}"
 
     targets = [
         (environments_table, "snapshots"),
+        (environments_table, "promoted_snapshot_ids"),
+        (environments_table, "previous_finalized_snapshots"),
+        (environments_table, "requirements"),
         (snapshots_table, "snapshot"),
-        (seeds_table, "content"),
         (plan_dags_table, "dag_spec"),
     ]
 
