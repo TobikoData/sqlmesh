b0de7f1715de75951d6de2e3dd3a087b9869825c
Max Mykal <mykalmax@yahoo.com>
2024-12-06 09:22:12 -0800

    types placeholder

--- web/client/src/utils/additional-components.ts
+++ web/client/src/utils/additional-components.ts
@@ -24,27 +24,27 @@ export const TBKDatetime = createComponent({
   tagName: 'tbk-datetime',
   elementClass: Datetime,
   react: React,
-})
+}) as any
 export const TBKDetails = createComponent({
   tagName: 'tbk-details',
   elementClass: Details,
   react: React,
-})
+}) as any
 export const TBKTabs = createComponent({
   tagName: 'tbk-tabs',
   elementClass: Tabs,
   react: React,
-})
+}) as any
 export const TBKTab = createComponent({
   tagName: 'tbk-tab',
   elementClass: Tab,
   react: React,
-})
+}) as any
 export const TBKTabPanel = createComponent({
   tagName: 'tbk-tab-panel',
   elementClass: TabPanel,
   react: React,
-})
+}) as any
 export const TBKSplitPane = createComponent({
   tagName: 'tbk-split-pane',
   elementClass: SplitPane,
@@ -52,32 +52,32 @@ export const TBKSplitPane = createComponent({
   events: {
     onChange: 'sl-reposition',
   },
-})
+}) as any
 export const TBKScroll = createComponent({
   tagName: 'tbk-scroll',
   elementClass: Scroll,
   react: React,
-})
+}) as any
 export const TBKMetadataSection = createComponent({
   tagName: 'tbk-metadata-section',
   elementClass: MetadataSection,
   react: React,
-})
+}) as any
 export const TBKMetadataItem = createComponent({
   tagName: 'tbk-metadata-item',
   elementClass: MetadataItem,
   react: React,
-})
+}) as any
 export const TBKMetadata = createComponent({
   tagName: 'tbk-metadata',
   elementClass: Metadata,
   react: React,
-})
+}) as any
 export const TBKBadge = createComponent({
   tagName: 'tbk-badge',
   elementClass: Badge,
   react: React,
-})
+}) as any
 export const TBKSourceList = createComponent({
   tagName: 'tbk-source-list',
   elementClass: SourceList,
@@ -85,24 +85,24 @@ export const TBKSourceList = createComponent({
   events: {
     onChange: 'change',
   },
-})
+}) as any
 export const TBKSourceListItem = createComponent({
   tagName: 'tbk-source-list-item',
   elementClass: SourceListItem,
   react: React,
-})
+}) as any
 export const TBKSourceListSection = createComponent({
   tagName: 'tbk-source-list-section',
   elementClass: SourceListSection,
   react: React,
-})
+}) as any
 export const TBKModelName = createComponent({
   tagName: 'tbk-model-name',
   elementClass: ModelName,
   react: React,
-})
+}) as any
 export const TBKResizeObserver = createComponent({
   tagName: 'tbk-resize-observer',
   elementClass: ResizeObserver,
   react: React,
-})
+}) as any
