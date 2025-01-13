528a8c97aed94c5ed2fce598234382304b6fff27
Max Mykal <mykalmax@yahoo.com>
2025-01-13 09:35:37 -0800

    Fix: Preserve file changes when navigating tabs (#3587)

--- web/client/src/library/components/editor/EditorTabs.tsx
+++ web/client/src/library/components/editor/EditorTabs.tsx
@@ -1,14 +1,14 @@
-import { useMemo, type MouseEvent, useEffect, useRef } from 'react'
-import { PlusIcon, XCircleIcon } from '@heroicons/react/24/solid'
+import { useMemo, type MouseEvent, useEffect } from 'react'
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { EnumSize, EnumVariant } from '~/types/enum'
 import { Button } from '../button/Button'
-import clsx from 'clsx'
 import { type EditorTab, useStoreEditor } from '~/context/editor'
 import { useStoreProject } from '@context/project'
 import { isFalse, isNil, isNotNil } from '@utils/index'
 import { useStoreContext } from '@context/context'
 import { ModelDirectory } from '@models/directory'
 import { ModelFile } from '@models/file'
+import Tab from './EditorTab'
 
 export default function EditorTabs(): JSX.Element {
   const modules = useStoreContext(s => s.modules)
@@ -82,25 +82,13 @@ export default function EditorTabs(): JSX.Element {
     setSelectedFile(file)
   }, [lastSelectedModel])
 
-  function addTabAndSelect(): void {
-    const tab = createTab()
-
-    addTab(tab)
-    selectTab(tab)
-    setSelectedFile(tab.file)
-  }
-
   return (
-    <div className="flex items-center">
+    <div className="flex items-center bg-neutral-5">
       <Button
         className="h-6 m-0 ml-1 mr-2 border-none"
         variant={EnumVariant.Alternative}
         size={EnumSize.sm}
-        onClick={(e: MouseEvent) => {
-          e.stopPropagation()
-
-          addTabAndSelect()
-        }}
+        onClick={handleAddTab}
       >
         <PlusIcon className="inline-block w-3 h-4" />
       </Button>
@@ -122,88 +110,18 @@ export default function EditorTabs(): JSX.Element {
       </ul>
     </div>
   )
-}
 
-function Tab({ tab, title }: { tab: EditorTab; title: string }): JSX.Element {
-  const elTab = useRef<HTMLLIElement>(null)
+  function handleAddTab(e: MouseEvent): void {
+    e.stopPropagation()
 
-  const addConfirmation = useStoreContext(s => s.addConfirmation)
-
-  const setSelectedFile = useStoreProject(s => s.setSelectedFile)
-
-  const activeTab = useStoreEditor(s => s.tab)
-  const closeTab = useStoreEditor(s => s.closeTab)
-
-  useEffect(() => {
-    if (isNil(tab)) return
-
-    tab.el = elTab.current ?? undefined
-
-    if (activeTab?.id === tab.id) {
-      setTimeout(() => {
-        tab?.el?.scrollIntoView({
-          behavior: 'smooth',
-          inline: 'center',
-        })
-      }, 300)
-    }
-  }, [elTab, tab, activeTab])
-
-  function closeEditorTabWithConfirmation(): void {
-    if (tab.file.isChanged) {
-      addConfirmation({
-        headline: 'Closing Tab',
-        description:
-          'All unsaved changes will be lost. Do you want to close the tab anyway?',
-        yesText: 'Yes, Close Tab',
-        noText: 'No, Cancel',
-        action: () => {
-          closeTab(tab.file)
-        },
-      })
-    } else {
-      closeTab(tab.file)
-    }
+    addTabAndSelect()
   }
 
-  const isActive = tab.file.id === activeTab?.file.id
+  function addTabAndSelect(): void {
+    const tab = createTab()
 
-  return (
-    <li
-      ref={elTab}
-      className={clsx(
-        'inline-block py-1 pr-2 last-child:pr-0 overflow-hidden text-center overflow-ellipsis cursor-pointer',
-      )}
-      onClick={(e: MouseEvent) => {
-        e.stopPropagation()
-
-        setSelectedFile(tab.file)
-      }}
-    >
-      <span
-        className={clsx(
-          'flex border-2 justify-between items-center pl-1 pr-1 py-[0.125rem] min-w-[8rem] rounded-md group border-transparent border-r border-r-theme-darker dark:border-r-theme-lighter',
-          isActive
-            ? 'bg-neutral-200 border-neutral-200 text-neutral-900 dark:bg-dark-lighter dark:border-dark-lighter dark:text-primary-500'
-            : 'bg-trasparent hover:bg-theme-darker dark:hover:bg-theme-lighter',
-        )}
-      >
-        <small className="text-xs">{title}</small>
-        <small
-          className={clsx(
-            'group-hover:hidden text-xs inline-block ml-3 mr-1 w-2 h-2 rounded-full',
-            tab.file.isChanged ? 'bg-warning-500' : 'bg-transparent',
-          )}
-        ></small>
-        <XCircleIcon
-          className="hidden group-hover:inline-block text-neutral-600 dark:text-neutral-100 w-4 h-4 ml-2 mr-0 cursor-pointer"
-          onClick={(e: MouseEvent) => {
-            e.stopPropagation()
-
-            closeEditorTabWithConfirmation()
-          }}
-        />
-      </span>
-    </li>
-  )
+    addTab(tab)
+    selectTab(tab)
+    setSelectedFile(tab.file)
+  }
 }
