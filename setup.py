de2476b0088180bfd4ee864ad6bff460553a00e5
Erin Drummond <erin.dru@gmail.com>
2025-01-28 08:06:57 +1300

    Chore!: bump sqlglot to v26.3.8 (#3716)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         "rich[jupyter]",
         "ruamel.yaml",
         "setuptools; python_version>='3.12'",
-        "sqlglot[rs]~=26.2.1",
+        "sqlglot[rs]~=26.3.8",
         "tenacity",
         "time-machine",
     ],
