bf5503b31a818cbf032352e099536d29f3891408
Ben King <9087625+benfdking@users.noreply.github.com>
2025-01-29 22:47:58 +0000

    chore: make context method private

--- sqlmesh/core/context.py
+++ sqlmesh/core/context.py
@@ -727,7 +727,7 @@ class GenericContext(BaseContext, t.Generic[C]):
                 self.notification_target_manager.notify(
                     NotificationEvent.RUN_FAILURE, traceback.format_exc()
                 )
-                self.log_failure_error("Run")
+                self._log_failure_error("Run")
                 analytics.collector.on_run_end(
                     run_id=analytics_run_id, succeeded=False, interrupted=False, error=e
                 )
@@ -1432,7 +1432,7 @@ class GenericContext(BaseContext, t.Generic[C]):
                 plan_id=plan.plan_id,
                 exc=traceback.format_exc(),
             )
-            self.log_failure_error(method="Apply")
+            self._log_failure_error(method="Apply")
             raise e
         self.notification_target_manager.notify(
             NotificationEvent.APPLY_END,
@@ -1440,7 +1440,7 @@ class GenericContext(BaseContext, t.Generic[C]):
             plan_id=plan.plan_id,
         )
 
-    def log_failure_error(self, method: t.Literal["Apply", "Run"]) -> None:
+    def _log_failure_error(self, method: t.Literal["Apply", "Run"]) -> None:
         logger.error(f"{method} Failure: {traceback.format_exc()}")
 
     @python_api_analytics
