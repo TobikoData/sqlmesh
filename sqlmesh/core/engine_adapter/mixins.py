4271999b84ec17b5bce19fa22b3f9c6420f3b4fc
eakmanrq <6326532+eakmanrq@users.noreply.github.com>
2025-02-11 12:31:53 -0800

    fix: add kwargs to build_table_properties_exp

--- sqlmesh/core/engine_adapter/mixins.py
+++ sqlmesh/core/engine_adapter/mixins.py
@@ -159,6 +159,7 @@ class HiveMetastoreTablePropertiesMixin(EngineAdapter):
         columns_to_types: t.Optional[t.Dict[str, exp.DataType]] = None,
         table_description: t.Optional[str] = None,
         table_kind: t.Optional[str] = None,
+        **kwargs: t.Any,
     ) -> t.Optional[exp.Properties]:
         properties: t.List[exp.Expression] = []
 
