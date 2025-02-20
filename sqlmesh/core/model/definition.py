1e84ba5dba7e963cd4ded059dcb1d055500d1eaa
Jo <46752250+georgesittas@users.noreply.github.com>
2025-02-20 20:57:53 +0200

    Fix: pass model dialect to sqlglot.diff to avoid possible generation issues (#3877)

--- sqlmesh/core/model/definition.py
+++ sqlmesh/core/model/definition.py
@@ -1414,6 +1414,7 @@ class SqlModel(_Model):
                 matchings=[(previous_query, this_query)],
                 delta_only=True,
                 copy=False,
+                dialect=self.dialect if self.dialect == previous.dialect else None,
             )
         inserted_expressions = {e.expression for e in edits if isinstance(e, Insert)}
 
