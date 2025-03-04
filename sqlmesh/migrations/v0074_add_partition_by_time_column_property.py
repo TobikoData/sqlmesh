0a8a3492da41b8735d2e715035c67ab5ab9e5537
Erin Drummond <erin.dru@gmail.com>
2025-03-04 21:45:38 +0000

    Feat: Allow opting out of the time_column being automatically added to the partitioned_by columns

---
+++ sqlmesh/migrations/v0074_add_partition_by_time_column_property.py
@@ -0,0 +1,6 @@
+"""Add 'partition_by_time_column' property to the IncrementalByTimeRange model kind
+(default: True to keep the original behaviour)"""
+
+
+def migrate(state_sync, **kwargs):  # type: ignore
+    pass
