d87a8d207bddf371c5f28b2739de54cad19c36b6
Trey Spiller <treyspiller@gmail.com>
2025-03-27 11:46:08 -0500

    fix tests

--- tests/integrations/jupyter/test_magics.py
+++ tests/integrations/jupyter/test_magics.py
@@ -293,7 +293,7 @@ def test_plan(
     # the models and how long it took
     assert len(output.stdout.strip().split("\n")) == 46
     assert not output.stderr
-    assert len(output.outputs) == 5
+    assert len(output.outputs) == 4
     text_output = convert_all_html_output_to_text(output)
     # TODO: Is this what we expect?
     # This has minor differences between CI/CD and local.
@@ -309,7 +309,6 @@ def test_plan(
         ["pre"] + ["span"] * 5,
         ["pre"],
         ["pre", "span"],
-        ["pre"] + ["span"] * 9,
     ]
 
 
