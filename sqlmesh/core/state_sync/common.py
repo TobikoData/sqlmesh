056a3bef135016c12cddcab5198d9dac321d396a
Erin Drummond <erin.dru@gmail.com>
2025-04-03 08:13:44 +1300

    Feat: state import/export (#4038)

--- sqlmesh/core/state_sync/common.py
+++ sqlmesh/core/state_sync/common.py
@@ -3,14 +3,19 @@ from __future__ import annotations
 import logging
 import typing as t
 from functools import wraps
+import itertools
+import abc
 
 from sqlmesh.core.console import Console
 from sqlmesh.core.dialect import schema_
-from sqlmesh.core.environment import Environment
+from sqlmesh.utils.pydantic import PydanticModel
+from sqlmesh.core.environment import Environment, EnvironmentStatements
 from sqlmesh.utils.errors import SQLMeshError
+from sqlmesh.core.snapshot import Snapshot
 
 if t.TYPE_CHECKING:
     from sqlmesh.core.engine_adapter.base import EngineAdapter
+    from sqlmesh.core.state_sync.base import Versions
 
 logger = logging.getLogger(__name__)
 
@@ -81,3 +86,39 @@ def transactional() -> t.Callable[[t.Callable], t.Callable]:
         return wrapper
 
     return decorator
+
+
+T = t.TypeVar("T")
+
+
+def chunk_iterable(iterable: t.Iterable[T], size: int = 10) -> t.Iterable[t.Iterable[T]]:
+    iterator = iter(iterable)
+    for first in iterator:
+        yield itertools.chain([first], itertools.islice(iterator, size - 1))
+
+
+class EnvironmentWithStatements(PydanticModel):
+    environment: Environment
+    statements: t.List[EnvironmentStatements] = []
+
+
+class StateStream(abc.ABC):
+    """
+    Represents a stream of state either going into the StateSync (perhaps loaded from a file)
+    or out of the StateSync (perhaps being dumped to a file)
+    """
+
+    @property
+    @abc.abstractmethod
+    def versions(self) -> Versions:
+        """The versions of the objects contained in this StateStream"""
+
+    @property
+    @abc.abstractmethod
+    def snapshots(self) -> t.Iterable[Snapshot]:
+        """A stream of Snapshot objects. Note that they should be fully populated with any relevant Intervals"""
+
+    @property
+    @abc.abstractmethod
+    def environments(self) -> t.Iterable[EnvironmentWithStatements]:
+        """A stream of Environments with any EnvironmentStatements attached"""
