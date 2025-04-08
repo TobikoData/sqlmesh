cbe704f41ddc1e7e39a8fe484a6a796e31574fa4
Ben King <9087625+benfdking@users.noreply.github.com>
2025-04-08 18:25:50 +0200

    getting react server to show

--- vscode/extension/src/webviews/lineagePanel.ts
+++ vscode/extension/src/webviews/lineagePanel.ts
@@ -8,9 +8,9 @@ import {
     WebviewViewProvider,
     WebviewViewResolveContext,
     window,
-    workspace,
   } from "vscode"
-  
+  import { isProduction } from "../is_dev"
+
   export class LineagePanel implements WebviewViewProvider, Disposable {
     public static readonly viewType = "sqlmesh.lineage"
   
@@ -61,18 +61,24 @@ import {
 	}
 
   getHtml() {
-    const externalUrl = this.getServerUrl()
+    
+    const isProd = isProduction()
+    
+    const externalUrl = !isProd ? "http://localhost:5173" : this.getServerUrl()
     const externalAuthority = new URL(externalUrl).origin;
     
+    // The CSP is too restrictive - it only allows frame-src but no other resources
+    // Adding connect-src for API calls, img-src for images, and style-src for CSS
     return `
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; frame-src ${externalAuthority}; script-src 'unsafe-inline'; style-src 'unsafe-inline';">
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; frame-src ${externalAuthority}; connect-src ${externalAuthority}; img-src ${externalAuthority} data:; script-src 'unsafe-inline' ${externalAuthority}; style-src 'unsafe-inline' ${externalAuthority};">
 </head>
 <body>
+  <div>${isProd ? "Production" : "Development"}</div>
   <div>${externalUrl}</div>
-  <iframe src="${externalUrl}" style="width:100%; height:100vh;" frameborder="0"></iframe>
+  <iframe src="${externalUrl}" style="width:100%; height:100vh;" frameborder="0" allow="clipboard-read; clipboard-write"></iframe>
 </body>
 </html>        `
   }
