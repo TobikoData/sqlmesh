e0815b86843b45fb1dc7613350dcd5d325302a68
vaggelisd <daniasevangelos@gmail.com>
2025-04-11 15:20:17 +0300

    Use a single loader if specific tests are specified

--- sqlmesh/core/context.py
+++ sqlmesh/core/context.py
@@ -2465,9 +2465,12 @@ class GenericContext(BaseContext, t.Generic[C]):
     def _load_model_tests(
         self, tests: t.Optional[t.List[str]] = None, patterns: list[str] | None = None
     ) -> t.List[ModelTestMetadata]:
-        model_tests = []
+        # If a set of tests is provided, use a single loader to load them
+        # Otherwise, gather all tests from all loaders/repos
+        loaders = [self._loaders[0]] if tests else self._loaders
 
-        for loader in self._loaders:
+        model_tests = []
+        for loader in loaders:
             model_tests.extend(loader._load_model_tests(tests=tests, patterns=patterns))
 
         return model_tests
