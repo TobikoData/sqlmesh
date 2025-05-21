a149d13ece4d195c1213328ea025dc1677a754d2
Vaggelis Danias <daniasevangelos@gmail.com>
2025-05-21 10:51:01 +0300

    Feat: Introduce runtime check for identifier limits per engine (#4432)

--- sqlmesh/core/engine_adapter/postgres.py
+++ sqlmesh/core/engine_adapter/postgres.py
@@ -34,6 +34,7 @@ class PostgresEngineAdapter(
     HAS_VIEW_BINDING = True
     CURRENT_CATALOG_EXPRESSION = exp.column("current_catalog")
     SUPPORTS_REPLACE_TABLE = False
+    MAX_IDENTIFIER_LENGTH = 63
     SCHEMA_DIFFER = SchemaDiffer(
         parameterized_type_defaults={
             # DECIMAL without precision is "up to 131072 digits before the decimal point; up to 16383 digits after the decimal point"
