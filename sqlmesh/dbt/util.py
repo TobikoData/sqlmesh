ede912091eb7b8df7d355f1c21588de17fb89dd6
Themis Valtinos <73662635+themisvaltinos@users.noreply.github.com>
2025-06-02 21:53:34 +0300

    Fix: Adapt get_dbt_version tuple to include patch in version (#4619)

--- sqlmesh/dbt/util.py
+++ sqlmesh/dbt/util.py
@@ -5,14 +5,18 @@ import pandas as pd
 from dbt.version import get_installed_version
 
 
-def _get_dbt_version() -> t.Tuple[int, int]:
+def _get_dbt_version() -> t.Tuple[int, int, int]:
     dbt_version = get_installed_version()
-    return (int(dbt_version.major or "0"), int(dbt_version.minor or "0"))
+    return (
+        int(dbt_version.major or "0"),
+        int(dbt_version.minor or "0"),
+        int(dbt_version.patch or "0"),
+    )
 
 
 DBT_VERSION = _get_dbt_version()
 
-if DBT_VERSION < (1, 8):
+if DBT_VERSION < (1, 8, 0):
     from dbt.clients.agate_helper import table_from_data_flat, empty_table, as_matrix  # type: ignore  # noqa: F401
 else:
     from dbt_common.clients.agate_helper import table_from_data_flat, empty_table, as_matrix  # type: ignore  # noqa: F401
