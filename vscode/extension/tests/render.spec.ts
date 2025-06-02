142efefe4f6be104c35e2d0df5887d51e144e5ec
Ben <9087625+benfdking@users.noreply.github.com>
2025-06-02 22:54:56 +0100

    chore(lsp): format test files (#4628)

--- vscode/extension/tests/render.spec.ts
+++ vscode/extension/tests/render.spec.ts
@@ -1,114 +1,155 @@
-import { test, expect } from '@playwright/test';
-import path from 'path';
-import fs from 'fs-extra';
-import os from 'os';
-import { startVSCode, SUSHI_SOURCE_PATH } from './utils';
+import { test, expect } from '@playwright/test'
+import path from 'path'
+import fs from 'fs-extra'
+import os from 'os'
+import { startVSCode, SUSHI_SOURCE_PATH } from './utils'
 
 test('Render works correctly', async () => {
-    const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'));
-    await fs.copy(SUSHI_SOURCE_PATH, tempDir);
-
-    try {
-      const { window, close } = await startVSCode(tempDir);
-
-      //   Wait for the models folder to be visible
-      await window.waitForSelector('text=models');
-
-      // Click on the models folder, excluding external_models
-      await window.getByRole('treeitem', { name: 'models', exact: true }).locator('a').click();
-
-      // Open the customer_revenue_lifetime model
-      await window.getByRole('treeitem', { name: 'customers.sql', exact: true }).locator('a').click();
-
-      await window.waitForSelector('text=grain');
-      await window.waitForSelector('text=Loaded SQLMesh Context')
-
-      // Render the model
-      await window.keyboard.press(process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P');
-      await window.keyboard.type('Render Model');
-      await window.keyboard.press('Enter');
-
-      // Check if the model is rendered by check if "`oi`.`order_id` AS `order_id`," is in the window
-      await expect(window.locator('text="marketing"."customer_id" AS')).toBeVisible();
-      await expect(window.locator('text=sushi.customers (rendered)')).toBeVisible();
-
-      await close();
-    } finally {
-      await fs.remove(tempDir);
-    }
-  });
-
-test('Render works correctly with model without a description', async () => {
-  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'));
-  await fs.copy(SUSHI_SOURCE_PATH, tempDir);
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'))
+  await fs.copy(SUSHI_SOURCE_PATH, tempDir)
 
   try {
-    const { window, close } = await startVSCode(tempDir);
+    const { window, close } = await startVSCode(tempDir)
 
     //   Wait for the models folder to be visible
-    await window.waitForSelector('text=models');
+    await window.waitForSelector('text=models')
 
     // Click on the models folder, excluding external_models
-    await window.getByRole('treeitem', { name: 'models', exact: true }).locator('a').click();
+    await window
+      .getByRole('treeitem', { name: 'models', exact: true })
+      .locator('a')
+      .click()
 
-    // Open the latest_order model
-    await window.getByRole('treeitem', { name: 'latest_order.sql', exact: true }).locator('a').click();
+    // Open the customer_revenue_lifetime model
+    await window
+      .getByRole('treeitem', { name: 'customers.sql', exact: true })
+      .locator('a')
+      .click()
 
-    await window.waitForSelector('text=custom_full_with_custom_kind');
+    await window.waitForSelector('text=grain')
     await window.waitForSelector('text=Loaded SQLMesh Context')
 
     // Render the model
-    await window.keyboard.press(process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P');
-    await window.keyboard.type('Render Model');
-    await window.keyboard.press('Enter');
+    await window.keyboard.press(
+      process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P',
+    )
+    await window.keyboard.type('Render Model')
+    await window.keyboard.press('Enter')
 
-    // Check if the model is rendered correctly
-    await expect(window.locator('text="orders"."id" AS "id",')).toBeVisible();
-    await expect(window.locator('text=sushi.latest_order (rendered)')).toBeVisible();
+    // Check if the model is rendered by check if "`oi`.`order_id` AS `order_id`," is in the window
+    await expect(
+      window.locator('text="marketing"."customer_id" AS'),
+    ).toBeVisible()
+    await expect(
+      window.locator('text=sushi.customers (rendered)'),
+    ).toBeVisible()
 
-    await close();
+    await close()
   } finally {
-    await fs.remove(tempDir);
+    await fs.remove(tempDir)
   }
-});
+})
 
-test('Render works correctly with every rendered model opening a new tab', async () => {
-  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'));
-  await fs.copy(SUSHI_SOURCE_PATH, tempDir);
+test('Render works correctly with model without a description', async () => {
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'))
+  await fs.copy(SUSHI_SOURCE_PATH, tempDir)
 
   try {
-    const { window, close } = await startVSCode(tempDir);
+    const { window, close } = await startVSCode(tempDir)
 
-    // Wait for the models folder to be visible
-    await window.waitForSelector('text=models');
-    await window.getByRole('treeitem', { name: 'models', exact: true }).locator('a').click();
-    await window.getByRole('treeitem', { name: 'latest_order.sql', exact: true }).locator('a').click();
-    await window.waitForSelector('text=custom_full_with_custom_kind');
+    //   Wait for the models folder to be visible
+    await window.waitForSelector('text=models')
+
+    // Click on the models folder, excluding external_models
+    await window
+      .getByRole('treeitem', { name: 'models', exact: true })
+      .locator('a')
+      .click()
+
+    // Open the latest_order model
+    await window
+      .getByRole('treeitem', { name: 'latest_order.sql', exact: true })
+      .locator('a')
+      .click()
+
+    await window.waitForSelector('text=custom_full_with_custom_kind')
     await window.waitForSelector('text=Loaded SQLMesh Context')
 
     // Render the model
-    await window.keyboard.press(process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P');
-    await window.keyboard.type('Render Model');
-    await window.keyboard.press('Enter');
+    await window.keyboard.press(
+      process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P',
+    )
+    await window.keyboard.type('Render Model')
+    await window.keyboard.press('Enter')
 
     // Check if the model is rendered correctly
-    await expect(window.locator('text=sushi.latest_order (rendered)')).toBeVisible();
+    await expect(window.locator('text="orders"."id" AS "id",')).toBeVisible()
+    await expect(
+      window.locator('text=sushi.latest_order (rendered)'),
+    ).toBeVisible()
+
+    await close()
+  } finally {
+    await fs.remove(tempDir)
+  }
+})
+
+test('Render works correctly with every rendered model opening a new tab', async () => {
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-test-sushi-'))
+  await fs.copy(SUSHI_SOURCE_PATH, tempDir)
+
+  try {
+    const { window, close } = await startVSCode(tempDir)
+
+    // Wait for the models folder to be visible
+    await window.waitForSelector('text=models')
+    await window
+      .getByRole('treeitem', { name: 'models', exact: true })
+      .locator('a')
+      .click()
+    await window
+      .getByRole('treeitem', { name: 'latest_order.sql', exact: true })
+      .locator('a')
+      .click()
+    await window.waitForSelector('text=custom_full_with_custom_kind')
+    await window.waitForSelector('text=Loaded SQLMesh Context')
+
+    // Render the model
+    await window.keyboard.press(
+      process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P',
+    )
+    await window.keyboard.type('Render Model')
+    await window.keyboard.press('Enter')
+
+    // Check if the model is rendered correctly
+    await expect(
+      window.locator('text=sushi.latest_order (rendered)'),
+    ).toBeVisible()
 
     // Open the customers model
-    await window.getByRole('treeitem', { name: 'customers.sql', exact: true }).locator('a').click();
-    await window.waitForSelector('text=grain');
+    await window
+      .getByRole('treeitem', { name: 'customers.sql', exact: true })
+      .locator('a')
+      .click()
+    await window.waitForSelector('text=grain')
 
     // Render the customers model
-    await window.keyboard.press(process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P');
-    await window.keyboard.type('Render Model');
-    await window.keyboard.press('Enter');
+    await window.keyboard.press(
+      process.platform === 'darwin' ? 'Meta+Shift+P' : 'Control+Shift+P',
+    )
+    await window.keyboard.type('Render Model')
+    await window.keyboard.press('Enter')
 
     // Assert both tabs exist
-    await expect(window.locator('text=sushi.latest_order (rendered)')).toBeVisible();
-    await expect(window.locator('text=sushi.customers (rendered)')).toBeVisible();
+    await expect(
+      window.locator('text=sushi.latest_order (rendered)'),
+    ).toBeVisible()
+    await expect(
+      window.locator('text=sushi.customers (rendered)'),
+    ).toBeVisible()
 
-    await close();
+    await close()
   } finally {
-    await fs.remove(tempDir);
+    await fs.remove(tempDir)
   }
 })
--- vscode/extension/tests/utils.ts
+++ vscode/extension/tests/utils.ts
@@ -1,50 +1,68 @@
-import path from 'path';
-import fs from 'fs-extra';
-import os from 'os';
-import { _electron as electron, Page } from '@playwright/test';
+import path from 'path'
+import fs from 'fs-extra'
+import os from 'os'
+import { _electron as electron, Page } from '@playwright/test'
 
 // Absolute path to the VS Code executable you downloaded in step 1.
-export const VS_CODE_EXE = fs.readJsonSync('.vscode-test/paths.json').executablePath;
+export const VS_CODE_EXE = fs.readJsonSync(
+  '.vscode-test/paths.json',
+).executablePath
 // Where your extension lives on disk
-export const EXT_PATH = path.resolve(__dirname, '..');
+export const EXT_PATH = path.resolve(__dirname, '..')
 // Where the sushi project lives which we copy from
-export const SUSHI_SOURCE_PATH = path.join(__dirname, '..', '..', '..', 'examples', 'sushi');
+export const SUSHI_SOURCE_PATH = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  'examples',
+  'sushi',
+)
 
 /**
  * Launch VS Code and return the window and a function to close the app.
  * @param workspaceDir The workspace directory to open.
  * @returns The window and a function to close the app.
  */
-export const startVSCode = async (workspaceDir: string): Promise<{
-    window: Page,
-    close: () => Promise<void>,
-  }> => {
-    const userDataDir = await fs.mkdtemp(path.join(os.tmpdir(), 'vscode-user-data-'));
-    const ciArgs = process.env.CI ? [
-      '--disable-gpu',
-      '--headless',
-      '--no-sandbox',
-      '--disable-dev-shm-usage',
-      '--window-position=-10000,0',
-    ] : [];
-    const args = [
-      ...ciArgs,
-      `--extensionDevelopmentPath=${EXT_PATH}`,
-      '--disable-workspace-trust',
-      '--disable-telemetry',
-      '--install-extension=ms-python.python',
-      `--user-data-dir=${userDataDir}`,
-      workspaceDir,
-    ];
-    const electronApp = await electron.launch({
-      executablePath: VS_CODE_EXE,
-      args,
-    });
-    const window = await electronApp.firstWindow();
-    await window.waitForLoadState('domcontentloaded');
-    await window.waitForLoadState('networkidle');
-    return { window, close: async () => {
-      await electronApp.close();
-      await fs.removeSync(userDataDir);
-    } };
+export const startVSCode = async (
+  workspaceDir: string,
+): Promise<{
+  window: Page
+  close: () => Promise<void>
+}> => {
+  const userDataDir = await fs.mkdtemp(
+    path.join(os.tmpdir(), 'vscode-user-data-'),
+  )
+  const ciArgs = process.env.CI
+    ? [
+        '--disable-gpu',
+        '--headless',
+        '--no-sandbox',
+        '--disable-dev-shm-usage',
+        '--window-position=-10000,0',
+      ]
+    : []
+  const args = [
+    ...ciArgs,
+    `--extensionDevelopmentPath=${EXT_PATH}`,
+    '--disable-workspace-trust',
+    '--disable-telemetry',
+    '--install-extension=ms-python.python',
+    `--user-data-dir=${userDataDir}`,
+    workspaceDir,
+  ]
+  const electronApp = await electron.launch({
+    executablePath: VS_CODE_EXE,
+    args,
+  })
+  const window = await electronApp.firstWindow()
+  await window.waitForLoadState('domcontentloaded')
+  await window.waitForLoadState('networkidle')
+  return {
+    window,
+    close: async () => {
+      await electronApp.close()
+      await fs.removeSync(userDataDir)
+    },
   }
+}
