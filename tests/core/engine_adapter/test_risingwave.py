8c11e973d99658ce7fe864737a75118d1f57ebd5
Bjarke Enkelund <47357343+MisterWheatley@users.noreply.github.com>
2025-07-03 12:43:11 +0000

    Fix: Ran formatter on updated test for RW.

--- tests/core/engine_adapter/test_risingwave.py
+++ tests/core/engine_adapter/test_risingwave.py
@@ -14,47 +14,43 @@ def adapter(make_mocked_engine_adapter):
     adapter = make_mocked_engine_adapter(RisingwaveEngineAdapter)
     return adapter
 
+
 def test_columns(adapter: t.Callable):
     adapter.cursor.fetchall.return_value = [
-        ("smallint_col","smallint"),
-        ("int_col","integer"),
-        ("bigint_col","bigint"),
-        ("ts_col","timestamp without time zone"),
-        ("tstz_col","timestamp with time zone"),
-        ("int_array_col","integer[]"),
-        ("vchar_col","character varying"),
-        ("struct_col","struct<nested_col integer>")
+        ("smallint_col", "smallint"),
+        ("int_col", "integer"),
+        ("bigint_col", "bigint"),
+        ("ts_col", "timestamp without time zone"),
+        ("tstz_col", "timestamp with time zone"),
+        ("int_array_col", "integer[]"),
+        ("vchar_col", "character varying"),
+        ("struct_col", "struct<nested_col integer>"),
     ]
     resp = adapter.columns("db.table")
     assert resp == {
-        "smallint_col" : exp.DataType.build(exp.DataType.Type.SMALLINT,nested=False),
-        "int_col" : exp.DataType.build(exp.DataType.Type.INT,nested=False),
-        "bigint_col" : exp.DataType.build(exp.DataType.Type.BIGINT,nested=False),
-        "ts_col" : exp.DataType.build(exp.DataType.Type.TIMESTAMP,nested=False),
-        "tstz_col" : exp.DataType.build(exp.DataType.Type.TIMESTAMPTZ,nested=False),
-        "int_array_col" : exp.DataType.build(
-                              exp.DataType.Type.ARRAY,
-                              expressions = [exp.DataType.build(exp.DataType.Type.INT,nested=False)],
-                              nested = True
-                          ),
+        "smallint_col": exp.DataType.build(exp.DataType.Type.SMALLINT, nested=False),
+        "int_col": exp.DataType.build(exp.DataType.Type.INT, nested=False),
+        "bigint_col": exp.DataType.build(exp.DataType.Type.BIGINT, nested=False),
+        "ts_col": exp.DataType.build(exp.DataType.Type.TIMESTAMP, nested=False),
+        "tstz_col": exp.DataType.build(exp.DataType.Type.TIMESTAMPTZ, nested=False),
+        "int_array_col": exp.DataType.build(
+            exp.DataType.Type.ARRAY,
+            expressions=[exp.DataType.build(exp.DataType.Type.INT, nested=False)],
+            nested=True,
+        ),
         "vchar_col": exp.DataType.build(exp.DataType.Type.VARCHAR),
         "struct_col": exp.DataType.build(
-                          exp.DataType.Type.STRUCT,
-                          expressions = [
-                              exp.ColumnDef(
-                                  this = exp.Identifier(
-                                             this = "nested_col",
-                                             quoted = False
-                                  ),
-                                  kind = exp.DataType.build(
-                                             exp.DataType.Type.INT,
-                                             nested = False
-                                  )
-                              )
-                          ],
-                          nested = True
-                      ),
+            exp.DataType.Type.STRUCT,
+            expressions=[
+                exp.ColumnDef(
+                    this=exp.Identifier(this="nested_col", quoted=False),
+                    kind=exp.DataType.build(exp.DataType.Type.INT, nested=False),
+                )
+            ],
+            nested=True,
+        ),
     }
+
 
 def test_create_view(adapter: t.Callable):
     adapter.create_view("db.view", parse_one("SELECT 1"), replace=True)
