c77155d44182012ae66b7a189f9b0f659a0bebee
Themis Valtinos <73662635+themisvaltinos@users.noreply.github.com>
2025-07-17 20:54:06 +0300

    Feat: Add signal listener and methods in scheduler

--- sqlmesh/utils/pydantic.py
+++ sqlmesh/utils/pydantic.py
@@ -60,6 +60,16 @@ def _expression_encoder(e: exp.Expression) -> str:
     return e.meta.get("sql") or e.sql(dialect=e.meta.get("dialect"))
 
 
+def serialize_expressions(kwargs: t.Dict[str, t.Optional[exp.Expression]]) -> t.Dict[str, str]:
+    serialized_kwargs: t.Dict[str, str] = {}
+    for key, value in kwargs.items():
+        if isinstance(value, exp.Expression):
+            serialized_kwargs[key] = _expression_encoder(value)
+        else:
+            serialized_kwargs[key] = str(value)
+    return serialized_kwargs
+
+
 AuditQueryTypes = t.Union[exp.Query, d.JinjaQuery]
 ModelQueryTypes = t.Union[exp.Query, d.JinjaQuery, d.MacroFunc]
 
