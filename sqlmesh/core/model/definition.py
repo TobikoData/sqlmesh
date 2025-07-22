0d6edefd48c43e8b1a746f00507afd321e3fce68
Themis Valtinos <73662635+themisvaltinos@users.noreply.github.com>
2025-07-22 17:44:31 +0300

    Feat: Add support for pre, post, on_virtual_update statements in config

--- sqlmesh/core/model/definition.py
+++ sqlmesh/core/model/definition.py
@@ -2472,6 +2472,24 @@ def _create_model(
 
     statements: t.List[t.Union[exp.Expression, t.Tuple[exp.Expression, bool]]] = []
 
+    # Merge default pre_statements with model-specific pre_statements
+    if "pre_statements" in defaults:
+        kwargs["pre_statements"] = list(defaults["pre_statements"]) + list(
+            kwargs.get("pre_statements", [])
+        )
+
+    # Merge default post_statements with model-specific post_statements
+    if "post_statements" in defaults:
+        kwargs["post_statements"] = list(defaults["post_statements"]) + list(
+            kwargs.get("post_statements", [])
+        )
+
+    # Merge default on_virtual_update with model-specific on_virtual_update
+    if "on_virtual_update" in defaults:
+        kwargs["on_virtual_update"] = list(defaults["on_virtual_update"]) + list(
+            kwargs.get("on_virtual_update", [])
+        )
+
     if "pre_statements" in kwargs:
         statements.extend(kwargs["pre_statements"])
     if "query" in kwargs:
