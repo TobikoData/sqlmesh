d8ba36cf168a2ea45d58f57b7be7a3a17c478862
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2025-07-29 16:11:04 -0700

    Fix: Respect the project configuration if the forward-only suffix was not set in the bot's config

--- sqlmesh/integrations/github/cicd/controller.py
+++ sqlmesh/integrations/github/cicd/controller.py
@@ -478,11 +478,13 @@ class GithubController:
         return self._pull_request.base.ref in self.bot_config.prod_branch_names
 
     @property
-    def forward_only_plan(self) -> bool:
-        head_ref = self._pull_request.head.ref
-        if isinstance(head_ref, str):
-            return head_ref.endswith(self.bot_config.forward_only_branch_suffix)
-        return False
+    def forward_only_plan(self) -> t.Optional[bool]:
+        if self.bot_config.forward_only_branch_suffix is not None:
+            head_ref = self._pull_request.head.ref
+            if isinstance(head_ref, str):
+                return head_ref.endswith(self.bot_config.forward_only_branch_suffix)
+            return False
+        return None
 
     @classmethod
     def _append_output(cls, key: str, value: str) -> None:
