d12d5b383137c398952ec92c2bc2749cc5037e81
Max Mykal <mykalmax@yahoo.com>
2025-08-18 09:29:21 -0700

    chore(web_common): add base styling + storybook (#5163)

---
+++ web/common/.storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/react-vite'
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+    '@storybook/addon-onboarding',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+}
+export default config
