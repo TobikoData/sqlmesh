e3540233eb2ce90d0ca48f88e6e8d4ba45d9e42d
Trey Spiller <treyspiller@gmail.com>
2025-09-16 09:46:51 -0500

    Don't repeat string conversion

--- sqlmesh/core/test/definition.py
+++ sqlmesh/core/test/definition.py
@@ -645,15 +645,14 @@ class ModelTest(unittest.TestCase):
 
         rows = values["rows"]
         if columns:
+            columns = [str(c) for c in columns]
             referenced_columns = list(dict.fromkeys(col for row in rows for col in row))
             _raise_if_unexpected_columns(columns, referenced_columns)
 
             if partial:
-                columns = [str(c) for c in columns if str(c) in referenced_columns]
+                columns = [c for c in columns if c in referenced_columns]
 
-        return pd.DataFrame.from_records(
-            rows, columns=[str(c) for c in columns] if columns else None
-        )
+        return pd.DataFrame.from_records(rows, columns=columns or None)
 
     def _add_missing_columns(
         self, query: exp.Query, all_columns: t.Optional[t.Collection[str]] = None
