26bba970d6d12f9ebf2c7802ad2693bdf8700b52
Max Mykal <mykalmax@yahoo.com>
2025-10-02 16:42:43 -0700

    fix(web_common): adjust types in lineage component (#5476)

--- web/common/src/components/Lineage/stories/dagreLayout.worker.ts
+++ web/common/src/components/Lineage/stories/dagreLayout.worker.ts
@@ -2,23 +2,37 @@ import {
   type LayoutedGraph,
   type LineageEdgeData,
   type LineageNodeData,
+  type EdgeId,
+  type NodeId,
+  type PortId,
 } from '../utils'
 import { buildLayout } from '../layout/dagreLayout'
 
 self.onmessage = <
   TNodeData extends LineageNodeData = LineageNodeData,
   TEdgeData extends LineageEdgeData = LineageEdgeData,
+  TNodeID extends string = NodeId,
+  TEdgeID extends string = EdgeId,
+  TPortID extends string = PortId,
 >(
-  event: MessageEvent<LayoutedGraph<TNodeData, TEdgeData>>,
+  event: MessageEvent<
+    LayoutedGraph<TNodeData, TEdgeData, TNodeID, TEdgeID, TPortID>
+  >,
 ) => {
   try {
-    const { edges, nodesMap } = buildLayout(event.data)
+    const { edges, nodesMap } = buildLayout<
+      TNodeData,
+      TEdgeData,
+      TNodeID,
+      TEdgeID,
+      TPortID
+    >(event.data)
 
     self.postMessage({
       edges,
       nodesMap,
-    } as LayoutedGraph<TNodeData, TEdgeData>)
-  } catch (outerError) {
-    self.postMessage({ error: outerError } as { error: ErrorEvent })
+    } satisfies LayoutedGraph<TNodeData, TEdgeData, TNodeID, TEdgeID, TPortID>)
+  } catch (error) {
+    self.postMessage({ error })
   }
 }
