d01a567332a4e0651abde64296ad23eed4fc735e
Max Mykal <mykalmax@yahoo.com>
2025-10-13 15:56:18 -0700

    fix(web_common): add title to control button (#5540)

--- web/common/src/components/Lineage/LineageControlButton.tsx
+++ web/common/src/components/Lineage/LineageControlButton.tsx
@@ -31,6 +31,7 @@ export function LineageControlButton({
               className,
             )}
             disabled={disabled}
+            title={text}
           >
             {children}
           </ControlButton>
