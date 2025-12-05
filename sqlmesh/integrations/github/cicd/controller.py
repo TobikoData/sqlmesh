289993ec05168648b70845b75180b456dc7dec79
Brian Crant <17074379+bcrant@users.noreply.github.com>
2025-12-05 09:14:09 -0800

    fix: CI/CD Bot exit early before applying plan on unmergeable PR (#5590)

--- sqlmesh/integrations/github/cicd/controller.py
+++ sqlmesh/integrations/github/cicd/controller.py
@@ -773,6 +773,11 @@ class GithubController:
                 "PR is already merged and this event was triggered prior to the merge."
             )
         merge_status = self._get_merge_state_status()
+        if merge_status.is_blocked:
+            raise CICDBotError(
+                "Branch protection or ruleset requirement is likely not satisfied, e.g. missing CODEOWNERS approval. "
+                "Please check PR and resolve any issues."
+            )
         if merge_status.is_dirty:
             raise CICDBotError(
                 "Merge commit cannot be cleanly created. Likely from a merge conflict. "
