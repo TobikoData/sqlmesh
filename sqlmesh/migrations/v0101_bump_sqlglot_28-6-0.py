7f147860f16efb85808645e24b13c4148a54a75c
Trey Spiller <treyspiller@gmail.com>
2026-01-22 11:07:46 -0600

    add migration script

---
+++ sqlmesh/migrations/v0101_bump_sqlglot_28-6-0.py
@@ -0,0 +1,9 @@
+"""Bump sqlglot to 28.6.0."""
+
+
+def migrate_schemas(engine_adapter, schema, **kwargs):  # type: ignore
+    pass
+
+
+def migrate_rows(engine_adapter, schema, **kwargs):  # type: ignore
+    pass
