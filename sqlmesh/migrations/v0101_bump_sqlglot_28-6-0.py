c5f79c10540ac2a587e07d71c79012843a706bef
Trey Spiller <treyspiller@gmail.com>
2026-01-26 17:21:38 -0600

    use new version's sushi in migration test

--- sqlmesh/migrations/v0101_bump_sqlglot_28-6-0.py
+++
@@ -1,9 +0,0 @@
-"""Bump sqlglot to 28.6.0."""
-
-
-def migrate_schemas(engine_adapter, schema, **kwargs):  # type: ignore
-    pass
-
-
-def migrate_rows(engine_adapter, schema, **kwargs):  # type: ignore
-    pass
